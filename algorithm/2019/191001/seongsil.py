4ebedaf2c0509bf866601e2d7813611b7eadfc5c
seongsilheo <44438752+seongsilheo@users.noreply.github.com>
2019-10-05 21:28:46 +0900

    Create seongsil.py

---
+++ algorithm/2019/191001/seongsil.py
@@ -0,0 +1,43 @@
+## 1
+def solution(operations):
+    queue = []
+    for i in operations:
+        op, num = i.split()
+        if op == "I":
+            queue.append(int(num))
+        elif op == "D":
+            if op == "D" and queue:
+                if num == "-1":
+                    queue.remove(min(queue))
+                elif num == "1":           
+                    queue.remove(max(queue))
+
+    if queue:
+        return [max(queue), min(queue)]
+    else:
+        return [0,0]
+    
+    
+    
+    
+
+## 2
+import heapq
+
+def solution(operations):
+    queue = []
+    
+    for i in operations:
+        op, num = i.split()
+        if op == "I":
+            heapq.heappush(queue ,int(num))
+        elif op == "D" and queue:
+            if num == "-1":
+                 heapq.heappop(queue)
+            elif num == "1":
+                queue.pop(queue.index(max(queue)))               
+
+    if queue:
+        return [max(queue), heapq.heappop(queue)]
+    else:
+        return [0,0]
