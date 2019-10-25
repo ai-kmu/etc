40fba0dc4b6a309769d9c48bfc248ae2f4222457
Ahnho <dksgh00@naver.com>
2019-10-25 10:31:42 +0900

    Update sangho.py

--- algorithm/2019/191023/sangho.py
+++ algorithm/2019/191023/sangho.py
@@ -1,7 +1,7 @@
 def solution(tri):
-    current_line = tri[-1]
+    current = tri[-1]
     for t in tri[-2::-1]:
-        x = [sum(x) for x in zip(t, current_line)]
-        y = [sum(x) for x in zip(t, current_line[1:])]
-        current_line = [max(*n) for n in zip(x, y)]
-    return current_line[0]
+        x = [sum(x) for x in zip(t, current)]
+        y = [sum(x) for x in zip(t, current[1:])]
+        current = [max(*n) for n in zip(x, y)]
+    return current[0]
