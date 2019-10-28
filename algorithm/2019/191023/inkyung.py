09ab2b67c54d1ef6bc849fb38f9eefd29483f905
InKyung Kim <37397258+IKKIM00@users.noreply.github.com>
2019-10-28 16:52:53 +0900

    Update inkyung.py

--- algorithm/2019/191023/inkyung.py
+++ algorithm/2019/191023/inkyung.py
@@ -4,3 +4,4 @@ def solution(triangle):
         for i in range(tri_len - 1):
             triangle[tri_len - 2][i] += max(triangle[tri_len - 1][i], triangle[tri_len - 1][i+1])
         tri_len -= 1
+    return triangle[0][0]
