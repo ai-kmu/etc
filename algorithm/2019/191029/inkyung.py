f9d45677cb9ce9defbf3c6fee54c91e21c83fffd
InKyung Kim <kimjennifer0429@gmail.com>
2019-10-30 16:56:16 +0900

    inkyung.py update

---
+++ algorithm/2019/191029/inkyung.py
@@ -0,0 +1,10 @@
+def solution(A, B):
+    A = sorted(A)
+    B = sorted(B)
+    lst = []
+    for i in range(len(A)):
+        for j in range(len(B)):
+            if A[i] < B[j]:
+                lst.append(B[j])
+                B.remove(B[j])
+                break
