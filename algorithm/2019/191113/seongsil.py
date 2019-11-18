dd02195f1e1b597398c0b67ee1d350fa10f803ab
seongsilheo <44438752+seongsilheo@users.noreply.github.com>
2019-11-18 16:55:26 +0900

    Create seongsil.py

---
+++ algorithm/2019/191113/seongsil.py
@@ -0,0 +1,37 @@
+from collections import defaultdict
+
+def numSubmatrix(matrix, target):
+    res = 0
+    row, col = len(matrix), len(matrix[0])
+    for k in range(row):
+        nums = [0 for _ in range(col)]
+        for i in range(k, row):
+            for j in range(col):
+                nums[j] += matrix[i][j]
+            # nums: [0,1,0], [1,2,1], [1,3,1], [1,1,1], [1,2,1], [0,1,0] 
+            res += check(nums, target)
+    return res
+        
+def check(nums, target):
+    counter, res = defaultdict(int), 0  # defaultdict(<class 'int'>, {})
+    counter[0], cum_sum = 1,0  # counter에 {0:1} 추가
+    #print("start")
+    #print("nums", nums)
+    for num in nums:  # cum_sum -> 1,4,5,3,4,1
+        cum_sum += num
+        res += counter[cum_sum - target]  #앞이랑 똑같으면 합이 0이라는뜻
+        #print("cum_sum:", cum_sum)
+        counter[cum_sum] += 1
+        #print("counter2:" ,counter)
+        #print("res:", res)
+    return res
+    #res : 1 -> 0 -> 0 -> 0 -> 0 -> 1
+    
+    
+    
+    
+matrix1 = [[0,1,0],[1,1,1],[0,1,0]] 
+matrix2 = [[1,-1],[-1,1]]
+target = 0
+
+print(numSubmatrix(matrix1,target))
