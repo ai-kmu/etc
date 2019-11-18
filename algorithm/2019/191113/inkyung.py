c19c456b162b277358daf63faacc39c9c2b693e6
InKyung Kim <37397258+IKKIM00@users.noreply.github.com>
2019-11-18 16:05:45 +0900

    Update inkyung.py

--- algorithm/2019/191113/inkyung.py
+++ algorithm/2019/191113/inkyung.py
@@ -2,13 +2,12 @@ import collections
 class Solution:
     def numSubmatrixSumTarget(self, matrix, target):
         ans, height, width = 0, len(matrix), len(matrix[0])
-        b = [[0] * width]
+        extra = [[0] * width]
         for i in range(height):
             new_row = []
             for j in range(width):
-                new_row.append(b[i][j] + matrix[i][j])
-            b.append(new_row)
-        #print(b)        
+                new_row.append(extra[i][j] + matrix[i][j])
+            extra.append(new_row)     
         for start in range(height):
             for end in range(start, height):
                 seen = collections.Counter()
@@ -16,9 +15,9 @@ class Solution:
                 tot = 0
                 #print(seen)
                 for j in range(width):
-                    print(b[end+1][j])
-                    print(b[start][j])
-                    tot += b[end + 1][j] - b[start][j]
+                    print(extra[end+1][j])
+                    print(extra[start][j])
+                    tot += extra[end + 1][j] - extra[start][j]
                     print("tot", tot)
                     if tot - target in seen:
                         ans += seen[tot - target]
