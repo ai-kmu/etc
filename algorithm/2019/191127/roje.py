b56dcfd93114987078e4716129018bf07b512fd3
darlcode <33410490+jorek1022@users.noreply.github.com>
2019-12-26 16:26:43 +0900

    Update roje.py

--- algorithm/2019/191127/roje.py
+++ algorithm/2019/191127/roje.py
@@ -1,11 +1,9 @@
+## sol1
 def solution(begin,target,words):
     ans_cnt = 0
-    # target이 words에 존재하지 않으면 바로 return 0
     if target not in words:
         return 0
-    # target이 words에 존재할 때
     else:
-        # words에 단어들이 존재할때까지
         while(len(words) != 0):
             for word in words:
                 count = 0
@@ -22,4 +20,31 @@ def solution(begin,target,words):
                 return ans_cnt
             else:
                 begin = temp
+    return ans_cnt
+
+## sol2
+def solution(begin,target,words):
+    answer = [begin]
+    ans_cnt = 0
+    if target not in words:
+        return 0
+    else:
+        while(len(words) != 0):
+            for start in answer:
+                for word in words:
+                    count = 0
+                    for idx in range(len(start)):
+                        if start[idx] != word[idx]:
+                            count += 1
+                        if count == 2:
+                            break
+                    if count == 1:
+                        answer.append(word)
+                        words.remove(word)
+                ans_cnt += 1
+                if target in answer:
+                    return ans_cnt
+                else:
+                    if ans_cnt == 1:
+                        answer.remove(begin)
     return ans_cnt
