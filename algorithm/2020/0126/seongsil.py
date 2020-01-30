c9f39190a0c1e8856c0e165ed5ee68feac613107
seongsilheo <44438752+seongsilheo@users.noreply.github.com>
2020-01-30 00:28:05 -0800

    Create seongsil.py

---
+++ algorithm/2020/0126/seongsil.py
@@ -0,0 +1,38 @@
+import math
+def solution(distance, rocks, n):
+    
+    rocks.sort()
+    rocks.append(distance)
+    
+    min_, max_ = 0, distance
+    
+    answer = 0
+    while min_ <= max_:
+
+        prev = 0
+        mins = distance
+
+        count = 0
+
+        criterion = (min_ + max_) // 2
+        
+        for rock in rocks:
+            if rock - prev < criterion:
+                count += 1
+            else:
+                mins = min(mins, rock -  prev)
+                prev = rock
+  
+        if count > n:
+            max_ = criterion -1   # -1, +1 안하면 left=4, right=5에서 무한루프
+            
+
+        else:
+            answer = mins 
+            min_ = criterion + 1 # +1 안하면 left=5, right=5에서 무한루프      
+    return answer
+
+distance = 25
+rocks = [2, 14, 11, 21, 17]
+n = 2
+print(solution(distance, rocks, n))
