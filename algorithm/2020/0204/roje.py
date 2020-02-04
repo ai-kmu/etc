9cbfbdfc4e4821e243f4ff39651183f16e849db1
darlcode <33410490+jorek1022@users.noreply.github.com>
2020-02-04 11:59:07 +0900

    Update roje.py

--- algorithm/2020/0204/roje.py
+++ algorithm/2020/0204/roje.py
@@ -7,3 +7,11 @@ for iters in range(1, iteration+1):
   for row in range(x, x + width):
     for col in range(y, y + height):
       maps[row][col] = iters
+      
+for iters in maps:
+  for num in iters:
+    if num:
+      answer[num] += 1
+
+for ans in answer[1:]:
+  print(ans)
