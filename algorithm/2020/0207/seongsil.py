555b735852ac1d9f0b254b244d64bb94954109c1
seongsilheo <44438752+seongsilheo@users.noreply.github.com>
2020-02-13 12:36:57 -0800

    Update seongsil.py

--- algorithm/2020/0207/seongsil.py
+++ algorithm/2020/0207/seongsil.py
@@ -12,7 +12,6 @@ def solve(num_of_house, cost):
     min_ = 1001
     for i in range(3): #첫번째 집 색 고정
         dp = [[-1]*3 for _ in range(num_of_house)]
-
         pre_solve(dp,i,num_of_house)        
         for j in range(1,num_of_house):
             dp[j][0] = cost[j][0] + min(dp[j-1][1], dp[j-1][2])
@@ -25,11 +24,8 @@ def solve(num_of_house, cost):
             min_ = min(dp[num_of_house-1])
 
     return min_
-        
-
 
 num_of_house = int(input())
-
 cost = []
 for n in range(num_of_house):
     temp = list(map(int, sys.stdin.readline().split()))
