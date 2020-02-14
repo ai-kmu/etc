e59fbd174f4c551830dbde638bff4ef605cba09b
seongsilheo <44438752+seongsilheo@users.noreply.github.com>
2020-02-13 18:29:31 -0800

    Update seongsil.py

--- algorithm/2020/0207/seongsil.py
+++ algorithm/2020/0207/seongsil.py
@@ -1,16 +1,14 @@
-# 답은 잘나오는데 왜 런타임 에러가 뜨는지 몰겠음다
-
-MAX = 1001
+import math
 
 def pre_solve(dp, first_color,num_of_house):
-    for j in range(num_of_house):
+    for j in range(3):
         if j == first_color:
             dp[0][j] = cost[0][j]
         else:
             dp[0][j] = MAX
 
 def solve(num_of_house, cost):
-    min_ = 1001
+    min_ = math.inf 
     for i in range(3): #첫번째 집 색 고정
         dp = [[-1]*3 for _ in range(num_of_house)]
         pre_solve(dp,i,num_of_house)        
@@ -32,5 +30,8 @@ cost = []
 for n in range(num_of_house):
     temp = list(map(int, input().split()))
     cost.append(temp)
-
+    
+MAX = 1001 * num_of_house + 1
 print(solve(num_of_house, cost))
+
+
