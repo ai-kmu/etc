137fac5ad764069d6e0803bb1c89f738834cfbda
dajimmy1120 <50998006+dajimmy1120@users.noreply.github.com>
2020-03-11 14:04:14 +0900

    Update dahyun.py

--- algorithm/2020/0313/dahyun.py
+++ algorithm/2020/0313/dahyun.py
@@ -5,7 +5,7 @@ def solution(progresses, speeds):
     remains = list(map(lambda x: 100-x, progresses))                # 남은 작업진도 구하기
     dates = list(map(lambda x,y: math.ceil(x/y), remains, speeds))  # 배포까지 남은 일수 구하기(remains/dates 값을 올림)
     
-    while dates != []:        # dates가 빈 리스트가 되면 stop
+    while dates != []:        # dates가 빈 리스트가 되기 전까지
         count = 0             # 각 loop마다 count 초기화
         for d in dates:       # dates의 첫번째 원소부터 차례대로
             if d <= dates[0]: # 첫번째 원소보다 작거나 같은 애들만 count
