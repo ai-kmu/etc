60fac5b8703913709e5b726e93cb30a39d53c2b7
Jude Park <judepark@kookmin.ac.kr>
2020-03-27 12:27:24 +0900

    Update eunhwan.py

--- algorithm/2020/0327/eunhwan.py
+++ algorithm/2020/0327/eunhwan.py
@@ -4,7 +4,7 @@ def solution(arrangement: str) -> int:
     for data in arrangement:
         if data == '(': # 새로운 막대기 길이
             bar += 1
-        elif data == '#':   # 액션 빔~ 레이저빔 쏘고 이전까지의 바 길이 더함
+        elif data == '#': # 레이저빔 쏘고 이전까지의 바 길이 더함
             answer += bar
         elif data == ')':   # 막대기의 끝, -1과 +1 해줌.
             bar -= 1
