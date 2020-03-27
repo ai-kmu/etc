bf5c20167b056a2e5b162971e6a5ea24f57e3651
JaeBinLee <39715124+Albert-learner@users.noreply.github.com>
2020-03-27 10:41:00 +0900

    Add files via upload

--- algorithm/2020/0327/JaeBin.py
+++ algorithm/2020/0327/JaeBin.py
@@ -1,11 +1,15 @@
 def solution(arrangement):
     answer = 0
+
+    # 리스트로 괄호 하나하나에 대해 연산
     arrange_list = list(arrangement)
     tmp = list()
 
+    # 자르기 전과 후의 막대의 개수 저장하는 변수
     cutting_before = 0
     cutting_after = 0
 
+    # list 길이만큼 돌면서 맨 앞의 요소에 따라 막대의 개수 판단
     for bracket in range(len(arrange_list)):
         first = arrange_list.pop(0)
         if first == '(':
@@ -22,5 +26,5 @@ def solution(arrangement):
     answer = cutting_after
     return answer
 
-arrange_1 = '()(((()())(())()))(())'
-print(solution(arrange_1))
+arrangement_1 = '()(((()())(())()))(())'
+print(solution(arrangement_1))
