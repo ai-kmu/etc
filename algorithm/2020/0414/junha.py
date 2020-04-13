427482baa19def2d16fc157cb81d0d2b675d2cd8
junha1125 <46951365+junha1125@users.noreply.github.com>
2020-04-13 21:02:02 +0900

    Update junha.py

--- algorithm/2020/0414/junha.py
+++ algorithm/2020/0414/junha.py
@@ -1,22 +1,31 @@
 '''
-(-1을 return하는 경우가 필요한지는 실험을 통해 알아보자.)
+(-1을 return하는 경우가 필요한지는 실험을 통해 알아보자. -> 비슷비슷 하다. 하지만 없는게 낫다.)
 1. food_times를 sort한다.
 2. cur_min = list[0]; cur_pos = 0; n = len(food_times);
-3. while cur_min * n <= k :
-   diff라는 개념! food_times간의 차이 <- 실수해서 초반 오답 해결: 손코딩한거 직접 예제 돌려보자. 
-   k, cur_min과 cur_pos를 갱신
-   cur_pos가 2001이면 -1 return하고 함수 종료
-4. 위의 while이 break되면, cur_min의 값보다 같거나 큰 수들 중, (k)%n 번쨰 음식을 찾는다.
+3. while cur_min * n <= k : 최소 시간이 걸리는 음식을 다 먹어 치워도 된다면.
+   diff (: 최소 food_times간의 차이) 갱신
+   cur_min과 cur_pos를 갱신 ---> cur_next함수가 해준다. 
+   if cur_min == -1;  return -1; 하고 끝.
+4. while이 break된다면, 
+   cur_min의 값보다 같거나 큰 수들 중, (k)%n 번쨰 음식을 찾는다. ---> find_index함수가 해준다.
 '''
 
 
 def cur_next(lis, cur_min, cur_pos, n, l):
+   '''
+   sort된 list에서
+   다음으로 작은 시간이 걸리는 음식과 그 음식의 index를 찾는다.
+   '''
     for i in range(cur_pos+1, l):
         if cur_min < lis[i]:
             return lis[i], i
     return -1 , l+1
 
 def fine_index(food_times, m, cur_min) : 
+   '''
+   sort되지 않은 list에서
+   cur_min보다 큰 수 중 m번째 음식의 index를 찾는다.
+   '''
     for index, Value in enumerate(food_times):
         if cur_min <= Value :
             if m == 0:
@@ -25,16 +34,16 @@ def fine_index(food_times, m, cur_min) :
                 m -= 1
 
 def solution(food_times, k):
-    # 1
+    # 1 food_times를 sort한다.
     lis = food_times[:]
     lis.sort()
-    # 2
+    # 2 cur_min = list[0]; cur_pos = 0; n = len(food_times);
     cur_min = lis[0]
     diff = cur_min
     cur_pos = 0 # list 0부터 시작
     n = len(lis) # 남은 음식 수
     l = n # 처음 음식의 총 갯수
-    # 3
+    # 3 while 문을 돌아야한다면. 
     while diff*n <= k:
         k = k - diff*n
         temp, cur_pos = cur_next(lis, cur_min, cur_pos, n, l)
@@ -43,7 +52,7 @@ def solution(food_times, k):
         cur_min = temp
         n = l - cur_pos
         
-    # 4
+    # 4 while break 
     cur_min = lis[cur_pos]
     answer = fine_index(food_times, k%n, cur_min)
     return answer
