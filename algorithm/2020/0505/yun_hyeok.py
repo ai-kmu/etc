eaf99dec8be39205b08635a53fec9f979ff8a073
Yun-hyeok Kwak <rev1c0sm0s@gmail.com>
2020-05-13 23:13:08 +0900

    Update yun_hyeok.py

--- algorithm/2020/0505/yun_hyeok.py
+++ algorithm/2020/0505/yun_hyeok.py
@@ -2,7 +2,7 @@ from collections import deque, namedtuple
 def solution(maps):
     N, M = len(maps) - 1, len(maps[0]) - 1
     Position = namedtuple("Position", ["y", "x"])
-    Position.__add__ = lambda left, right: Position(left.y + right.y, left.x + right.x)
+    Position.__add__ = lambda lhs, rhs: Position(lhs.y + rhs.y, lhs.x + rhs.x)
     Position.is_out_of_range = lambda self: not 0 <= self.y <= N or not 0 <= self.x <= M
     
     raw_directions = [[-1, 0], [0, 1], [1, 0], [0, -1]]
