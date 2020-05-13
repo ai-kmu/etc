4fd0aa4f5f069e6ed1d37d9d4febe614172a2af4
Yun-hyeok Kwak <rev1c0sm0s@gmail.com>
2020-05-13 23:07:01 +0900

    Update yun_hyeok.py

--- algorithm/2020/0505/yun_hyeok.py
+++ algorithm/2020/0505/yun_hyeok.py
@@ -4,19 +4,18 @@ def solution(maps):
     Position = namedtuple("Position", ["y", "x"])
     Position.__add__ = lambda left, right: Position(left.y + right.y, left.x + right.x)
     Position.is_out_of_range = lambda self: not 0 <= self.y <= N or not 0 <= self.x <= M
-    State = namedtuple("State", ["map", "pos", "depth"])
     
-    raw_actions = [[-1, 0], [0, 1], [1, 0], [0, -1]]
-    actions = list(map(lambda x: Position(*x), raw_actions))
-    states = deque([State(maps, Position(0, 0), 1)])
+    raw_directions = [[-1, 0], [0, 1], [1, 0], [0, -1]]
+    directions = list(map(lambda x: Position(*x), raw_directions))
+    q = deque([(Position(0, 0), 1)])
     
     while True:
-        if len(states) == 0: return -1
-        (map_, pos, depth) = states.popleft()
+        if len(q) == 0: return -1
+        pos, depth = q.popleft()
         if pos == Position(N, M): return depth
-        for action in actions:
-            next_pos = pos + action
+        for d in directions:
+            next_pos = pos + d
             if next_pos.is_out_of_range(): continue
-            if map_[next_pos.y][next_pos.x] == 1:
-                map_[pos.y][pos.x] = -1
-                states.append(State(map_, next_pos, depth+1))
+            if maps[next_pos.y][next_pos.x] == 1:
+                maps[pos.y][pos.x] = -1
+                q.append((next_pos, depth+1))
