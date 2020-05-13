50703b0955f22ed44589f9073fb4d7adb1374e0c
Yun-hyeok Kwak <rev1c0sm0s@gmail.com>
2020-05-13 23:14:12 +0900

    Update yun_hyeok.py

--- algorithm/2020/0505/yun_hyeok.py
+++ algorithm/2020/0505/yun_hyeok.py
@@ -1,18 +1,18 @@
 from collections import deque, namedtuple
 def solution(maps):
     N, M = len(maps) - 1, len(maps[0]) - 1
-    Position = namedtuple("Position", ["y", "x"])
-    Position.__add__ = lambda lhs, rhs: Position(lhs.y + rhs.y, lhs.x + rhs.x)
-    Position.is_out_of_range = lambda self: not 0 <= self.y <= N or not 0 <= self.x <= M
+    Pos = namedtuple("Position", ["y", "x"])
+    Pos.__add__ = lambda lhs, rhs: Pos(lhs.y + rhs.y, lhs.x + rhs.x)
+    Pos.is_out_of_range = lambda self: not 0 <= self.y <= N or not 0 <= self.x <= M
     
     raw_directions = [[-1, 0], [0, 1], [1, 0], [0, -1]]
-    directions = list(map(lambda x: Position(*x), raw_directions))
-    q = deque([(Position(0, 0), 1)])
+    directions = list(map(lambda x: Pos(*x), raw_directions))
+    q = deque([(Pos(0, 0), 1)])
     
     while True:
         if len(q) == 0: return -1
         pos, depth = q.popleft()
-        if pos == Position(N, M): return depth
+        if pos == Pos(N, M): return depth
         for d in directions:
             next_pos = pos + d
             if next_pos.is_out_of_range(): continue
