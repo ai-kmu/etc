6016a3ecf4ccac4bdfd50e029deda86bc8313fe4
Yun-hyeok Kwak <rev1c0sm0s@gmail.com>
2020-05-13 22:44:31 +0900

    Update yun_hyeok.py

--- algorithm/2020/0505/yun_hyeok.py
+++ algorithm/2020/0505/yun_hyeok.py
@@ -6,22 +6,21 @@ def solution(maps):
 
     Position = namedtuple("Position", ["y", "x"])
     Position.__add__ = lambda left, right: Position(left.y + right.y, left.x + right.x)
-    State = namedtuple("State", ["map", "pos", "count"])
+    Position.is_out_of_range = lambda self: not 0 <= self.y <= N or not 0 <= self.x <= M
+    State = namedtuple("State", ["map", "pos", "depth"])
     
     raw_actions = [[-1, 0], [0, 1], [1, 0], [0, -1]]
     actions = list(map(lambda x: Position(*x), raw_actions))
     states = deque([State(maps, Position(0, 0), 1)])
     
     while True:
-        if len(states) == 0:
-            return -1
-        (map_, pos, count) = states.popleft()
+        if len(states) == 0: return -1
+        (map_, pos, depth) = states.popleft()
         if pos == Position(N, M):
-            return count
+            return depth
         for action in actions:
             next_pos = pos + action
-            if not 0 <= next_pos.y <= N or not 0 <= next_pos.x <= M:
-                continue
+            if next_pos.is_out_of_range(): continue
             if map_[next_pos.y][next_pos.x] == 1:
                 map_[pos.y][pos.x] = -1
-                states.append(State(deepcopy(map_), next_pos, count+1))
+                states.append(State(deepcopy(map_), next_pos, depth+1))
