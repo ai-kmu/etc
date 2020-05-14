6d17f25bf6ac5ff247d2c700e9252c2d312a1c45
Yun-hyeok Kwak <rev1c0sm0s@gmail.com>
2020-05-14 12:33:39 +0900

    Update yun_hyeok.py

--- algorithm/2020/0505/yun_hyeok.py
+++ algorithm/2020/0505/yun_hyeok.py
@@ -10,12 +10,12 @@ def solution(maps):
     q = deque([(Pos(0, 0), 1)])
     
     while True:
-        if len(q) == 0: return -1
+        if len(q) == 0: return -1 # 더이상 이어갈 곳이 없는 경우
         pos, depth = q.popleft()
-        if pos == Pos(N, M): return depth
+        if pos == Pos(N, M): return depth # 도착
         for d in directions:
             next_pos = pos + d
             if next_pos.is_out_of_range(): continue
-            if maps[next_pos.y][next_pos.x] == 1:
-                maps[pos.y][pos.x] = -1
+            if maps[next_pos.y][next_pos.x] == 1: # 가본 곳: -1, 벽: 0, 길: 1
+                maps[pos.y][pos.x] = -1 # 가본 곳은 -1로 세팅
                 q.append((next_pos, depth+1))
