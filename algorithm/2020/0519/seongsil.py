0503d93c14b516b6556bcb24a0cb1493e0503ade
seongsilheo <44438752+seongsilheo@users.noreply.github.com>
2020-05-17 14:37:38 -0700

    Update seongsil.py

--- algorithm/2020/0519/seongsil.py
+++ algorithm/2020/0519/seongsil.py
@@ -1,10 +1,10 @@
 def solution(answers):
-    student = [[1,2,3,4,5], [2,1,2,3,2,4,2,5] ,[3,3,1,1,2,2,4,4,5,5]]    
+    student = [[1,2,3,4,5], [2,1,2,3,2,4,2,5] ,[3,3,1,1,2,2,4,4,5,5]]  #수포자 패턴 초기화  
     scores = [0,0,0]
     
     for i in range(len(answers)):  
         for j, stu in enumerate(student):
-            if answers[i] == stu[i % len(stu)]:
+            if answers[i] == stu[i % len(stu)]:  # 정답과 일치할경우 score 더하기
                 scores[j] += 1
                 
     return [i+1 for i, x in enumerate(scores) if x == max(scores)]  #가장 max한 점수를 가지는 학생 리턴
