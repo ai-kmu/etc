aacb65dc3471a5c44af4eca036e68d4bd9b7e307
Yun-hyeok Kwak <rev1c0sm0s@gmail.com>
2020-06-03 14:54:41 +0900

    Update yun_hyeok.py

--- algorithm/2020/0602/yun_hyeok.py
+++ algorithm/2020/0602/yun_hyeok.py
@@ -1,9 +1,9 @@
 PROBLEM_N = 11
-problems_v_d = sorted([tuple(map(int, input().split())) for _ in range(PROBLEM_N)], key=lambda x: x[0])
+problems_d_v = sorted([tuple(map(int, input().split())) for _ in range(PROBLEM_N)], key=lambda x: x[0])
 total_penalty = 0
 t = 0
 for i in range(PROBLEM_N):
-    d, v = problems_v_d[i]
+    d, v = problems_d_v[i]
     t += d
     total_penalty += t + 20*v
 print(total_penalty)
