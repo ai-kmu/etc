4752ed01381dbc91e8ba0700d45a12274e4c989a
Juhee Kang <38813640+juheesvt@users.noreply.github.com>
2020-07-27 02:58:30 +0900

    Runtime: 984 ms

--- algorithm/2020/0727/juheesvt.py
+++ algorithm/2020/0727/juheesvt.py
@@ -23,11 +23,9 @@ class Solution:
         substring = ["" for i in range(length)]       # substring의 각 자리에는 문자열 s에서 해당 인덱스까지의 longest substring이 저장된다
         
         while i < length:
-            print(substring[i], "/n/n")
             while (j < length) and (substring[i].find(s[j]) == -1) :    # 같은 문자가 나오지 않을 때까지 계속 문자열에 더함 !!
                 substring[i] += s[j]
                 j += 1
-                print(substring[i])
             i += 1
             j = i
             
