3841aeb05f95207a259dff88387f9c142f2f2a17
IKKIM00 <kimjennifer0429@gmail.com>
2020-09-02 14:34:15 +0900

    update inkyung.py

--- algorithm/2020/0824/inkyung.py
+++ algorithm/2020/0824/inkyung.py
@@ -1,11 +1,12 @@
 class Solution:
     def convert(self, s: str, numRows: int) -> str:
-        if numRows < 2:
+        if numRows == 1:
             return s
         i = 0
         str_lst = [""]*numRows     
         
         for letter in s:
+            print(i)
             if i == numRows-1:  
                 go_down = False 
             elif i == 0:        
@@ -14,3 +15,4 @@ class Solution:
             str_lst[i] += letter
             i = (i+1) if go_down else i-1  					
         return "".join(str_lst)
+
