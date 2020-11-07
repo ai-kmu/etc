f81bacedb08b504003d02e8453ed539e2b6a2ff9
seongsilheo <44438752+seongsilheo@users.noreply.github.com>
2020-11-07 23:20:57 +0900

    Update seongsil.py

--- algorithm/2020/1107_Binary_Tree_Inorder_Traversal/seongsil.py
+++ algorithm/2020/1107_Binary_Tree_Inorder_Traversal/seongsil.py
@@ -1,5 +1,3 @@
-
-
 class Solution:
     def inorderTraversal(self, root: TreeNode) -> List[int]:
         return (self.inorderTraversal(root.left) + [root.val] + self.inorderTraversal(root.right)) if root else []
