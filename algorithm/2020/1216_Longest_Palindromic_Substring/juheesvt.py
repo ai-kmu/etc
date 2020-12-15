5d916d159ded3267d7df8479e57b6705c457a52c
Juhee Kang <38813640+juheesvt@users.noreply.github.com>
2020-12-16 04:07:32 +0900

    Create juheesvt.py

---
+++ algorithm/2020/1216_Longest_Palindromic_Substring/juheesvt.py
@@ -0,0 +1,19 @@
+
+
+class Solution:
+    def longestPalindrome(self, s: str) -> str:
+        if len(s) <= 1:
+            return s
+            
+        i,l=0,0
+        
+        # j로 문자열 s을 순회하면서 s[j-l-1:j+1] -> 즉 j를 기준으로 l+1 만큼의 길이를 가진 substring이 팬린드롬인지 검사하고 i와 l을 업데이트
+        for j in range(len(s)):
+            # 가장 긴 substring의 시작 idx를 i에 저장하고, 길이는 l에 저장
+            if s[j-l: j+1] == s[j-l: j+1][::-1]:
+                i, l = j-l, l+1
+               
+            elif j-l > 0 and s[j-l-1: j+1] == s[j-l-1: j+1][::-1]:
+                i, l = j-l-1, l+2
+                
+        return s[i: i+l]
