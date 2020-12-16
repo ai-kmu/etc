d0387d353d2035eb3d051dc21818cfb4deebe2fd
Juhee Kang <38813640+juheesvt@users.noreply.github.com>
2020-12-16 12:17:26 +0900

    Update juheesvt.py

--- algorithm/2020/1216_Longest_Palindromic_Substring/juheesvt.py
+++ algorithm/2020/1216_Longest_Palindromic_Substring/juheesvt.py
@@ -1,4 +1,21 @@
+"""
+brute force
 
+class Solution:
+    def longestPalindrome(self, s: str) -> str:
+        long = ""
+        if len(s) <= 1:
+            return s
+        
+        for i in range(len(s)):
+            for j in range(len(s), i, -1):
+                if len(long) >= j-i:
+                    continue
+                elif s[i:j] == s[i:j][::-1]:
+                    long = s[i:j]
+
+        return long
+"""
 
 class Solution:
     def longestPalindrome(self, s: str) -> str:
