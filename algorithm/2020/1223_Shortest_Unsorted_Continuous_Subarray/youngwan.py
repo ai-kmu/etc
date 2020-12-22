6b500040fb8ad42dfbeb65d37eddbc13e8f07977
whduddhks <39693818+whduddhks@users.noreply.github.com>
2020-12-22 14:19:26 +0900

    Create youngwan.py

---
+++ algorithm/2020/1223_Shortest_Unsorted_Continuous_Subarray/youngwan.py
@@ -0,0 +1,14 @@
+class Solution:
+    def findUnsortedSubarray(self, nums: List[int]) -> int:
+        front = -1
+        back = -1
+        sort_nums = sorted(nums)         # 정렬
+        for i in range(len(nums)):
+            if nums[i] != sort_nums[i]:  # 정렬된 리스트랑 다른 곳을 찾음
+                if front == -1:          # 앞인지 뒤인지 찾음
+                    front = i
+                else:
+                    back = i
+        if front == -1:                  # 틀린 곳이 없다면 0 반환
+            return 0
+        return back - front + 1          # 바꿔야하는 숫자의 개수를 반환
