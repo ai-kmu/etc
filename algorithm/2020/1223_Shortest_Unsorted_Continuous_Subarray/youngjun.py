a9c51fdca03682659f661814b0902ed538fc96a5
junyoo96 <junyoo96@gmail.com>
2020-12-23 12:01:20 +0900

    Update youngjun.py

--- algorithm/2020/1223_Shortest_Unsorted_Continuous_Subarray/youngjun.py
+++ algorithm/2020/1223_Shortest_Unsorted_Continuous_Subarray/youngjun.py
@@ -9,7 +9,7 @@ class Solution:
             start += 1
 
         #제일 오른쪽이 최대값이 안되게게
-       while start <= end and nums[end]==max(nums[start:end + 1]):
+        while start <= end and nums[end]==max(nums[start:end + 1]):
             end -= 1
 
         return end - start + 1
