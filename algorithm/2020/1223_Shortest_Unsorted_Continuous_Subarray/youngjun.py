62b78334e4ce201a7545367522fad831657ca91b
junyoo96 <junyoo96@gmail.com>
2020-12-23 12:05:26 +0900

    Update youngjun.py

--- algorithm/2020/1223_Shortest_Unsorted_Continuous_Subarray/youngjun.py
+++ algorithm/2020/1223_Shortest_Unsorted_Continuous_Subarray/youngjun.py
@@ -4,11 +4,11 @@ class Solution:
         start = 0
         end = len(nums) - 1
 
-        #맨 왼쪽이 최소값이 안되게 하기
+        #맨 왼쪽이 최소값이 아닐 경우 까지
         while start <= end and nums[start]==min(nums[start:end + 1]):
             start += 1
 
-        #제일 오른쪽이 최대값이 안되게게
+        #맨 오른쪽이 최대값이 아닐 경우 까지
         while start <= end and nums[end]==max(nums[start:end + 1]):
             end -= 1
 
