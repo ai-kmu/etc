5c45a7ee7642235c4e04c24b4a246da0127f05ab
jmin0530 <72537757+jmin0530@users.noreply.github.com>
2020-12-30 01:32:16 +0900

    Add files via upload

---
+++ algorithm/2020/1230_Jump_Game/jungmin.py
@@ -0,0 +1,16 @@
+class Solution:
+    def canJump(self, nums: List[int]) -> bool:
+        maximum_index = 0
+        for i in range(len(nums)):
+            if i + nums[i] >= maximum_index:
+                maximum_index = i + nums[i]
+            
+            if maximum_index == i:
+                break
+
+        if maximum_index == nums(len(nums)-1):
+            return True
+
+        elif maximum_index < nums(len(nums)-1):
+            return False
+
