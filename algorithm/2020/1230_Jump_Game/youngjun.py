9d422c95ec0c39b9f3d328d4d4005bcbfe7b6d9f
junyoo96 <junyoo96@gmail.com>
2020-12-30 11:39:39 +0900

    Update youngjun.py

--- algorithm/2020/1230_Jump_Game/youngjun.py
+++ algorithm/2020/1230_Jump_Game/youngjun.py
@@ -1,10 +1,13 @@
 class Solution:
     def canJump(self, nums: List[int]) -> bool:
+        answer=False
         d_idx = len(nums) - 1
 
         for i in range(len(nums) - 1, -1, -1): #마지막 도착 d_idx를 앞으로 이동하기
             if nums[i] + i >= d_idx:
                 d_idx = i
 
+        if d_idx==0:
+            answer=True
         #마지막에 d_idx가 연결된지 확인
-        return d_idx == 0
+        return answer
