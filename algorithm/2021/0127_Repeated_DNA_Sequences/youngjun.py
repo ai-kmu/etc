e27f25940f43a6c114f6d096f3c1094b2a9788ee
junyoo96 <junyoo96@gmail.com>
2021-01-27 11:48:02 +0900

    Update youngjun.py

--- algorithm/2021/0127_Repeated_DNA_Sequences/youngjun.py
+++ algorithm/2021/0127_Repeated_DNA_Sequences/youngjun.py
@@ -1,13 +1,12 @@
 class Solution(object):
     def findRepeatedDnaSequences(self, s):
-        ans = set()
-        seen = set()
+        ans,checked = set(),set() # 중복값 허용안하기 때문에 set 사용
+      
+        for i in range(len(s) - 9): # 뒤에 마지막 10자리가 남을 때까지 반복하면서 
+            sequence = s[i:i + 10]
+            if sequence in checked: # seen안에 현재 반복된 길이 10의 substring이 존재한다면  
+                ans.add(sequence) # answ에 추가 
+            checked.add(sequence) #아니면 새로 seen에 추가 
 
-        for i in range(len(s) - 9):
-            seq = s[i:i + 10]
-            if seq in seen:
-                ans.add(seq)
-            seen.add(seq)
-
-        return list(ans)
+        return list(ans) # ans를 list형태로 반환 
         
