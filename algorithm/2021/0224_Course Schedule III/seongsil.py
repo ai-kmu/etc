c38943d3e31b0536bfccc725dad4fac218553fb8
seongsilheo <44438752+seongsilheo@users.noreply.github.com>
2021-03-02 12:37:44 +0900

    Update seongsil.py

--- algorithm/2021/0224_Course Schedule III/seongsil.py
+++ algorithm/2021/0224_Course Schedule III/seongsil.py
@@ -1,8 +1,7 @@
 import heapq
 
 class Solution:
-    def scheduleCourse(self, courses: List[List[int]]) -> int:
-        
+    def scheduleCourse(self, courses: List[List[int]]) -> int:        
         courses.sort(key=lambda k: k[1])  # course를 데드라인 d를 기준으로 sorting
         start = 0
         que = []
@@ -11,4 +10,4 @@ class Solution:
             heapq.heappush(que,-t)  # 젤 오래걸리는 course가 제일 윗쪽으로 갈수 있도록 heap구성
             while start > d: # 시작 day가 d를 이미넘어선 경우, que에서 가장 오래걸리는 course를 먼저 제거
                 start += heapq.heappop(que)
-            
+        return len(que)
