127c66a87d47a73c3f213d76381e86827965f21c
jmin0530 <72537757+jmin0530@users.noreply.github.com>
2021-03-03 19:00:35 +0900

    Update jungmin.py

--- algorithm/2021/0224_Course Schedule III/jungmin.py
+++ algorithm/2021/0224_Course Schedule III/jungmin.py
@@ -12,10 +12,10 @@ class Solution(object):
 
             during = during + day_sort[i][0] # 처음 0부터 코스에 들어갈 때마다 t값을 누적해서 더한다.
 
-        if during <= day_sort[i][1]:
-            count+=1 # 만약에 다음 course를 수행할 수 있으면 카운트한다.
-            if i == len(day_sort)-1:
-                return count # 만약에 courses를 다 수행했다면 courses 길이값 출력
+            if during <= day_sort[i][1]:
+                count+=1 # 만약에 다음 course를 수행할 수 있으면 카운트한다.
+                if i == len(day_sort)-1:
+                    return count # 만약에 courses를 다 수행했다면 courses 길이값 출력
 
-        elif during > day_sort[i][1]:
-            return count # 만약 더이상 course를 수행할 수 없으면 코스 수행한 만큼 count 출력
+            elif during > day_sort[i][1]:
+                return count # 만약 더이상 course를 수행할 수 없으면 코스 수행한 만큼 count 출력
