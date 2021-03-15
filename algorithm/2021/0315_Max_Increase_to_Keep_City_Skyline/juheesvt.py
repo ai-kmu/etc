0f562ea46bce79187bdeee93cb515e335e60635c
Juhee Kang <38813640+juheesvt@users.noreply.github.com>
2021-03-15 16:43:26 +0900

    Update juheesvt.py

--- algorithm/2021/0315_Max_Increase_to_Keep_City_Skyline/juheesvt.py
+++ algorithm/2021/0315_Max_Increase_to_Keep_City_Skyline/juheesvt.py
@@ -7,9 +7,11 @@ class Solution(object):
         
         change = 0
         
+        # 각 행과 열의 최댓값 저장
         row_skyline = [max(h) for h in grid]
         col_skyline = [max(h) for h in zip(*grid)]
         
+        # grid를 하나하나 돌면서, 각 행과 열의 최댓값 중 작은 값을 기준으로 최대로 더할 수 있는 수를 change에다가 
         for i, arr in enumerate(grid):
             for j, h in enumerate(arr):
                 
