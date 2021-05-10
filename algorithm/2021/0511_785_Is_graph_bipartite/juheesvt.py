18e34acc0207ba473ee98fddde407937ab0750ec
Juhee Kang <38813640+juheesvt@users.noreply.github.com>
2021-05-10 17:38:17 +0900

    Update juheesvt.py

--- algorithm/2021/0511_785_Is_graph_bipartite/juheesvt.py
+++ algorithm/2021/0511_785_Is_graph_bipartite/juheesvt.py
@@ -37,3 +37,31 @@ class Solution(object):
                 
         return True
                 
+        
+        
+        
+# Union find
+"""
+class Solution:
+    def isBipartite(self, graph: List[List[int]]) -> bool:
+        def find(i):
+            if roots[i] != i:
+                roots[i] = find(roots[i])
+            return roots[i]
+
+        if not graph:
+            return False
+        roots = [i for i in range(len(graph))]
+        for i in range(len(graph)):
+            if graph[i]:
+                ri = find(i)
+                rj = find(graph[i][0])
+                if ri == rj:
+                    return False
+                for k in range(1, len(graph[i])):
+                    rk = find(graph[i][k])
+                    if ri == rk:
+                        return False
+                    roots[graph[i][k]] = rj
+        return True
+"""
