606d195a58569204becc998a26ad8cd1cb4a0dcf
LeeMyungHak <mh9716@naver.com>
2021-05-17 10:58:49 +0900

    Create myunghak.py

---
+++ algorithm/2021/0517_735_Asteroid_Collision/myunghak.py
@@ -0,0 +1,16 @@
+class Solution:
+    def asteroidCollision(self, asteroids: List[int]) -> List[int]:
+        ans = [asteroids[0]]
+        for i in range(1, len(asteroids)):
+            dst = False # 현재 소행성이 파괴되었는지 기록
+            while(len(ans) > 0 and ans[-1] >0 and asteroids[i] < 0):
+                if(ans[-1]<-asteroids[i]):
+                    ans.pop() # 파괴되지 않고 계속 진행
+                else:
+                    if(ans[-1]==-asteroids[i]):
+                        ans.pop()
+                    dst=True # 파괴됨
+                    break
+            if not dst:
+                ans.append(asteroids[i])
+        return ans
