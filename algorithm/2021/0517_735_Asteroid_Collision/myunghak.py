21d99b0398a6c849c97e75c89d73dfabe045802e
LeeMyungHak <mh9716@naver.com>
2021-05-17 11:15:48 +0900

    Update myunghak.py

--- algorithm/2021/0517_735_Asteroid_Collision/myunghak.py
+++ algorithm/2021/0517_735_Asteroid_Collision/myunghak.py
@@ -5,7 +5,7 @@ class Solution:
             dst = False # 현재 소행성이 파괴되었는지 기록
             while(len(ans) > 0 and ans[-1] >0 and asteroids[i] < 0):
                 if(ans[-1]<-asteroids[i]):
-                    ans.pop() # 파괴되지 않고 계속 진행
+                    ans.pop() # ans에 있는 소행성 파괴, 현재 소행성 파괴되지 않고 계속 진행
                 else:
                     if(ans[-1]==-asteroids[i]):
                         ans.pop()
