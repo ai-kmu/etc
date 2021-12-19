116e811dbff39b11e2fc0fbf1330097909907f1a
Daehee Kim <frshield2@kookmin.ac.kr>
2021-12-19 22:30:39 +0900

    Update daehee.py

--- algorithm/2021/1220_44_Wildcard_Matching/daehee.py
+++ algorithm/2021/1220_44_Wildcard_Matching/daehee.py
@@ -2,14 +2,14 @@ class Solution:
     def isMatch(self, s: str, p: str) -> bool:
         length = len(s)
         
-        dp = [True] + [False] * length
-        for i in p:
-            if i != '*':
+        dp = [True] + [False] * length      # dp 초기화
+        for i in p:                         # p string 패턴 검사 시작
+            if i != '*':                    # * 아닌 경우
                 for n in reversed(range(length)):
                     dp[n+1] = dp[n] and (i == s[n] or i == '?')
-            else:
+            else:                           # *인 경우
                 for n in range(1, length+1):
                     dp[n] = dp[n-1] or dp[n]
-            dp[0] = dp[0] and i == '*'
+            dp[0] = dp[0] and i == '*'     
         return dp[-1]
         
