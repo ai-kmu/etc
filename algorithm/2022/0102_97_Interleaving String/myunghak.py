eb228d980d300cec04fe21991f5dfb51d8519a5c
Kim Sungsik <sskim0126@kookmin.ac.kr>
2022-01-04 16:12:03 +0900

    이명학님 코드 리뷰

--- algorithm/2022/0102_97_Interleaving String/myunghak.py
+++ algorithm/2022/0102_97_Interleaving String/myunghak.py
@@ -27,6 +27,15 @@ class Solution:
         # 새로 추가되는애가 남은것의 첫번째와 같으면 True 아니면 False
         for i in range(1, len(s1) + 1):
             for j in range(1, len(s2) + 1):
-                dp[i][j] += (dp[i - 1][j] and s1[i - 1] == s3[i + j - 1])
-                dp[i][j] += (dp[i][j - 1] and s2[j - 1] == s3[i + j - 1])
-        return dp[-1][-1] > 0
+                # dp[i][j] += (dp[i - 1][j] and s1[i - 1] == s3[i + j - 1])
+                # dp[i][j] += (dp[i][j - 1] and s2[j - 1] == s3[i + j - 1])
+                '''
+                    코드 수정 by 김성식
+                    matrix를 boolean value로 초기화한 다음 이를 integer로 변환한 후 덧셈을 실행
+                    두가지 문제점
+                    1. implicit type casting 반복
+                    2. 첫번째 행과 열은 boolean, 그 외는 integer => 불균형 발생, python이기에 가능한 코드
+                '''
+                dp[i][j] |= (dp[i - 1][j] and s1[i - 1] == s3[i + j - 1])
+                dp[i][j] |= (dp[i][j - 1] and s2[j - 1] == s3[i + j - 1])
+        return dp[-1][-1]
