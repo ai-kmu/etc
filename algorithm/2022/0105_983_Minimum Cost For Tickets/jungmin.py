45b60cdaa09012db8bc8f0de4717e6cbe2139f50
jmin0530 <72537757+jmin0530@users.noreply.github.com>
2022-01-10 10:39:04 +0900

    Update jungmin.py

--- algorithm/2022/0105_983_Minimum Cost For Tickets/jungmin.py
+++ algorithm/2022/0105_983_Minimum Cost For Tickets/jungmin.py
@@ -1,11 +1,17 @@
 class Solution:
     def mincostTickets(self, days: List[int], costs: List[int]) -> int:
-        dp = [0] * (days[-1]+1)
+        # dynamic programming 방식으로 문제 풀이
+        
+        dp = [0] * (days[-1]+1) # dp배열의 길이: 마지막으로 여행해야 하는 날짜 + 1
 
         for i,v in enumerate(dp):
+            # dp배열의 0번째 인덱스는 0으로 시작
             if i == 0:
                 dp[i] = 0
-                
+            
+            # i가 days에서 여행해야 하는 날이면 경비 계산
+            # 여행해야 하는 날은 아래 3개 값 중 최소값을 선택
+            # dp[i-1]+cost[0], dp[i-7]+cost[1], dp[i-30]+cost[2]
             elif i in days:
                 a = costs[2]
                 b = costs[1]
@@ -19,8 +25,9 @@ class Solution:
                     c = dp[i-1] + costs[0]
                     
                 dp[i] = min(a,b,c)
-                
+            
+            # 여행 안하는 날에는 이전 인덱스에서의 dp값 저장
             else:
                 dp[i] = dp[i-1]
-        return dp[-1]
+        return dp[-1] # 최종 답은 dp의 마지막 인덱스에서의 dp값을 출력
         
