3b73d9721576750875d34784d4b47bd921a73342
Kim Sungsik <sskim0126@kookmin.ac.kr>
2022-01-16 13:20:57 +0900

    Create sungsik.py

---
+++ algorithm/2022/0112_54_Spiral Matrix/sungsik.py
@@ -0,0 +1,37 @@
+import numpy as np
+
+
+class Solution:
+    def spiralOrder(self, matrix: List[List[int]]) -> List[int]:
+        # slice해야 하는 edge의 위치를 나타내는 변수
+        # 0: 위쪽, 1: 오른쪽, 2: 아래쪽, 3: 왼쪽
+        direction = 0
+        # column의 slicing을 위해 numpy를 사용
+        np_matrix = np.array(matrix)
+        result = []
+        
+        while np_matrix.size != 0:
+            # 위쪽 edge
+            if direction == 0:
+                edge = list(np_matrix[0,:])
+                np_matrix = np_matrix[1:,:]
+            
+            # 오른쪽 edge
+            elif direction == 1:
+                edge = list(np_matrix[:,-1])
+                np_matrix = np_matrix[:,:-1]
+            
+            # 아래쪽 edge => 순서를 반대로 저장해야 하기 때문에 np.flip을 사용
+            elif direction == 2:
+                edge = list(np.flip(np_matrix[-1,:]))
+                np_matrix = np_matrix[:-1,:]
+            
+            # 왼쪽 edge => 순서를 반대로 저장해야 하기 때문에 np.flip을 사용
+            elif direction == 3:
+                edge = list(np.flip(np_matrix[:,0]))
+                np_matrix = np_matrix[:,1:]
+            
+            direction = (direction + 1) % 4
+            result += edge
+        
+        return result
