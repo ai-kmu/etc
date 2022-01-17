b8ffb20d2f59b94007d5bd005b71420cdd5303e6
LeeMyungHak <mh9716@naver.com>
2022-01-17 11:34:54 +0900

    Update myunghak.py

--- algorithm/2022/0112_54_Spiral Matrix/myunghak.py
+++ algorithm/2022/0112_54_Spiral Matrix/myunghak.py
@@ -2,21 +2,27 @@ class Solution:
     def spiralOrder(self, matrix):
         top, bottom = 0, len(matrix)-1
         left, right = 0, len(matrix[0])-1
-        ans = []
+        ans = [0 for _ in range(len(matrix) * len(matrix[0]))]
         
         idx = 0
-        while (len(ans) < len(matrix) * len(matrix[0])):
+        size = 0
+        while (size < len(matrix) * len(matrix[0])):
+            print(ans)
             if idx % 4 ==0:
-                ans += matrix[top][left:right+1]
+                ans[size:size-left+right+1] = matrix[top][left:right+1]
+                size += right+1 - left
                 top+=1
             elif idx % 4 ==1:
-                ans += [d[right] for d in matrix[top:bottom+1]]
+                ans[size:size+bottom-top+1] = [d[right] for d in matrix[top:bottom+1]]
+                size += bottom+1 - top
                 right-=1
             elif idx % 4 ==2:
-                ans += matrix[bottom][left:right+1][::-1]
+                ans[size:size-left+right+1] = matrix[bottom][left:right+1][::-1]
+                size += right+1 - left
                 bottom-=1
             elif idx % 4 ==3:
-                ans += [d[left] for d in matrix[top:bottom+1]][::-1]
+                ans[size:size+bottom-top+1] = [d[left] for d in matrix[top:bottom+1]][::-1]
+                size += bottom+1 - top
                 left += 1
             idx += 1
         return ans
