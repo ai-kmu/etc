ea4f403d1c98eea1cce990a25b7cc706fcd32a00
Kang4882 <85673206+Kang4882@users.noreply.github.com>
2022-01-22 23:59:06 +0900

    Update Minjun.py

--- algorithm/2022/0120_230. Kth Smallest Element in a BST/Minjun.py
+++ algorithm/2022/0120_230. Kth Smallest Element in a BST/Minjun.py
@@ -9,15 +9,20 @@ class Solution:
         result = []
         
         #  순회를 했을때 정렬이 되도록, 중위순회를 사용하였다.
+        #  재귀를 사용하였다.
         
         def traversal(root):
             
+            #  빈 경우 패스
             if root is None:
                 pass
             
             else:
+                #  왼쪽 탐색
                 traversal(root.left)
+                #  값 
                 result.append(root.val)
+                #  오른쪽 탐색
                 traversal(root.right)
             
         traversal(root)
