dc3accf082658836f4831ad047f407bfa5adbfaf
Lhyejin <hub0913@kookmin.ac.kr>
2022-01-23 13:32:06 +0900

    Update hyejin.py

--- algorithm/2022/0120_230. Kth Smallest Element in a BST/hyejin.py
+++ algorithm/2022/0120_230. Kth Smallest Element in a BST/hyejin.py
@@ -12,6 +12,7 @@ class Solution:
         stack = deque([])
         inorder_arr = []
         curr = root
+        cnt = 0
         while curr is not None or stack:
             # 왼쪽 계속 넣어주고
             while curr is not None:
@@ -19,10 +20,14 @@ class Solution:
                 curr = curr.left
             # curr를 맨 left에서부터 시작
             curr = stack.pop()
+            cnt += 1
             # 값 넣어주기
             inorder_arr.append(curr.val)
+            if k == cnt: # k번째 되면 끝내기
+                break
             # 본인의 right subtree 탐색
             curr = curr.right
         
-        return inorder_arr[k-1]
+        return inorder_arr[-1]
+        
         
