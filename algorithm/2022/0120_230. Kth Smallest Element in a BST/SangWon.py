1086b03dde51ab88ab77047b5169bb2901b93073
tkddnjs98 <89904421+tkddnjs98@users.noreply.github.com>
2022-01-24 19:33:59 +0900

    Update SangWon.py

--- algorithm/2022/0120_230. Kth Smallest Element in a BST/SangWon.py
+++ algorithm/2022/0120_230. Kth Smallest Element in a BST/SangWon.py
@@ -10,11 +10,11 @@ class Solution:
         
         while True:
             while root:
-                stack.append(root)
-                root = root.left
-            root = stack.pop()
-            k -= 1
-            if not k:
+                stack.append(root) #root를 stack에 넣으면서 왼쪽우선으로 이동 
+                root = root.left 
+            root = stack.pop() #왼쪽에 아무것도 없을 때 pop()을 수행, k가 카운트인데 카운트를 하나씩 줄임
+            k -= 1 
+            if not k:                 #k가 0일때 root의 값을 반환 
                 return root.val
-            root = root.right
+            root = root.right         #pop을 했는데도 k가 아직 0이 아니면 오른쪽을 보기 
         
