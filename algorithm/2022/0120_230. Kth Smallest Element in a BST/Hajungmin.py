a250efb802f4880af1ca81d70bb5e57469650e61
Kang4882 <85673206+Kang4882@users.noreply.github.com>
2022-01-24 20:39:12 +0900

    Update Hajungmin.py

--- algorithm/2022/0120_230. Kth Smallest Element in a BST/Hajungmin.py
+++ algorithm/2022/0120_230. Kth Smallest Element in a BST/Hajungmin.py
@@ -6,7 +6,7 @@
 #  원소를 다 추가한 뒤, return answer[k-1]을 이용하여 다 탐색한 뒤에 k번째 작은 수를 호출하였다. -- 1
 
 #  1) 중간에 count를 따로 생성해서 노드를 방문해서 원소를 append를 할 때마다 count를 +1을 하게 만든다. 
-#  -> count가 k와 같으면 정답을 출력하는 방식으로 생각했으면 좋겠습니다.
+#  -> count를 이용하여 정답을 출력하는 방식도 생각해보면 좋을 것 같습니다.
 
 class Solution:
     def kthSmallest(self, root: Optional[TreeNode], k: int) -> int:
