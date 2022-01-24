885bd33845bf563309a445dda4d54a6975ad2ed1
tkddnjs98 <89904421+tkddnjs98@users.noreply.github.com>
2022-01-24 16:57:00 +0900

    Create Hajungmin.py

--- algorithm/2022/0120_230. Kth Smallest Element in a BST/Hajungmin.py
+++ algorithm/2022/0120_230. Kth Smallest Element in a BST/Hajungmin.py
@@ -15,6 +15,7 @@ class Solution:
         BFS(root)
         # k번째 작은 노드를 반환해야하기 때문에 k-1인덱스를 return해준다.
         return answer[k-1]
-
-   #하나씩 방문하여 append를 하는 방식으로 취했다. 1
-   #return answer[k-1]을 이용하여 다 탐색한 뒤에 k번째 작은 수를 호출하였다. 중간에 count를 따로 생성해서 count가 k와 같으면 break를 하는 방식을 고려하면 좋겠다. 1
+   #중위 순위 구현
+   #하나씩 방문하여 append를 하는 방식으로 취했다. 
+   #원소를 다 추가한 뒤, return answer[k-1]을 이용하여 다 탐색한 뒤에 k번째 작은 수를 호출하였다. 1
+   #중간에 count를 따로 생성해서 노드를 방문해서 원소를 append를 할 때마다 count를 +1을 하게 만든다. count가 k와 같으면 정답을 출력하는 방식으로 생각했으면 좋겠다. 1
