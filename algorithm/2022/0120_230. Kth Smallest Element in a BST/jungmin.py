5461a9b7c1287019e987bb320f67c9545b07e5d4
Kang4882 <85673206+Kang4882@users.noreply.github.com>
2022-01-24 20:42:17 +0900

    Update jungmin.py

--- algorithm/2022/0120_230. Kth Smallest Element in a BST/jungmin.py
+++ algorithm/2022/0120_230. Kth Smallest Element in a BST/jungmin.py
@@ -1,3 +1,13 @@
+#  출제자 comment
+
+#  중위 순회 구현
+
+#  하나씩 방문하여 append를 하는 방식으로 취했다. 
+#  원소를 다 추가한 뒤, return answer[k-1]을 이용하여 다 탐색한 뒤에 k번째 작은 수를 호출하였다. -- 1
+
+#  1) 중간에 count를 따로 생성해서 노드를 방문해서 원소를 append를 할 때마다 count를 +1을 하게 만든다. 
+#  -> count를 이용하여 정답을 출력하는 방식도 생각해보면 좋을 것 같습니다.
+
 # Definition for a binary tree node.
 # class TreeNode:
 #     def __init__(self, val=0, left=None, right=None):
