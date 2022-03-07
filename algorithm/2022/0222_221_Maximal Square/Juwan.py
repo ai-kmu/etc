fc351f7ff4cf3501f5bc574063a028d442da166e
minsik1349 <97005576+minsik1349@users.noreply.github.com>
2022-03-07 16:59:02 +0900

    Update Juwan.py

--- algorithm/2022/0222_221_Maximal Square/Juwan.py
+++ algorithm/2022/0222_221_Maximal Square/Juwan.py
@@ -5,6 +5,20 @@ class Solution:
         
         w = len(matrix[0])
         
+            ''' 
+            (feedback)
+    
+            dp matrix를 0으로 초기화한 이후 => 이중 for문 돌려서 matrix 값을 할당했는데 (이중 for문 한번 + for 문 한번)
+            list comprehension이나 map 함수 등을 이용해서 이중 for문 내에서 같이 str type => int type으로 변경하는 방법
+
+            혹은 이중 for문을 이용할거면 다음과 같은 코드로 한번에 변경하는 것이 조금 더 효율적이지 않을까 싶습니다.
+            for i in range(h):
+                dp.append([])
+                for j in range(w):
+                    dp[i].append(int(matrix[i][j]))
+                    
+             ''' 
+        
         dp = [[0]*w for _ in range(h)]
 
         res = 0
