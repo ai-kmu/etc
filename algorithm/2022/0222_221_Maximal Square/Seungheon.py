4ac232523e14e2e53b2607dc61e88afac67653bf
minsik1349 <97005576+minsik1349@users.noreply.github.com>
2022-03-07 18:10:29 +0900

    Update Seungheon.py

--- algorithm/2022/0222_221_Maximal Square/Seungheon.py
+++ algorithm/2022/0222_221_Maximal Square/Seungheon.py
@@ -11,7 +11,13 @@ class Solution(object):
         for row in matrix :
             if row[0] == '1' :
                 answer = 1      
-    
+        
+        ''' 
+        (feedback)
+        행벡터 혹은 열벡터 일 경우 정사각형의 최대 값은 1이고 앞서 첫 행과 열의 존재 여부를 확인해서 바로 return 하는 것은 좋은 생각 / 
+        코드가 없어도 돌아가는데 문제가 없어서 생략해도 무방
+        '''
+        
         # 1행 n 열짜리 matrix면 answer return
         if len(matrix) == 1 :
             return answer
@@ -24,6 +30,11 @@ class Solution(object):
         # 왼쪽, 왼쪽위, 위쪽 을보고 현재 위치에서 만들 수 있는 최대box_size update
         for row in range(1, len(matrix)):
             for col in range(1, len(matrix[0])):
+                '''
+                (feedback) 잘 짜셧는데..
+                현재 위치의 사각형이 존재하는 것과 상관없이 주변 사각형의 존재를 여부를 탐색하고 현재 위치의 정사각형이 존재할 경우(1) 가질 수 있는 최대 사각형을 계산해서
+                존재할 경우 이를 업데이틑 하는 방식 => 현재 위치에서 사각형이 존재하는 여부와 상관없이 연산을 진행해야 하
+                '''
                 up = int(matrix[row - 1][col])
                 left_up = int(matrix[row - 1][col - 1])
                 left = int(matrix[row][col - 1])
