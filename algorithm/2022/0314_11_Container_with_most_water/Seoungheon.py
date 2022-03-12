18789efc3083ac782f6497fb92b9b79f027aecd7
song1248 <84365977+song1248@users.noreply.github.com>
2022-03-12 23:30:15 +0900

    Update Seoungheon.py

--- algorithm/2022/0314_11_Container_with_most_water/Seoungheon.py
+++ algorithm/2022/0314_11_Container_with_most_water/Seoungheon.py
@@ -3,14 +3,14 @@ class Solution(object):
 
         # 왼쪽과 오른쪽의 값을 구해서 현재 구할수 있는 최대크기를 구하고 height가 작은쪽을 한칸씩 이동하며 최대크기구하는것을 반복한다.
         
-        left_i = 0
-        right_i = len(height)
+        left = 0
+        right = len(height)
         max_size = 0
         cur_weight = len(height)
         
-        while True :
-            left_height = height[left_i]
-            right_height = height[right_i-1]
+        while left != right :
+            left_height = height[left]
+            right_height = height[right-1]
             
             # 최대크기 계산
             cur_height = min(left_height, right_height)
@@ -19,12 +19,8 @@ class Solution(object):
             
             # height 가 작은 쪽을 한칸 이동
             if left_height > right_height :
-                right_i -= 1
+                right -= 1
             else :
-                left_i += 1
-            
-            # 두 값이 같아지면 탈출
-            if left_i == right_i :
-                break
-            
+                left += 1
+              
         return max_size
