247a675b20ad4a7bc0b9c91761b764eb11206e9c
jongssoo <56548207+jongssoo@users.noreply.github.com>
2022-03-12 22:26:11 +0900

    Update Jongsoo.py

--- algorithm/2022/0314_11_Container_with_most_water/Jongsoo.py
+++ algorithm/2022/0314_11_Container_with_most_water/Jongsoo.py
@@ -8,6 +8,7 @@ class Solution:
         #양쪽의 숫자중에서 작은 숫자를 길이와 곱해줌
         #양쪽의 숫자중에서 작은 숫자가 곱해지기 때문에 작은 숫자의 포인터를 옮김
         #모든 과정에서 max값을 사용하여 최대값을 갱신
+        #최대 길이부터 사용하고 양쪽의 숫자 중에서 작은 숫자를 바꾸기 때문에 그 과정에서 가질 수 있는 최대값을 가질 수 있겠다고 생각했음
         while start < end:
             if height[start] <= height[end]:
                 answer = max(answer,height[start]*(end-start))
