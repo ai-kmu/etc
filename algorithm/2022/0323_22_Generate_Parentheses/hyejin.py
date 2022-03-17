5f105692770460d41e30f3b954d2ef77c7392b4e
Lhyejin <hub0913@kookmin.ac.kr>
2022-03-17 14:30:18 +0900

    Create hyejin.py

---
+++ algorithm/2022/0323_22_Generate_Parentheses/hyejin.py
@@ -0,0 +1,16 @@
+class Solution:
+    def generateParenthesis(self, n: int) -> List[str]:
+        answer = []
+        
+        # 재귀 함수
+        def recursive(curr_s, num_of_front, num_of_back):
+            if num_of_front == n and num_of_back == n: # (과 )의 개수가 꽉찼을 떄
+                return answer.append(curr_s)
+            else:
+                if num_of_front < n: # "("를 추가할 때는 n개에 도달할 때까지
+                    recursive(curr_s+"(", num_of_front+1, num_of_back)
+                if num_of_front > num_of_back: # ")"를 추가할 때는 "("가 있을 때
+                    recursive(curr_s+")", num_of_front, num_of_back+1)
+        recursive("", 0, 0)
+        
+        return answer
