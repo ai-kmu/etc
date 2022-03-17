c2dae36646292eb7e17d4bb1b45f38ec68130793
Lhyejin <hub0913@kookmin.ac.kr>
2022-03-17 16:17:20 +0900

    Update hyejin.py

--- algorithm/2022/0323_22_Generate_Parentheses/hyejin.py
+++ algorithm/2022/0323_22_Generate_Parentheses/hyejin.py
@@ -2,6 +2,9 @@ class Solution:
     def generateParenthesis(self, n: int) -> List[str]:
         answer = []
         
+        # 안좋은 방법
+        # n으로 만들 수 있는 모든 pair를 만든 뒤에, 그 string이 유효한지 검사하는 방법
+        
         # 재귀 함수
         def recursive(curr_s, num_of_front, num_of_back):
             if num_of_front == n and num_of_back == n: # (과 )의 개수가 꽉찼을 떄
