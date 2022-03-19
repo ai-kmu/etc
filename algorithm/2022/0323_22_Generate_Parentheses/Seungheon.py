359073e1360f0cad0f0770406929cab9903147b9
song1248 <84365977+song1248@users.noreply.github.com>
2022-03-20 00:56:29 +0900

    Update Seungheon.py

--- algorithm/2022/0323_22_Generate_Parentheses/Seungheon.py
+++ algorithm/2022/0323_22_Generate_Parentheses/Seungheon.py
@@ -1,7 +1,7 @@
 class Solution(object):
     def generateParenthesis(self, n):
         
-        # 가운대에 )) , (( , )( , () 을 추가시키는 방식
+        # 가운데 )) , (( , )( , () 를 추가하는 방식
         
         # [모양,왼쪽 '(' 개수, 오른쪽 ")" 개수]
         
@@ -13,25 +13,18 @@ class Solution(object):
             
             for answer in answers:
                 
-                # 가운대 )) , (( , )( , () 를 넣었을때, 완전한 모양을 만들 수 있으면 (pair가 없이 바깥을 향하는것이 안되도록) 
+                # 가운데 )) , (( , )( , () 를 넣었을때, 완전한 모양을 만들 수 있으면 (pair가 없이 바깥을 향하는것이 안되도록) 
                 # 마지막이 아니거나 마지막 괄호를 넣을때 완전한 모양이 되면 '(' 의 개수와 ')' 의 개수가 동일 하면
                 
-                if (answer[1] + 1 <= (i+1)//2 and answer[2] <= (i+1)//2) and (i+1 != n or (answer[1] + 1) + (n - answer[2]) == n): 
-                    temp_answer.append([answer[0][:i] + "))" + answer[0][-i:], answer[1] + 1, answer[2]])
-                        
-                if (answer[1] <= (i+1)//2 and answer[2] + 1 <= (i+1)//2) and (i+1 != n or answer[1] + n - (answer[2] + 1) == n):
-                    temp_answer.append([answer[0][:i] + "((" + answer[0][-i:], answer[1], answer[2] + 1])
-                        
-                if (answer[1] + 1 <= (i+1)//2 and answer[2] + 1 <= (i+1)//2) and (i+1 != n or (answer[1] + 1) + n - (answer[2] + 1) == n):
-                    temp_answer.append([answer[0][:i] + ")(" + answer[0][-i:], answer[1] + 1, answer[2] + 1])
-                
-                if i+1 != n or answer[1] + n - answer[2] == n:
-                    temp_answer.append([answer[0][:i] + "()" + answer[0][-i:], answer[1], answer[2]])
+                for left, right, shape in zip([1, 0, 1, 0],[0, 1, 1, 0],["))", "((", ")(", "()"]):
+                    if (answer[1]+left <= (i+1)//2 and answer[2]+right <= (i+1)//2) and (i+1 != n or (answer[1]+left) + n-(answer[2]+right) == n):
+                        # 마지막 추가시에는 shape만 answers에 저장
+                        if len(answer[0])+2 == 2*n:
+                            temp_answer.append(answer[0][:i]+shape+answer[0][-i:])
+                        else: 
+                            temp_answer.append([answer[0][:i]+shape+answer[0][-i:], answer[1]+left, answer[2]+right])
     
             answers = temp_answer
-        
-        final_answer = []
-        for j in range(len(answers)):
-            final_answer.append(answers[j][0])
-            
-        return final_answer
+        if n == 1:
+            answers = ["()"]
+        return answers
