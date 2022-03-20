7a2f9029f7c14b02ff20eea9bbd4c7cc450a11a7
daisophila <70212844+daisophila@users.noreply.github.com>
2022-03-20 22:23:42 +0900

    Update Jongchan.py

--- algorithm/2022/0323_22_Generate_Parentheses/Jongchan.py
+++ algorithm/2022/0323_22_Generate_Parentheses/Jongchan.py
@@ -1,8 +1,8 @@
 class Solution:
     def generateParenthesis(self, n: int) -> List[str]:
-        
-        def search_correct(pare):
-            stk=0
+        # 가능한 모든 괄호를 만들고, 그중에서 올바른 괄호쌍만 골라낸다.
+        def search_correct(pare): # 올바른 괄호 찾기
+            stk=0 # stack을 이용해 stack에 (의 갯수가 0보다 작아지면 틀리다고 판단
             res=[]
             for ps in pare:
                 for p in ps:
@@ -22,9 +22,9 @@ class Solution:
         
         parentheses=[]
         
-        for b in range(2**(2*n-1),2**(2*n)):
+        for b in range(2**(2*n-1),2**(2*n)): # n쌍의 크기를 가지는 괄호 만들기
             par=''
-            b_str=format(b,'b')
+            b_str=format(b,'b') # 숫자를 이진화시켜 1이면 (, 0이면 ) 로 치환
             for s in b_str:
                 if s=='1':
                     par+='('
