0e85865f06efb09129b813a7d8f8a407585a20b8
song1248 <84365977+song1248@users.noreply.github.com>
2022-03-27 15:54:18 +0900

    Update Seungheon.py

--- algorithm/2022/0329_1054_Distant_Barcodes/Seungheon.py
+++ algorithm/2022/0329_1054_Distant_Barcodes/Seungheon.py
@@ -9,7 +9,7 @@ class Solution(object):
     
         barcode_inorder = Counter(barcodes).most_common()
         
-        # 2칸씩 띄어가며 큰수부터 answer에 추가
+        # 2칸씩 띄어가며 수(count)가 많은 수(num)부터 answer에 추가
         # 범위를 넘어서면 처음부터 값 추가
         # 이미 채워진 칸을 만나면 cur_point에 +1 하여 진행
         
