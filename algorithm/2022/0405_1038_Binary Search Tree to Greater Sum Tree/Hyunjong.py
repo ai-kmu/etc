518e3a4803aeba367ca91fe9d75876a1db5e7827
lhj-3 <77227377+lhj-3@users.noreply.github.com>
2022-04-03 21:09:37 +0900

    Update Hyunjong.py

--- algorithm/2022/0405_1038_Binary Search Tree to Greater Sum Tree/Hyunjong.py
+++ algorithm/2022/0405_1038_Binary Search Tree to Greater Sum Tree/Hyunjong.py
@@ -4,7 +4,7 @@
 #         self.val = val
 #         self.left = left
 #         self.right = right
-# 중위 순회 역순(left -> root -> right)으로 조회하면서 조회하는 노드의 값을 acc에 누적시킴
+# 중위 순회 역순(right -> root -> left)으로 조회하면서 조회하는 노드의 값을 acc에 누적시킴
 # input : root = [4,1,6,0,2,5,7,null,null,null,3,null,null,null,8]
 # acc 값 누적 예시 : 8 -> 15 -> 26 -> 26 -> 33 -> 35 ->36 ->36 ->36 
 # 
