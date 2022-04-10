5d6753507887f93f61cd3ad9bf4f419fc71783ba
Kang4882 <85673206+Kang4882@users.noreply.github.com>
2022-04-11 04:15:22 +0900

    Update Minjun.py

--- algorithm/2022/0412_787_Cheapest_Flights_Within_K_Stops/Minjun.py
+++ algorithm/2022/0412_787_Cheapest_Flights_Within_K_Stops/Minjun.py
@@ -2,54 +2,50 @@ import copy
 
 class Solution:
     def findCheapestPrice(self, n: int, flights: List[List[int]], src: int, dst: int, k: int) -> int:
+
+        # 참고용 배열 만들기
+        prev = [float("inf")] * n
         
-        if src == dst:
-            return 0;
-        
-        prev = [float("inf")]*n
-        
+        # 스타팅 포인트 지정
         prev[src] = 0
         
         
         for i in range(1, k+2):
+            # 비교용
+            now = copy.deepcopy(prev)
             
-            now = []*n
-            
-            for j in range(n):
-                
-                now[j] = prev[j]
-                
-            
+            # 플래그
             changed = False
             
             for flight in flights:
                 
-                x = flight[0]
-                y = flight[1]
-                z = flight[2]
+                # src
+                start = flight[0]
+                # dst
+                dest = flight[1]
+                # cost
+                cost = flight[2]
                 
-                if prev[x] != float("inf") and prev[x] + z < now[y]:
+                if prev[start] != float("inf") and prev[start]+ cost < now[dest]:
                     
-                    now[y] = prev[x] + z
+                    now[dest] = prev[start] + cost
                     changed = True
-                    
+            
+            # 갱신 실패하면 멈춘다
             if not changed:
                 break
             
+            # 계산한 값 옮겨 담기
             prev = copy.deepcopy(now)
         
         ans = 0
         
-        if prev[dst] == float("inf"):
+        # 갱신 성공 - 가능했던 루트면 반환
+        if prev[dst] != float("inf"):
             ans = prev[dst]
-            
+        
+        # inf면 불가능 루트 -1 반환
         else:
-            
-            ans = prev[dst]
+            ans = -1
             
         return ans
-    
-    
-    
-    
-    
