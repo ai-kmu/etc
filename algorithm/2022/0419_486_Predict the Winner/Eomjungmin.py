fc5c8119168363b2e2804422ab15212bccfc9c05
jmin0530 <72537757+jmin0530@users.noreply.github.com>
2022-04-15 16:34:05 +0900

    Create Eomjungmin.py

---
+++ algorithm/2022/0419_486_Predict the Winner/Eomjungmin.py
@@ -0,0 +1,13 @@
+class Solution:
+    def PredictTheWinner(self, nums: List[int]) -> bool:
+        # 2-D Dynamic programming
+        dp = [[0 for _ in range(len(nums))] for _ in range(len(nums))] # 2차원 dp 선언
+        
+        # dp로 풀 때의 작은 부분: 맨 오른쪽에서 부분으로 취했을 때 player1이 선택 할 수 있는 최대 effective score
+        for i in range(len(nums)-1,-1,-1): # 행은 역순으로 진행
+            for j in range(i, len(nums)): # 열은 현재 행 숫자부터 마지막 열까지 진행
+                if  i==j: # 대각 요소는 nums값 대입
+                    dp[i][j] = nums[i]
+                else: # 대각 요소가 아닌 곳은 player1이 선택할 수 있는 최대 effective score 계산한 결과 대입
+                    dp[i][j] = max(nums[i] - dp[i+1][j], nums[j] - dp[i][j-1])
+        return dp[0][len(nums)-1] >= 0
