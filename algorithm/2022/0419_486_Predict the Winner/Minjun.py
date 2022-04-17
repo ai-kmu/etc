c36255cf64a45ae7336485f6e47fb884e28edcc6
Kang4882 <85673206+Kang4882@users.noreply.github.com>
2022-04-17 23:56:35 +0900

    Create Minjun.py

---
+++ algorithm/2022/0419_486_Predict the Winner/Minjun.py
@@ -0,0 +1,10 @@
+'''
+1번 선수의 점수가 2번 선수보다 크거나 같도록 할 수 있는가? 문제
+dp 문제
+1번 선수와 2번 선수의 총합 차이 비교
+1번 선수 총합이 2번 선수 총합보다 크거나 같을 수 있나?
+'''
+
+class Solution:
+    def PredictTheWinner(self, nums: List[int]) -> bool:
+        
