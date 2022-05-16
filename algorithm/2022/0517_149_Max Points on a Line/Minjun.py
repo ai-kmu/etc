59a64d84f188a8b9ce067a64f0fdfa504a06ed80
Kang4882 <85673206+Kang4882@users.noreply.github.com>
2022-05-17 01:11:51 +0900

    Update Minjun.py

--- algorithm/2022/0517_149_Max Points on a Line/Minjun.py
+++ algorithm/2022/0517_149_Max Points on a Line/Minjun.py
@@ -1,9 +1,12 @@
+# 통과못한 풀이
+# 한 점에서 모든 점에 대한 기울기를 구하고자 합니다
 class Solution:
     def maxPoints(self, points: List[List[int]]) -> int:
         
         gx = []
         gy = []
         gxy = []
+        
         for i in range(len(points)-1):
             dx = points[i+1][0] - points[i][0]
             dy = points[i+1][1] - points[i][1]
