6a33a54861357b9d49ff94d927ae9f4751f68bea
tkddnjs98 <89904421+tkddnjs98@users.noreply.github.com>
2022-05-23 00:10:35 +0900

    Update Sangwon.py

--- algorithm/2022/0524_1487_Making File Names Unique/Sangwon.py
+++ algorithm/2022/0524_1487_Making File Names Unique/Sangwon.py
@@ -1,13 +1,20 @@
 class Solution:
     def getFolderNames(self, names: List[str]) -> List[str]:
-        file = {}
-        for name in names:
-            if name not in file:
-                file[name] = 1
-            k = 0;
-            
-            while file[name] not in file:
-                k += 1
-                file[name] = file[name].keys() + '(' + str(k) + ')'
-        
-        return file.keys()
+        used = {}
+        output = []
+        for i in names:
+            if i not in used:
+                used[i] = 1
+                output.append(i)
+            else:
+                k = used[i]
+                used[i] += 1
+                new = i + "(" + str(k) + ")"
+                while new in used:
+                    k += 1
+                    new = i + "(" + str(k) + ")"
+                    used[new] = 1
+                    output.append(new)
+        return(output)
+    
+#메모리 초과
