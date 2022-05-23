5c09ca4d0918dd9ba88839e426e34f3349dc5c48
김민식 <97005576+minsik1349@users.noreply.github.com>
2022-05-23 09:21:51 +0900

    Update Juwan.py

--- algorithm/2022/0524_1487_Making File Names Unique/Juwan.py
+++ algorithm/2022/0524_1487_Making File Names Unique/Juwan.py
@@ -1,22 +1,24 @@
+'''
+[feedback]
+dictionary에 지금까지 등장한 중복된 이름을 value로 갱신하면 Time Error 해결 가능(한줄)
+'''
+
 class Solution:
     def getFolderNames(self, names: List[str]) -> List[str]:
-        
-        
         games = defaultdict(lambda : 0) # 딕셔너리 생성
+        
         for i in names:
-
             if games[i] == 0: # 만약 딕셔너리에 없다면
                 games[i] += 1 # 딕셔너리에 추가
 
             else: # 만약 있으면
-
                 cnt = games[i]
 
                 while(games[f'{i}({cnt})']): # 이미 생성한 폴더가 있는 지 확인. 없으면 while 탈출
                     cnt += 1
                 s = f'{i}({cnt})'
                 games[s] += 1
-
+                games[i] += 1
 
         return games.keys()
       
