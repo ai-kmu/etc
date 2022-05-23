5ed1bfcba0ab667ce16bd40d02100dcca63bcbbd
Kang4882 <85673206+Kang4882@users.noreply.github.com>
2022-05-23 21:44:56 +0900

    Update Minjun.py

--- algorithm/2022/0524_1487_Making File Names Unique/Minjun.py
+++ algorithm/2022/0524_1487_Making File Names Unique/Minjun.py
@@ -1,14 +1,32 @@
 class Solution:
     def getFolderNames(self, names: List[str]) -> List[str]:
+        # 해시함수 활용
         file = {}
+        
         for name in names:
+            # 맵핑
             if name not in file:
                 file[name] = 1
-            k = 0;
+                continue
             
-            while file[name] not in file:
-                k += 1
-                file[name] = file[name].keys() + '(' + str(k) + ')'
-        
-        return file.keys()
+            # 중복된다면,
+            else:
+                # 중복 회피용 폴더 이름에 쓰일 k 값 초기화
+                # 첫 중복이라면 value = 1, 
+                # 두번째 등장부터는 value값으로 체크한 중복 횟수로 초기화 --> 시간 단축
+                k = file[name]
+                new_name = name + '(' + str(k) + ')'
                 
+                while new_name in file:
+                    k += 1
+                    new_name = name + '(' + str(k) + ')'
+                
+                # 중복 벗어났다면 새로 맵핑
+                file[new_name] = 1
+                
+                # 확인한 중복 횟수 업데이트
+                file[name] += 1
+                
+        return file.keys()
+                        
+                           
