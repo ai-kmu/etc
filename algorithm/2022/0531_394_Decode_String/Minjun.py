6e5ec91dbd88205467571391499908c62ebed529
Kang4882 <85673206+Kang4882@users.noreply.github.com>
2022-05-31 18:37:43 +0900

    Update Minjun.py

--- algorithm/2022/0531_394_Decode_String/Minjun.py
+++ algorithm/2022/0531_394_Decode_String/Minjun.py
@@ -1,5 +1,31 @@
 class Solution:
     def decodeString(self, s: str) -> str:
-        for i in range(len(s)):
-            if s[i] != ']':
+        stack = []
+
+        for c in s:
+            if c == ']':
+                string = []
+                num = []
                 
+                while stack:
+                    char = stack.pop()
+                    
+                    if char.isdigit():
+                        num.insert(0, char)
+                        print("number", num)
+                    
+                    elif char != '[':
+                        string.insert(0, char)
+                        print("string",string)
+                
+                gob = ''.join(num)
+                gob = int(gob)
+                string *= gob
+                for j in string:
+                    stack.append(j)
+                
+            else:
+                stack.append(c)                    
+        
+        answer = ''.join(string)
+        return answer
