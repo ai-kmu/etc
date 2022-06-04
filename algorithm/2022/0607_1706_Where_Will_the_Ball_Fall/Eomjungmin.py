cd677a3dad5339168cbfbfc5382f5082e698ca80
jmin0530 <72537757+jmin0530@users.noreply.github.com>
2022-06-04 20:13:30 +0900

    Update Eomjungmin.py

--- algorithm/2022/0607_1706_Where_Will_the_Ball_Fall/Eomjungmin.py
+++ algorithm/2022/0607_1706_Where_Will_the_Ball_Fall/Eomjungmin.py
@@ -1,7 +1,7 @@
 class Solution:
     def findBall(self, grid: List[List[int]]) -> List[int]:
         def fall(grid_map, r, c):
-            if c <= len(grid_map[0])-2 and c >= 1: # 현재 dfs 함수 안에서 grid 열 인덱스가 0<column_index<grid열 길이-1 인 경우
+            if c <= len(grid_map[0])-2 and c >= 1: # 현재 dfs 함수 안에서 grid 열 인덱스가 '0 < grid 열 인덱스 < (grid의 열 길이-1)' 인 경우
                 if grid_map[r][c] == grid_map[r][c+1] and grid_map[r][c] == 1: # 오른쪽 아래 방향 대각선으로 공이 흘러갈 수 있는 경우
                     # 마지막 row이면 열 인덱스+1 값 리턴
                     # 마지막 row가 아니면 dfs 반복
