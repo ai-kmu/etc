61087c8abf059776f408bad369eabecbef905c32
Kim Sungsik <sskim0126@kookmin.ac.kr>
2022-06-26 15:02:07 +0900

    Update sungsik.py

--- algorithm/2022/0628_140_Word_Break_II/sungsik.py
+++ algorithm/2022/0628_140_Word_Break_II/sungsik.py
@@ -4,23 +4,23 @@ from collections import deque
 class Solution:
     def wordBreak(self, s: str, wordDict: List[str]) -> List[str]:
         queue = deque()
-        N = len(s)
+        total_n = len(s)
         for sub_str in wordDict:
             n = len(sub_str)
-            if n <= N and s[:n] == sub_str:
+            if n <= total_n and s[:n] == sub_str:
                 queue.append((sub_str, n))
         
         answer = []
         
         while queue:
             prev_str, prev_n = queue.popleft()
-            if prev_n == N:
+            if prev_n == total_n:
                 answer.append(prev_str)
                 continue
             
             for sub_str in wordDict:
                 n = len(sub_str)
-                if prev_n + n <= N and s[prev_n:prev_n+n] == sub_str:
+                if prev_n + n <= total_n and s[prev_n:prev_n+n] == sub_str:
                     queue.append((prev_str + " " + sub_str, prev_n + n))
         
         return answer
