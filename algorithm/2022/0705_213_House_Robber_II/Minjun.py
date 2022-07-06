5445ec7f1093c64110f8a29dcf5f0327ffe51a3d
Kang4882 <85673206+Kang4882@users.noreply.github.com>
2022-07-06 17:52:58 +0900

    Update Minjun.py

--- algorithm/2022/0705_213_House_Robber_II/Minjun.py
+++ algorithm/2022/0705_213_House_Robber_II/Minjun.py
@@ -1,32 +1,36 @@
 class Solution:
     def rob(self, nums: List[int]) -> int:
-        def evenSum(nums):
+        tmp = []
+        
+        # len(nums) 짝수일 때, 최대값 계산 함수
+        def evenSum(n):
             odd = 0
             even = 0
-            for i in range(len(nums)):
+            for i in range(len(n)):
                 if (i%2) == 0:
-                    odd += i
+                    odd += n[i]
                 else:
-                    even += i
+                    even += n[i]
             return max(odd, even)
         
-        # nums 길이가 짝수이면,        
+        # nums 길이가 짝수이면, 최대값 계산 함수 호출  
         if len(nums) % 2 == 0:
             return evenSum(nums)
         
-        # nums 길이가 홀수이면,
-        for i, v in enumerate(nums):
-            standard = i
-            
-            if nums == 1:
-                nums.pop(-1)
-                nums.pop(2)
-                nums.pop(1)
-            else:
-                nums.pop(i+1)
-                nums.pop(i-1)
-                nums.pop(i)
-            
-            return standard + evenSum(nums)
-            
+        if len(nums) == 1:
+            return nums[0]
         
+        if len(nums) == 3:
+            return max(nums)
+        
+        # nums 길이가 홀수이면, len(nums) 짝수로 만들고, 계산 함수 호출
+        for i in range((len(nums)//2)+1):
+            standard = nums[i]
+            copy = nums[:]
+            copy.pop(i+1)
+            copy.pop(i)
+            copy.pop(i-1)
+            tmp.append((standard + evenSum(copy)))
+            
+        return max(tmp)
+            
