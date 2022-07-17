1f7546e2489f8eb0ff7ae0ea9e60b0c7096dea8b
Kang4882 <85673206+Kang4882@users.noreply.github.com>
2022-07-17 23:58:43 +0900

    Create Minjun.py

---
+++ algorithm/2022/0719_199_Binary_Tree_Right_Side_View/Minjun.py
@@ -0,0 +1,28 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def rightSideView(self, root: Optional[TreeNode]) -> List[int]:
+        if not root:
+            return
+        
+        answer = []
+        
+        
+        def find(node, idx):
+            
+            if node == None:
+                return
+            
+            if len(answer) == idx:
+                answer.append(node.val)
+            
+            find(node.right, idx+1)
+            find(node.left, idx+1)
+        
+        find(root, 0)
+        
+        return answer
