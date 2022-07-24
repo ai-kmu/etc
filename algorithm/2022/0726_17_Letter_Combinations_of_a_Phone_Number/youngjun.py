2a16806a018d57f61750f30392d37a45b4db0618
LeeYoungJun <46258521+YJJonatanLee@users.noreply.github.com>
2022-07-25 01:37:39 +0900

    Update youngjun.py

--- algorithm/2022/0726_17_Letter_Combinations_of_a_Phone_Number/youngjun.py
+++ algorithm/2022/0726_17_Letter_Combinations_of_a_Phone_Number/youngjun.py
@@ -11,13 +11,11 @@ class Solution:
         answer = []
         # 알파벳 리스트
         alphabet_list = list(ascii_lowercase)
-        # 알파벳 chunk 리스트
-        alphabet_chunk_list = []
         # phone number dictionary(hash)
-        phone_number = defaultdict()
+        phone_number = {str(c):[] for c in range(2,10)}
         
-        # 알파벳 chunk list 생성
-        for i in range(0,8):
+        # phone number dictionary(hash) 생성
+        for i in range(2,10):
             alphabet_chunk = []
             if i == 7 or i == 9:
                 size = 4
@@ -25,14 +23,9 @@ class Solution:
                 size = 3
             for j in range(size):
                 if i >= 8:
-                    alphabet_chunk.append(alphabet_list[3*i+j+1])
+                    phone_number[str(i)].append(alphabet_list[3*(i-2)+j+1])
                 else:
-                    alphabet_chunk.append(alphabet_list[3*i+j])
-            alphabet_chunk_list.append(alphabet_chunk)
-            
-        # 만들어진 알파벳 chunk 활용하여 phone number dictionary 만들어줌
-        for key, value in enumerate(alphabet_chunk_list):
-            phone_number[str(key+2)] = value
+                    phone_number[str(i)].append(alphabet_list[3*(i-2)+j])
         
         # 재귀 활용하여 조합 구함
         def recursion(i, str):
@@ -42,7 +35,7 @@ class Solution:
                 return answer
             # digits마다 계속 알파벳 더해줌
             for c in phone_number[digits[i]]:
-                backtrack(i+1, str + c)
+                recursion(i+1, str + c)
         
         recursion(0,"")
         
