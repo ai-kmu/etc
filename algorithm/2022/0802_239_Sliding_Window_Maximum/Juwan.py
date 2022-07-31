a6733616509310e12483ae7bb88b51adc394919a
Juwan-s <65757425+Juwan-s@users.noreply.github.com>
2022-07-31 23:15:33 +0900

    Update Juwan.py

--- algorithm/2022/0802_239_Sliding_Window_Maximum/Juwan.py
+++ algorithm/2022/0802_239_Sliding_Window_Maximum/Juwan.py
@@ -38,3 +38,27 @@ class Solution:
                 
 
         return answer # 테스트 케이스는 통과하지만 시간 초과뜸
+    
+    
+# 다 똑같이 deque로 푼 것 같아서 다르게 풀려했으나 실패
+    
+# 그냥 공부할겸 heapq쓰는 코드 찾아봄
+class Solution:
+    def maxSlidingWindow(self, nums: List[int], k: int) -> List[int]:
+        window = None
+        
+        answer = []
+        
+        for j in range(0, len(nums) - k + 1): # 처음부터 윈도우가 생성되는 길이까지 루프를 도는데
+            
+            if j == 0: # 만약 첫번째 윈도우일 때
+                
+                window = [(-n, i) for i, n in enumerate(nums[:k])] # 최대 힙 구현을 위해 음수로..
+                heapify(window) # 해당 윈도우를 힙으로 만들고
+                answer.append(-window[0][0]) # 가장 첫번째 원소를 반환할 때 음수를 붙여 최대값이 반환되도록 함
+            else: 
+                heappush(window, (-nums[j-1+k], j-1+k)) # 그 다음부터는 (음수로 변환한 값, 인덱스) 를 힙 구조에 넣고
+                while window[0][1] < j: # 최대값의 인덱스가 j보다 크거나 같을 때 까지 계속 꺼냄.
+                    heappop(window)
+                answer.append(-window[0][0]) # 정답에 추가해주면 됨
+        return answer
