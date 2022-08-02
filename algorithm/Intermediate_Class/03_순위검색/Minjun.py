0ce0fd75eeb6062233cbf53b14f1f02cb8222cc1
Kang4882 <85673206+Kang4882@users.noreply.github.com>
2022-08-02 12:56:25 +0900

    Update Minjun.py

--- algorithm/Intermediate_Class/03_순위검색/Minjun.py
+++ algorithm/Intermediate_Class/03_순위검색/Minjun.py
@@ -1,36 +1,34 @@
-# 하드코딩을 해봐도 시간초과를 어찌할 수 없었습니다.
+# 시간 초과
+# O(N^2)의 한계 . . . .
 # 잊지 않겠습니다 순위검색 . . .
 
 def solution(info, query):
     answer = []
-    person = []
-    que = []
-    for p in info:
-        person.append(p.split())
-    for q in query:
-        que.append(q.split(" and "))
+    
+    info = list(map(lambda x: x.split(), info))
+    query = list(map(lambda x: x.split(), query))
 
-    for q in que:
-        q.append(q[-1].split()[0])
-        q.append(q[-2].split()[1])
-        q.pop(-3)
-
-    for i in que:
+    for i in query:
         cnt = 0
-        for j in person:
+        for j in info:
+            # 언어 비교
             if i[0] != "-":
                 if i[0] != j[0]:
                     continue
-            if i[1] != "-":
-                if i[1] != j[1]:
-                    continue
+            # 직군 비교
             if i[2] != "-":
-                if i[2] != j[2]:
+                if i[2] != j[1]:
                     continue
-            if i[3] != "-":
-                if i[3] != j[3]:
+            # 경력 비교
+            if i[4] != "-":
+                if i[4] != j[2]:
                     continue
-            if int(j[4]) >= int(i[4]):
-                cnt+=1
+            # 소울푸드 비교
+            if i[6] != "-":
+                if i[6] != j[3]:
+                    continue
+            # 점수 비교
+            if int(j[4]) >= int(i[7]):
+                cnt += 1
         answer.append(cnt)
-    return answer            
+    return answer
