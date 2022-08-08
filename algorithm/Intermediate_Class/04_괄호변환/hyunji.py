39f8bc7e6b7fddb24af72041263e3d34c1c7f11e
hyunji12 <81363336+hyunji12@users.noreply.github.com>
2022-08-08 16:30:10 +0900

    Create hyunji.py

---
+++ algorithm/Intermediate_Class/04_괄호변환/hyunji.py
@@ -0,0 +1,44 @@
+def split_u_v(p):
+    l, r = 0, 0
+    u, v = '', ''
+    for pp in p:
+        if l != 0 and l == r:
+            v += pp
+            continue
+            
+        if pp == '(': l += 1
+        else: r += 1
+        
+        u += pp
+    return u, v
+    
+def solution(p):
+    
+    # 1
+    if not p:
+        return ""
+    
+    # 2
+    u, v = split_u_v(p)
+    
+    # 3. u가 '올바른 괄호 문자열'인 경우
+    if u[0] == '(' and u[-1] == ')':
+        return u + solution(v)
+    
+    # 4. u가 '올바른 괄호 문자열'이 아닌 경우
+    else:
+        # 4-1
+        answer = '('
+        # 4-2
+        answer += solution(v)
+        # 4-3
+        answer += ')'
+        # 4-4
+        u = u[1:-1]
+        for uu in u:
+            if uu == '(': 
+                answer += ')'
+            else: 
+                answer += '('
+        
+    return answer
