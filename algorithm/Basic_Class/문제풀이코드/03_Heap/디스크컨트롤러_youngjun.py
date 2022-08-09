4b6343453b60728011a5e5164e3dd54c6bf5fa14
LeeYoungJun <46258521+YJJonatanLee@users.noreply.github.com>
2022-08-09 18:13:17 +0900

    Update 디스크컨트롤러_youngjun.py

--- algorithm/Basic_Class/문제풀이코드/03_Heap/디스크컨트롤러_youngjun.py
+++ algorithm/Basic_Class/문제풀이코드/03_Heap/디스크컨트롤러_youngjun.py
@@ -6,13 +6,9 @@ def solution(jobs):
     heapq.heapify(jobs)
     jobs_num = len(jobs)
     sum_jobs = 0
-    start, end = 0, 0
     heap = []
     
     while jobs or heap:
-        while jobs and start <= jobs[0][0] <= end:
-            input_t, work = heapq.heappop(jobs)
-            heapq.heappush(heap, (work, input_t))
         
         if heap:
             work, input_t = heapq.heappop(heap)
@@ -24,6 +20,10 @@ def solution(jobs):
             start = input_t
             end = start + work
             sum_jobs += (end - start)
+            
+        while jobs and start <= jobs[0][0] <= end:
+            input_t, work = heapq.heappop(jobs)
+            heapq.heappush(heap, (work, input_t))
             
     answer = (sum_jobs//jobs_num)
     
