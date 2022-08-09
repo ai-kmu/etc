557739efe183d90f79f5d65f11f7ce5798607cd1
jmin0530 <72537757+jmin0530@users.noreply.github.com>
2022-08-09 13:32:39 +0900

    Update Eomjungmin.py

--- algorithm/2022/0809_1765_Map_of_Highest_Peak/Eomjungmin.py
+++ algorithm/2022/0809_1765_Map_of_Highest_Peak/Eomjungmin.py
@@ -2,22 +2,24 @@ from collections import deque
 
 class Solution:
     def highestPeak(self, isWater: List[List[int]]) -> List[List[int]]:
+        m = len(isWater)
+        n = len(isWater[0])
         
         # bfs 구현을 위한 deque 변수 선언
         queue = deque()
         
         # 정답 출력 행렬
-        ans = [[0 for i in range(len(isWater[0]))] for j in range(len(isWater))]
+        ans = [[0 for i in range(n)] for j in range(m)]
         
         # 방문 여부 기록하는 행렬
-        visited = [[-1 for i in range(len(isWater[0]))] for j in range(len(isWater))]
+        visited = [[-1 for i in range(n)] for j in range(m)]
         
         '''        
         먼저 queue에 물이 있는 곳의 위치와 높이를 저장
         그리고 먼저 방문했다고 기록함
         '''
-        for i in range(len(isWater)):
-            for j in range(len(isWater[0])):
+        for i in range(m):
+            for j in range(n):
                 if isWater[i][j] == 1:
                     queue.append((i,j,0))
                     visited[i][j] = 1
@@ -34,28 +36,28 @@ class Solution:
         
         '''
         while queue:
-            p = queue.popleft()
+            x, y, v = queue.popleft()
             if p[0]+1 < len(isWater):
-                if visited[p[0]+1][p[1]] == -1:
-                    visited[p[0]+1][p[1]] = 1
-                    ans[p[0]+1][p[1]] = p[2] + 1
-                    queue.append((p[0]+1, p[1], ans[p[0]+1][p[1]]))
+                if visited[x+1][y] == -1:
+                    visited[x+1][y] = 1
+                    ans[x+1][y] = v + 1
+                    queue.append((x+1, y, ans[x+1][y]))
                     
             if p[0]-1 >= 0:
-                if visited[p[0]-1][p[1]] == -1:
-                    visited[p[0]-1][p[1]] = 1
-                    ans[p[0]-1][p[1]] = p[2] + 1
-                    queue.append((p[0]-1, p[1], ans[p[0]-1][p[1]]))
+                if visited[x-1][y] == -1:
+                    visited[x-1][y] = 1
+                    ans[x-1][y] = v + 1
+                    queue.append((x-1, y, ans[x-1][y]))
                     
             if p[1]+1 < len(isWater[0]):
-                if visited[p[0]][p[1]+1] == -1:
-                    visited[p[0]][p[1]+1] = 1
-                    ans[p[0]][p[1]+1] = p[2] + 1
-                    queue.append((p[0], p[1]+1, ans[p[0]][p[1]+1]))
+                if visited[x][y+1] == -1:
+                    visited[x][y+1] = 1
+                    ans[x][y+1] = v + 1
+                    queue.append((x, y+1, ans[x][y+1]))
                     
             if p[1]-1 >= 0:
-                if visited[p[0]][p[1]-1] == -1:
-                    visited[p[0]][p[1]-1] = 1
-                    ans[p[0]][p[1]-1] = p[2] + 1
-                    queue.append((p[0], p[1]-1, ans[p[0]][p[1]-1]))
+                if visited[x][y-1] == -1:
+                    visited[x][y-1] = 1
+                    ans[x][y-1] = v + 1
+                    queue.append((x, y-1, ans[x][y-1]))
         return ans
