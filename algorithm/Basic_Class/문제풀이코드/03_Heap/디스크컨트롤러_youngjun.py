81646c631c09c7b3331bb0d7d7e18740a4691c4b
LeeYoungJun <46258521+YJJonatanLee@users.noreply.github.com>
2022-08-09 18:08:25 +0900

    Update 디스크컨트롤러_youngjun.py

--- algorithm/Basic_Class/문제풀이코드/03_Heap/디스크컨트롤러_youngjun.py
+++ algorithm/Basic_Class/문제풀이코드/03_Heap/디스크컨트롤러_youngjun.py
@@ -2,34 +2,29 @@ import heapq
 from collections import deque
 
 def solution(jobs):
-    
-    if len(jobs) == 1:
-        return jobs[0][1]
-    
-    jobs = sorted(jobs, key = lambda x : (x[0],x[1]))
-    jobs_queue = deque(jobs)
+
+    heapq.heapify(jobs)
     jobs_num = len(jobs)
     sum_jobs = 0
     start, end = 0, 0
     heap = []
-
-    while jobs_queue or heap:
-        
-        while jobs_queue and start <= jobs_queue[0][0] <= end:
-            input_t, work = jobs_queue.popleft()
+    
+    while jobs or heap:
+        while jobs and start <= jobs[0][0] <= end:
+            input_t, work = heapq.heappop(jobs)
             heapq.heappush(heap, (work, input_t))
-            
+        
         if heap:
             work, input_t = heapq.heappop(heap)
             start = end
             end += work
             sum_jobs += (end-input_t)
         else:
-            input_t, work = jobs_queue.popleft()
+            input_t, work = heapq.heappop(jobs)
             start = input_t
             end = start + work
             sum_jobs += (end - start)
-    
+            
     answer = (sum_jobs//jobs_num)
     
     return answer
