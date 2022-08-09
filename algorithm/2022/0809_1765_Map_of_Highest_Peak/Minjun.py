c55968d7fc46ebce0573b127315fcb2e2d0816b1
Kang4882 <85673206+Kang4882@users.noreply.github.com>
2022-08-09 12:31:09 +0900

    Update Minjun.py

--- algorithm/2022/0809_1765_Map_of_Highest_Peak/Minjun.py
+++ algorithm/2022/0809_1765_Map_of_Highest_Peak/Minjun.py
@@ -1,19 +1,37 @@
-# 오답
+from collections import deque
+
 class Solution:
     def highestPeak(self, isWater: List[List[int]]) -> List[List[int]]:
-        hmap = [[1 for i in range(len(isWater))] for j in range(len(isWater[0]))]
+        queue = deque()
+        hmap = [[-1 for i in range(len(isWater[0]))] for j in range(len(isWater))]
+        visit = []
+
+        # 방문하지 않았고, 인덱스 범위를 벗어나지 않는다면,
+        # 현재 높이보다 +1 해주고, 해당 위치를 queue에 넣는다.
+        def bfs(i,j):
+            if hmap[i-1][j] == -1 and i > 0:
+                hmap[i-1][j] = hmap[i][j] +1
+                queue.append([i-1,j])
+            if i < len(isWater) - 1 and hmap[i+1][j] == -1 :
+                hmap[i+1][j] = hmap[i][j] +1
+                queue.append([i+1,j])
+            if hmap[i][j-1] == -1 and j > 0:
+                hmap[i][j-1] = hmap[i][j] +1
+                queue.append([i,j-1])
+            if j < len(isWater[0]) - 1 and hmap[i][j+1] == -1 :
+                hmap[i][j+1] = hmap[i][j] +1
+                queue.append([i,j+1])
         
-        for i in range(len(isWater)):
-            for j in range(len(isWater[0])):
+        # water 위치를 queue에 다 넣는다.
+        for i, k in enumerate(isWater):
+            for j in range(len(k)):
                 if isWater[i][j] == 1:
                     hmap[i][j] = 0
-        for i, k in enumerate(hmap):
-            print(i, k)
-            for j in k:
-                print(j)
-                
-    def bfs(i, j):
-        k = hmap[i][j]
-        if hmap[i-1][j] - :
+                    queue.append([i,j])
         
-        
+        # queue 빌때까지 해준다.
+        while queue:
+            a = queue.popleft()
+            bfs(a[0],a[1])
+            
+        return hmap
