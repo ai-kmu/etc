35a422abc52b7dedaf3f7df3f1f78868e6398872
yoon1544 <45252342+dbsdmlgus50@users.noreply.github.com>
2022-08-14 16:30:19 +0900

    Create Euihyun.py

---
+++ algorithm/2022/0816_300_Longest_Increasing_Subsequence/Euihyun.py
@@ -0,0 +1,14 @@
+class Solution(object):
+    def lengthOfLIS(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: int
+        """
+        n = len(nums)
+        dp = [1 for _ in range(n)]
+        for i in range(n):
+            for j in range(i):
+                if nums[j] < nums[i]:
+                    dp[i] = max(dp[i], dp[j] + 1)
+
+        return max(dp)
