14a7e15dca7c5d27009dda8f64ff4b4e239aeca3
LeeYoungJun <46258521+YJJonatanLee@users.noreply.github.com>
2022-08-15 01:15:37 +0900

    Create youngjun.py

---
+++ algorithm/Intermediate_Class/05_구명보트/youngjun.py
@@ -0,0 +1,19 @@
+from collections import deque
+
+def solution(people, limit):
+    answer = 0
+    people.sort()
+    people_deque = deque(people)
+
+    while len(people_deque) > 1:
+        if people_deque[0] + people_deque[-1] <= limit:
+            people_deque.popleft()
+            people_deque.pop()
+        else:
+            people_deque.pop()
+        answer += 1
+        
+    if people_deque:
+        answer += 1
+        
+    return answer
