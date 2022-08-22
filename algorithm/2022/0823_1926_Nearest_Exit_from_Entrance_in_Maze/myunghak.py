11381f8982f4efc23c8c1acd999b0a785fe93512
LeeMyungHak <mh9716@kookmin.ac.kr>
2022-08-22 21:20:49 +0900

    Update myunghak.py

--- algorithm/2022/0823_1926_Nearest_Exit_from_Entrance_in_Maze/myunghak.py
+++ algorithm/2022/0823_1926_Nearest_Exit_from_Entrance_in_Maze/myunghak.py
@@ -1,3 +1,5 @@
+# 현재위치에서 시작한 후 현재 위치를 WALL로 바꾸고 BFS를 시작한다.
+
 from collections import deque 
 
 WALL = '+'
@@ -5,14 +7,12 @@ EMPTY = '.'
 POS = '-'
 
 class Solution:
-
     def nearestExit(self, maze, entrance):
         
         direction = ((0,1), (1,0), (-1,0), (0,-1))
-        dp = deque([[entrance[0], entrance[1], 0]])
+        dp = deque([[entrance[0], entrance[1], 0]])  # dp에는 현재 위치와 시작점으로부터의 거리를 저장함
         
-        def get_next_pos(pos):
-            
+        def get_next_pos(pos):  # 현재위치로부터 갈수있는 곳을 deque에 넣기
             for dir in direction:
                 if 0<= pos[0] + dir[0] < len(maze) and 0<= pos[1] + dir[1] < len(maze[0])  and maze[pos[0]+dir[0]][pos[1]+dir[1]] == EMPTY:
                     maze[pos[0] + dir[0]][pos[1] + dir[1]] = WALL
