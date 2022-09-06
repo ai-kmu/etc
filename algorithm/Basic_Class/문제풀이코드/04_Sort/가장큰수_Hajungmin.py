f53034ac49a3394c4796e27f5aeb133e0dc35b60
Ha-Jungmin <76420366+Ha-Jungmin@users.noreply.github.com>
2022-09-06 21:37:22 +0900

    Create 가장큰수_Hajungmin.py

---
+++ algorithm/Basic_Class/문제풀이코드/04_Sort/가장큰수_Hajungmin.py
@@ -0,0 +1,9 @@
+# 테스트 케이스 1만 통과
+
+def solution(numbers):
+    # numbers에 있는 숫자들을 1000의 자리에 맞춰준다음 내림차순으로 정렬
+    # 이후 string으로 변환해서 join을 통해 정답값 만들기
+    nums = sorted(numbers, key=lambda x: str(x + 10*x + 100*x), reverse=True)
+    nums = list(map(str, nums))
+    
+    return ''.join(nums)
