f442523ec1b20ee8140fa9edfcdd7fdfe4c0b815
Jaeseok Lee <dlwotjraks@gmail.com>
2022-09-11 19:16:42 +0900

    Update jaeseok.py

--- algorithm/2022/0913_1191_K-Concatenation_Maximum_Sum/jaeseok.py
+++ algorithm/2022/0913_1191_K-Concatenation_Maximum_Sum/jaeseok.py
@@ -2,7 +2,8 @@ class Solution:
     def kConcatenationMaxSum(self, arr: List[int], k: int) -> int:
         
         
-        # kadane's algorithm
+        # kadane's algorithm : dynamic programming 기법 중 하나
+        # maximum subarray sum을 구할 수 있음
         def kadanesum(arr):
             cursum = 0
             maxsum = -1e-9
@@ -15,9 +16,8 @@ class Solution:
             return maxsum
         
         
-        # prefix, suffix sum
-        # kadane's algorithm과는 다르게 무조건 맨 끝에서부터 sum을 구함
-        def fixsum(arr):
+        # 처음 혹은 끝에서부터 셈
+        def sum_(arr):
             cursum = 0
             maxsum = -1e-9
             for num in arr:
@@ -31,6 +31,6 @@ class Solution:
         if k == 1:
             return int(kadanesum(arr) % (1e9 + 7))
         # k > 1일 경우 array를 두개 이어붙인 경우의 subarray의 maximum sum,
-        # prefix sum과 suffix sum, array의 전체합을 k배만큼 더한 것 중 큰 쪽이 정답
+        # array의 전체합을 k배만큼 더한 것에서 양 끝에서부터 더한 maximum subarray 중 큰 쪽이 정답
         else:
-            return int(max(kadanesum(arr*2), fixsum(arr) + fixsum(arr[::-1]) + sum(arr) * (k - 2)) % (1e9 + 7))
+            return int(max(kadanesum(arr*2), sum_(arr) + sum_(arr[::-1]) + sum(arr) * (k - 2)) % (1e9 + 7)
