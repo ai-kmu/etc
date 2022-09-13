ab051d1f111df1ecb16ebcd29ef32bce4f9eff41
shinyousong <66022429+shinyousong@users.noreply.github.com>
2022-09-13 23:27:28 +0900

    Update 카펫_shinyousong.py

--- algorithm/Basic_Class/문제풀이코드/05_BruteForce/카펫_shinyousong.py
+++ algorithm/Basic_Class/문제풀이코드/05_BruteForce/카펫_shinyousong.py
@@ -1,7 +1,9 @@
 def solution(brown, yellow):
+    # 가로의 길이가 더 길게, 1부터 사각형의 둘레/2+1까지 돌면서 조합을 계산한다.
     for i in range(1, int(brown/2)+2):
         col = i
         row = int(brown/2)+2-i
+        # 사각형 내부 넓이가 yellow가 될 때 리턴
         if (row-2)*(col-2) == yellow:
             return [row, col]
         
