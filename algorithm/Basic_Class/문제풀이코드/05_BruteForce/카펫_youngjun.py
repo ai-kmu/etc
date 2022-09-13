5a1c74b7cb9544eb2c4f7d43eab6e1a15ae9cc54
LeeYoungJun <46258521+YJJonatanLee@users.noreply.github.com>
2022-09-13 13:18:57 +0900

    Update 카펫_youngjun.py

--- algorithm/Basic_Class/문제풀이코드/05_BruteForce/카펫_youngjun.py
+++ algorithm/Basic_Class/문제풀이코드/05_BruteForce/카펫_youngjun.py
@@ -1,9 +1,14 @@
 import math
 
 def solution(brown, yellow):
+    # x*y = brown + yellow
     mulxy = brown + yellow
+    # x + y = (brown + 4) / 2라면,
     plxy = (brown + 4) / 2
+    # x - y는 (x + y)^2 - 4xy의 제곱근이다.
     mixy = math.sqrt(plxy**2 - 4*mulxy)
+    # 이렇게 (x + y)와 (x - y)를 구한 후, 둘을 빼준 후 2로 나누면 y를 구할 수 있다.
     y = (plxy - mixy) / 2
+    # x를 구해준다.
     x = plxy - y
     return [x, y]
