6564d91b34954cfb0972793a7ac119cb71cc79cb
Jaeseok Lee <dlwotjraks@gmail.com>
2022-09-19 00:57:46 +0900

    Update jaeseok.py

--- algorithm/2022/0920_154_Find_Minimum_in_Rotated_Sorted_Array_II/jaeseok.py
+++ algorithm/2022/0920_154_Find_Minimum_in_Rotated_Sorted_Array_II/jaeseok.py
@@ -1,9 +1,10 @@
 class Solution:
     def findMin(self, nums: List[int]) -> int:
         # nums를 순차탐색하면서 첫 번째 숫자보다 작아지는 숫자가 있으면 그 숫자가 최솟값이 됨.
+        start = nums[0]
         for i in nums:
-            if nums[0] > i:
+            if start > i:
                 return i
         # 그런 숫자가 존재하지 않으면 nums는 rotation이 일어나지 않음.
-        return nums[0]
-      
+        return start
+    
