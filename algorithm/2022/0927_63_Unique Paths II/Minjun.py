c90357f1dde4c596cf35b22d6fc0d4b966e7d0f4
Kang4882 <85673206+Kang4882@users.noreply.github.com>
2022-09-26 23:01:48 +0900

    Update Minjun.py

--- algorithm/2022/0927_63_Unique Paths II/Minjun.py
+++ algorithm/2022/0927_63_Unique Paths II/Minjun.py
@@ -1,21 +1,37 @@
 class Solution:
     def uniquePathsWithObstacles(self, obstacleGrid: List[List[int]]) -> int:
-        dp = [[0] * len(obstacleGrid[0])]*len(obstacleGrid)
+        dp = [[0 for i in range(len(obstacleGrid[0]))] for j in range(len(obstacleGrid))]
         
+        # 예외처리
+        if obstacleGrid[0][0] == 1:
+            return 0
+
+        # 예외처리
+        if len(obstacleGrid) == 1 or len(obstacleGrid[0]) == 1:
+            for i in range(len(obstacleGrid)):
+                for j in range(len(obstacleGrid[0])):
+                    if obstacleGrid[i][j] == 1:
+                        return 0
+            return 1
+        
+        # 상단 1 채우기
+        for i in range(len(obstacleGrid[0])):
+            dp[0][i] = 1
+            
+        # 좌단 1채우기
+        for i in range(len(obstacleGrid)):
+            dp[i][0] = 1
         
         for i in range(len(obstacleGrid)):
             for j in range(len(obstacleGrid[0])):
                 if i == 0 and j == 0:
-                    continue                
-#                 # 상단
-                if i == 0:
-                    dp[i][j] = dp[i][j-1] + 1
+                    continue
                 
-                print(dp, i, j)
-                # 좌단
-                # if j == 0:
-                #     dp[i][j] = dp[i-1][j] + 1
-                #     print(dp, "i", i, "j", j)
-#                 # 내부 matix
-#                 dp[i][j] = dp[i-1][j] + dp[i][j-1]
-        print(dp)
+                if obstacleGrid[i][j] == 1:
+                    dp[i][j] = 0
+                    
+                    continue
+                
+                # 내부 matix
+                dp[i][j] = dp[i-1][j] + dp[i][j-1]
+        return dp[len(obstacleGrid)-1][len(obstacleGrid[0])-1]
