672fd5edf0856ab39e1e7b1bd56a9419148266c7
yoon1544 <45252342+dbsdmlgus50@users.noreply.github.com>
2022-09-29 10:42:19 +0900

    Update 정수삼각형_Euihyun.py

--- algorithm/Basic_Class/문제풀이코드/07_DP/정수삼각형_Euihyun.py
+++ algorithm/Basic_Class/문제풀이코드/07_DP/정수삼각형_Euihyun.py
@@ -4,21 +4,21 @@ def solution(triangle):
     m = len(triangle)
     # dp table 생성
     dp = [[0 for i in range(j+1)] for j in range(m)]
-    # 0,0 업데이트
+    # 1층 2층 미리 업데이트
     dp[0][0] = triangle[0][0]
     dp[1][0] = dp[0][0] + triangle[1][0]
     dp[1][1] = dp[0][0] + triangle[1][1]
 
-    # 1,0 부터 돌면서 업데이트
+    # 3층 부터 돌면서 업데이트
     for i in range(2, m):
         for j in range(len(triangle[i])):
-            # 처음이면 이전층의 첫번째 값을 더해줌
+            # 해당층의 첫번째 값은 이전층의 첫번째 값을 더해줌
             if j == 0:
                 dp[i][j] = dp[i-1][0] + triangle[i][j]
-            # 마지막 값이면 이전층의 마지막값에 더해줌
+            # 해당층의 마지막 값은 이전층의 마지막값에 더해줌
             elif j == (len(triangle[i])-1):
                 dp[i][j] = dp[i-1][-1] + triangle[i][j]
-            # 사이값 이면 이전층의 이전값+지금, 이전층의 지금값+지금 중에 맥스를 넣어줌
+            # 해당층의 사이값은 이전층의 이전값+지금, 이전층의 지금값+지금 중에 맥스를 넣어줌
             else:
                 dp[i][j] = max(dp[i-1][j-1]+triangle[i][j], dp[i-1][j]+triangle[i][j])
                 
