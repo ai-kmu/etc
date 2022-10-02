c0b3e128f047f62e2e231296b5382726b19c1b14
Jong Yoon Shin <92678942+JJongyn@users.noreply.github.com>
2022-10-02 15:09:02 +0900

    Create Jongyoon.py
    Create Jongyoon.py

---
+++ algorithm/2022/1004_93_Restore_IP_Addresses/Jongyoon.py
@@ -0,0 +1,35 @@
+class Solution(object):
+    def restoreIpAddresses(self, s: str) -> List[str]:
+    
+        result = []
+        
+        if len(s) < 4 or len(s) > 12:                                                          # 최소 길이 : 4, 최대 길이 : 12 
+            return None
+        
+        for a1 in range(1, 4):               
+            for a2 in range(1, 4):
+                for a3 in range(1, 4):
+                    for a4 in range(1, 4):
+                        if len(s) == a1 + a2 + a3 + a4:                                        # array에서 선택할 원소의 수가 전체 원소와 같아야 함.
+                            
+                            first = s[0:a1]                                                    # 첫 번째 ip
+                            second = s[a1:a1+a2]                                               # 두 번째 ip 
+                            third = s[a1+a2:a1+a2+a3]                                          # 세 번째 ip
+                            fourth = s[a1+a2+a3:a1+a2+a3+a4]                                   # 네 번째 ip
+                                                        
+                            data = list(map(self.CheckNum, [first, second, third, fourth]))    # 중복 제거 후 2차원 배열로
+                            
+                            if None not in data:                                               # check 후 문제가 없다면  
+                                result.append(".".join(data))                                  # .으로 나눈 후 join
+                    
+        return result
+    
+    # 첫 번째 숫자가 0 or 255 보다 큰 경우 check
+    def CheckNum(self, num):
+        if (len(num) > 1 and num[0] == "0") or int(num) > 255 :
+            return None
+        return num
+    
+    
+           
+            
