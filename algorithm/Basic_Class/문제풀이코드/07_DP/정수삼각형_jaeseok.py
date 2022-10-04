3489e5a07dd0dc3275ee6bde104958ddf8e6a854
Jaeseok Lee <dlwotjraks@gmail.com>
2022-10-05 01:36:08 +0900

    Update 정수삼각형_jaeseok.py

--- algorithm/Basic_Class/문제풀이코드/07_DP/정수삼각형_jaeseok.py
+++ algorithm/Basic_Class/문제풀이코드/07_DP/정수삼각형_jaeseok.py
@@ -4,11 +4,11 @@ def solution(triangle):
     for i in range(1, len(triangle) + 1):
         dp.append([0] * i)
     dp[0][0] = triangle[0][0]
-    # 외곽 부분 계산
+    # 외곽 부분은 항상 고정이므로 미리 계산
     for i in range(1, len(triangle)):
         dp[i][0] = dp[i - 1][0] + triangle[i][0]
         dp[i][-1] = dp[i - 1][-1] + triangle[i][-1]
-    # 나머지 부분 계산
+    # 나머지 부분은 위의 두 부분 중에서 더했을 때 더 큰 값으로 갱신해줌
     for i in range(2, len(triangle) + 1):
         for j in range(i - 2):
             dp[i - 1][j + 1] = max((dp[i - 2][j] + triangle[i - 1][j + 1]),
