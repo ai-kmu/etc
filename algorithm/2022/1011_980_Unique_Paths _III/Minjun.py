0cfbd4f08be262905245cfe6101025605c68e6e1
Kang4882 <85673206+Kang4882@users.noreply.github.com>
2022-10-10 20:42:57 +0900

    Update Minjun.py

--- algorithm/2022/1011_980_Unique_Paths _III/Minjun.py
+++ algorithm/2022/1011_980_Unique_Paths _III/Minjun.py
@@ -1,55 +1,52 @@
 class Solution:
+    
     def uniquePathsIII(self, grid: List[List[int]]) -> int:
-        sp = []
-        answer = 0
-        move = [-1,1]
+        self.answer = 0
+        dx = [0, 0, -1, 1]
+        dy = [-1, 1, 0, 0]
         
-        for i, gri in enumerate(grid):
-            for j, g in enumerate(gri):
-                if sp:
+        # start point 추출
+        for i in range(len(grid)):
+            for j in range(len(grid[0])):
+                if grid[i][j] == 1:
+                    sx, sy = i, j
                     break
-                if g == 1:
-                    sp.append((i,j))
-                    
         
-        def dfs(visit_grid, i, j):
-            print("시작", visit_grid, i, j)
-            # 인덱스
+        def dfs(grid, i, j):
+            # 인덱스 범위처리
             if i < 0 or i > len(grid)-1:
                 return
-            # 인덱스
+            # 인덱스 범위처리
             if j < 0 or j > len(grid[0])-1:
                 return
-            # 장애물
+            # 장애물 backtracking
             if grid[i][j] == -1:
                 return
             
             # 목적지 도착했을 때,
             if grid[i][j] == 2:
-                # 목적지 방문처리
-                print("도착")
-                visit_grid[i][j] = -1
                 # 모든 곳 방문 했는지 확인.
-                for _ in visit_grid:
-                    if 0 in _:
-                        break
-                # 다 방문 했으면 굳.
-                answer += 1
+                # grid 에 0이 아닌 곳이 있다면, 다 방문한 것이 아님.
+                for k in grid:
+                    if 0 in k:
+                        return
+                # 다 방문했으니 경로 개수 +1 후 return
+                self.answer += 1
+                return
             
-            print("방문처리", i,j)
-            visit_grid[i][j] = -1
+            # ======================
+            # 경로 탐색 과정
             
-            for m in move:
-                dfs(visit_grid, i+m, j)
-                dfs(visit_grid, i, j+m)
+            # grid 원상복구용 변수
+            temp = grid[i][j]
+            
+            # grid 방문 처리
+            grid[i][j] = -1
+            for m,n in zip(dx,dy):
+                dfs(grid, i+m, j+n)
                 
-        dfs(grid,sp[0][0],sp[0][1])
-        return answer
-        
+            # 다른 경로 찾을 때를 대비하여 grid  원상복구
+            grid[i][j] = temp
             
-            
-            
-            
-            
-            
-            
+        dfs(grid, sx, sy)
+        return self.answer
