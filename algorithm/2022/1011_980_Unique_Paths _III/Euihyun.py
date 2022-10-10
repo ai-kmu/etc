5e1529d010d78c9f419295198f9528d15523a916
yoon1544 <45252342+dbsdmlgus50@users.noreply.github.com>
2022-10-10 18:01:50 +0900

    Update Euihyun.py

--- algorithm/2022/1011_980_Unique_Paths _III/Euihyun.py
+++ algorithm/2022/1011_980_Unique_Paths _III/Euihyun.py
@@ -1,17 +1,50 @@
 # dfs 해보려고 했는데 실패
-
+# 추가로 공부하고 업데이트 했습니다. 코드리뷰 안해주셔도 됩니다!
 class Solution:
     def uniquePathsIII(self, grid: List[List[int]]) -> int:
-        
-        dx = [1, -1, 0, 0]
-        dy = [0, 0, 1, -1]
+        ans = 0
         m = len(grid)
         n = len(grid[0])
+        x = 0
+        y = 0
+        empty = 1
         
-        # 시작점 찾기
-        for i in range(m):
-            for j in range(n):
+        # 시작점 찾고 
+        # 0 이면 갈수있는 곳들 체크
+        for i in range(0, m):
+            for j in range(0, n):
                 if grid[i][j] == 1:
-                    x, y =i, j
+                    x = i
+                    y = j
+                elif grid[i][j] == 0:
+                    empty += 1
+        
+        def dfs(x, y, empty):
+            nonlocal ans
+            # 범위를 초과하거나 장애물이 있으면 return
+            if  x >= m or x < 0  or y >= n or y < 0 or grid[x][y] == -1: 
+                return
+            
+            # 도착지첨 들어 오면 정답 추가
+            if grid[x][y] == 2 :
+                if empty == 0:
+                    ans += 1
+                return
+            
+            # 이미 갔던 곳은 -1로 바꿔줌
+            grid[x][y] = -1
+            
+            # 네 방향 조회
+            dfs(x+1, y, empty-1)
+            dfs(x-1, y, empty-1)
+            dfs(x, y+1, empty-1)
+            dfs(x, y-1, empty-1)
+
+            # 다시 이동가능하게 바꿔줌 
+            grid[x][y] = 0
+        
+        dfs(x,y, empty)
+        
+        return ans
 
                     
