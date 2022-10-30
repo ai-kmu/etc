04c5b360b28c17d864820144c589e3aa3551761b
Kang4882 <85673206+Kang4882@users.noreply.github.com>
2022-10-31 00:00:58 +0900

    Create Minjun.py

---
+++ algorithm/2022/1102_127._Word_Ladder/Minjun.py
@@ -0,0 +1,20 @@
+class Solution:
+    def ladderLength(self, beginWord: str, endWord: str, wordList: List[str]) -> int:
+        # trivial case
+        if endWord not in wordList:
+            return 0
+        
+        l = len(beginWord)
+        
+        for i in range()
+        for i in range(l):
+            
+            
+        
+        a = 'abc'
+        b = 'abd'
+        print(a[1])
+        
+        
+        # def bfs(b):
+             
