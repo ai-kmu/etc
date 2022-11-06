473421fa1a2dcc6056d745cac18e0406ac52720f
Juwan-s <65757425+Juwan-s@users.noreply.github.com>
2022-11-06 20:07:12 +0900

    Update Juwan.py

--- algorithm/2022/1108_18_4Sum/Juwan.py
+++ algorithm/2022/1108_18_4Sum/Juwan.py
@@ -1,1 +1,30 @@
-
+class Solution:
+    def fourSum(self, nums: List[int], target: int) -> List[List[int]]:
+        
+        n = len(nums)
+        
+        nums.sort()
+        
+        answer = set() # 밑에서 중복방지를 위한
+        
+        for i in range(n):
+            
+            for j in range(i + 1, n): # 4개의 pointer 사용
+                
+                x = j + 1
+                y = n - 1
+                
+                while x < y:
+                    
+                    sum_val = nums[i] + nums[j] + nums[x] + nums[y]
+                    
+                    if sum_val > target: y -= 1
+                        
+                    elif sum_val < target: x += 1
+                        
+                    else:
+                        ans = (nums[i], nums[j], nums[x], nums[y])
+                        answer.add(ans)
+                        x += 1
+                        y -= 1 # 포인터 이동
+        return answer
