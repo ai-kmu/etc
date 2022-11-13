12b33e143423eedf4f236ec00f10a8159e19c96f
justinday123 <45382680+justinday123@users.noreply.github.com>
2022-11-13 23:40:58 +0900

    Update jaewoong.py

--- algorithm/2022/1115_741_Cherry Pickup/jaewoong.py
+++ algorithm/2022/1115_741_Cherry Pickup/jaewoong.py
@@ -1,5 +1,9 @@
 class Solution(object):
     def cherryPickup(self, grid):
+        """
+        :type grid: List[List[int]]
+        :rtype: int
+        """
         n = len(grid)
 		
         dp = [[0]*n for _ in range(n)]
@@ -9,14 +13,20 @@ class Solution(object):
         for k in range(1, 2*n-1):
             curr = [[-1]*n for _ in range(n)]
             for i in range(min(n, k+1)):
-                if (k-i) >= n or grid[i][k-i] < 0: continue
+                if (k-i) >= n or grid[i][k-i] < 0: 
+                    continue
                 for j in range(min(n, k+1)):
-                    if k-j >= n or grid[k-j][j] < 0: continue
+                    if k-j >= n or grid[k-j][j] < 0: 
+                        continue
                     ans = dp[i][j]
-                    if i > 0: ans = max(ans, dp[i-1][j])
-                    if j > 0: ans = max(ans, dp[i][j-1])
-                    if i > 0 and j > 0: ans = max(ans, dp[i-1][j-1])
-                    if ans < 0: continue
+                    if i > 0: 
+                        ans = max(ans, dp[i-1][j])
+                    if j > 0: 
+                        ans = max(ans, dp[i][j-1])
+                    if i > 0 and j > 0: 
+                        ans = max(ans, dp[i-1][j-1])
+                    if ans < 0: 
+                        continue
                     ans += grid[i][k-i]
                     if i != k-j:
                         ans += grid[k-j][j]
