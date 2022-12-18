88ded2991b707b69b97b51cefe09088dd992f1a0
yoon1544 <45252342+dbsdmlgus50@users.noreply.github.com>
2022-12-19 00:01:49 +0900

    Create Euihyun.py

---
+++ algorithm/2022/1219_952_Largest_Component_Size_by_Common_Factor/Euihyun.py
@@ -0,0 +1,18 @@
+더 풀어보겠습니다...
+class Solution(object):
+    def largestComponentSize(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: int
+        """
+        
+        n = len(nums)
+        temp = []
+        check_list = [False for i in range(len(nums))
+        #소수 판별   
+        def check_(n):
+                      
+        for i in range(n):
+            for j in range(i+1,n):
+            #소수 판별
+                check_
