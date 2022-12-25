d40d5bbf0ef54e835249c9f06543b47d19bb3fba
Kang4882 <85673206+Kang4882@users.noreply.github.com>
2022-12-26 00:20:16 +0900

    Update Minjun.py

--- algorithm/2022/1227_2448_Minimum_Cost_to_Make_Array_Equal/Minjun.py
+++ algorithm/2022/1227_2448_Minimum_Cost_to_Make_Array_Equal/Minjun.py
@@ -1,9 +1,12 @@
 class Solution:
     def minCost(self, nums: List[int], cost: List[int]) -> int:
-        ans = float('inf')
         under = min(nums)
         upper = max(nums)
         k = (under + upper) // 2
+        
+        # trivial case
+        if under == upper:
+            return 0
 
         while under <= upper:
             k = (under + upper) // 2
@@ -14,14 +17,15 @@ class Solution:
             for i, n in enumerate(nums):
                 pre_total += abs(n - (k-1)) * cost[i]
 
+            # 현재의 직전 값과 비교한다.
+            # 직전값보다 현재가 작으면 under 땡겨온다.
             if pre_total > total:
                 under = k + 1
+            # 직전값보다 현재가 크면 upper 땡겨온다.
             elif pre_total < total:
                 upper = k - 1
+                # 직전값이 더 작으니 최신화 해준다.
+                total = pre_total
             else:
                 break
-        
         return total
-
-
-            
