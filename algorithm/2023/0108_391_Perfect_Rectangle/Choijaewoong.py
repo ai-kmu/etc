867d9f886aa977ea934ba2021faf550d9135f8a6
justinday123 <45382680+justinday123@users.noreply.github.com>
2023-01-08 23:54:57 +0900

    Create Choijaewoong.py

---
+++ algorithm/2023/0108_391_Perfect_Rectangle/Choijaewoong.py
@@ -0,0 +1,14 @@
+# 풀이실패...
+class Solution:
+    hs = set()
+    def build_r_map(self, rectangles):
+        total_area, pt = 0, defaultdict(int)
+        for r in rectangles:
+            # 영역 설정
+            top_left = (r[0], r[1])
+            top_right = (r[0], r[3])
+            bottom_left = (r[2], r[1])
+            bottom_right = (r[2], r[3])
+            for i in [top_left, top_right, bottom_left, bottom_right]:
+                #if i not in hs:
+                #
