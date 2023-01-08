f595ec1dfff86445ddd0e35cc3687b72210f13da
Jaeseok Lee <dlwotjraks@gmail.com>
2023-01-08 23:47:23 +0900

    Update Jaeseok.py

--- algorithm/2023/0108_391_Perfect_Rectangle/Jaeseok.py
+++ algorithm/2023/0108_391_Perfect_Rectangle/Jaeseok.py
@@ -2,21 +2,19 @@
 # 33/49 : 넓이 차이가 동일한 경우에는 안됨
 # 수정중
 
-class Solution:
+cclass Solution:
     def isRectangleCover(self, rectangles: List[List[int]]) -> bool:
         gx_min, gx_max, gy_min, gy_max = 10**5, -10**5, 10**5, -10**5
         res_area = 0
         for x_min, y_min, x_max, y_max in rectangles:
-            if x_min < gx_min:
-                gx_min = x_min
-            if y_min < gy_min:
-                gy_min = y_min
-            if x_max > gx_max:
-                gx_max = x_max
-            if y_max > gy_max:
-                gy_max = y_max
+            gx_min = min(x_min, gx_min)
+            gy_min = min(y_min, gy_min)
+            gx_max = max(x_max, gx_max)
+            gy_max = max(y_max, gy_max)
             res_area += (x_max - x_min) * (y_max - y_min)
         rect_area = (gx_max - gx_min) * (gy_max - gy_min)
+        print(gx_min, gx_max, gy_min, gy_max)
         if res_area == rect_area:
             return True
         return False
+
