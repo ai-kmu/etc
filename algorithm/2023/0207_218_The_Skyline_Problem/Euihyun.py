7dbb76cfaf602b0fe329d73b2507cbea07872bef
yoon1544 <45252342+dbsdmlgus50@users.noreply.github.com>
2023-02-05 23:51:51 +0900

    Create Euihyun.py

---
+++ algorithm/2023/0207_218_The_Skyline_Problem/Euihyun.py
@@ -0,0 +1,19 @@
+# 아예 못풀겠어서 정답 보고 공부만 했습니다..
+
+class Solution:
+    def getSkyline(buildings: List[List[int]]) -> List[List[int]]:
+        events = [(L, -H, R) for (L, R, H) in buildings]
+        events += [(R, 0, 0) for (_, R, _) in buildings]
+        events.sort()
+
+        ans = [(0, 0)]
+        live = [(0, float("inf"))]
+
+        for (L, negH, R) in events:
+            while live[0][1] <= L:
+                heapq.heappop(live)
+            if negH:
+                heapq.heappush(live, (negH, R))
+            if ans[-1][1] != -live[0][0]:
+                ans.append((L, -live[0][0]))
+        return ans[1:]
