aa1d3ff7c91c81bf2b2c9b237fdf77847735d97c
LeeMyungHak <mh9716@kookmin.ac.kr>
2023-03-28 01:06:32 +0900

    Update Myunghak.py

--- algorithm/2023/0328_378_Kth Smallest_Element_in_a_Sorted_Matrix/Myunghak.py
+++ algorithm/2023/0328_378_Kth Smallest_Element_in_a_Sorted_Matrix/Myunghak.py
@@ -1,4 +1,40 @@
-# flatten 한다음 sorting
+# # flatten 한다음 sorting
+# class Solution:
+#     def kthSmallest(self, matrix: List[List[int]], k: int) -> int:
+#         return sorted([item for sublist in matrix for item in sublist])[k-1]
+
+
+# directed graphg의 BFS
+# n*n크기를 matrix에서 (x,y)가 rank r이 될 수 있는 조건은 아래와 같다.
+# x(n+1) + y(n+1) -xy - 2n < r < xy
+# 이 떄 n값과 r값은 상수이므로 부등식을 방정식으로 바꾸어 x,y쌍의 해를 구하면 가능한 부분만 BFS로 탐색이 가능하다.
+
+import heapq
+
 class Solution:
     def kthSmallest(self, matrix: List[List[int]], k: int) -> int:
-        return sorted([item for sublist in matrix for item in sublist])[k-1]
+        m, n = len(matrix), len(matrix[0])
+        
+        visited = set()
+        start_index = 0
+        pq = []
+        for x in range(1,n+1):
+            y = max(int((k + 2 * n - x * (n+1)) / (n+1 - x) - 1e-10), 0)
+            
+            start_index += y
+            if y < n:
+                visited.add((x-1, y))
+                heapq.heappush(pq, [matrix[x-1][y], x-1, y])
+        
+        for i in range(start_index, k):
+            v, r, c = heapq.heappop(pq)
+
+            if r + 1 < m and (r + 1, c) not in visited:
+                heapq.heappush(pq, [matrix[r + 1][c], r + 1, c])
+                visited.add((r + 1, c))
+
+            if c + 1 < n and (r, c + 1) not in visited:
+                heapq.heappush(pq, [matrix[r][c + 1], r, c + 1])
+                visited.add((r, c + 1))
+
+        return v
