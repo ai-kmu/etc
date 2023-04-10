37fd255e1e608fc2ce84b12f337a1a9ae78e3ec8
SeungHeon <84365977+song1248@users.noreply.github.com>
2023-04-10 23:15:32 +0900

    Update Seungheon.py

--- algorithm/2023/0412_1219_Path_with_Maximum_Gold/Seungheon.py
+++ algorithm/2023/0412_1219_Path_with_Maximum_Gold/Seungheon.py
@@ -1,43 +1,41 @@
-# fail code
-# dp로풀어보려했지만 실패 이렇게 풀면 안되는둣
-
-
 class Solution:
     def getMaximumGold(self, grid: List[List[int]]) -> int:
         
-        # 시작위치부터 최대값찾기
-        # dp
+    # 시작위치부터 최대값찾기
         def initialize(i, j):
             visited = [[0 for _ in grid[0]] for _ in grid]
+            visited[i][j] = 1
             tmp_gird = copy.deepcopy(grid)
             max_gold = 0
             def explore(i, j, prv_g = 0):
                 nonlocal max_gold
                 
-                # for asd in tmp_gird:
-                #     print(asd)
-                # print()
-                # 범위밖 처리
-                if i < 0 or i >= len(grid) or j < 0 or j >= len(grid[0]):
-                    return
-
-                # 방문체크
-                if visited[i][j] == 1:
-                    return
-                visited[i][j] = 1
-
-                # 금이 없으면 안감
-                if tmp_gird[i][j] == 0:
-                    return
-
+                tmp = tmp_gird[i][j]
                 tmp_gird[i][j] += prv_g
-                tmp_gold = tmp_gird[i][j]
 
                 max_gold = max(max_gold, tmp_gird[i][j])
 
-                for di, dj in ([1,0],[0,1],[-1,0],[0,-1]):
-                    explore(i+di,j+dj, tmp_gold)
-
+                for di, dj in [[1,0],[0,1],[-1,0],[0,-1]]:
+                    
+                    # 범위밖 처리
+                    if i+di < 0 or i+di >= len(grid) or j+dj < 0 or j+dj >= len(grid[0]):
+                        continue
+                    
+                    # 방문체크
+                    if visited[i+di][j+dj] == 1  :
+                        continue 
+                    visited[i+di][j+dj] = 1
+                
+                    # 금이 없으면 안감
+                    if tmp_gird[i+di][j+dj] == 0:
+                        continue
+                    
+                    # 백트랙킹
+                    tmp = tmp_gird[i+di][j+dj]
+                    explore(i+di,j+dj, tmp_gird[i][j])
+                    visited[i+di][j+dj] = 0
+                    tmp_gird[i+di][j+dj] = tmp
+            
             explore(i, j)
 
             return max_gold 
@@ -45,6 +43,8 @@ class Solution:
         answer = 0
         for r in range(len(grid)):
             for c in range(len(grid[0])):
+                if grid[r][c] == 0:
+                    continue
                 answer = max(answer, initialize(r,c))
 
         return answer
