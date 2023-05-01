ec66c2b27a50905079f56295ba1c1dbd3f6cc4d0
jmin0530 <72537757+jmin0530@users.noreply.github.com>
2023-05-01 23:49:33 +0900

    Create Eomjungmin.py

---
+++ algorithm/2023/0503_2498_Frog_Jump_II/Eomjungmin.py
@@ -0,0 +1,29 @@
+class Solution:
+    def maxJump(self, stones: List[int]) -> int:
+    # test case에서 막힘
+        if len(stones) == 3:
+            return stones[2]
+        ans = stones[1]
+
+        for i in range(2,len(stones),2):
+            if ans < stones[i] - stones[i-2]:
+                ans = stones[i] - stones[i-2]
+
+        if len(stones) % 2 !=0:
+            for j in range(len(stones)-1,0,-2):
+                if j == 1:
+                    break
+
+                if ans < abs(stones[j-2] - stones[j]):
+                    ans = abs(stones[j-2] - stones[j])
+                    print(ans)
+        else:
+            for j in range(len(stones)-2,0,-2):
+                if j == 1:
+                    break
+
+                if ans < abs(stones[j-2] - stones[j]):
+                    ans = abs(stones[j-2] - stones[j])
+                    print(ans)
+
+        return ans
