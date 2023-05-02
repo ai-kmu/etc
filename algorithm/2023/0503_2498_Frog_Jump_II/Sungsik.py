e160ba6c87886a6c312b3395c12e47e317a19e19
Kim Sungsik <sskim0126@kookmin.ac.kr>
2023-05-02 14:57:30 +0900

    Update Sungsik.py

--- algorithm/2023/0503_2498_Frog_Jump_II/Sungsik.py
+++ algorithm/2023/0503_2498_Frog_Jump_II/Sungsik.py
@@ -1,8 +1,3 @@
 class Solution:
     def maxJump(self, stones: List[int]) -> int:
-        # 돌을 하나 skip하고 뛰어가면서 최대 거리를 찾으면 됨
-        n = len(stones)
-        if n == 0:
-            return 0
-        # 돌을 2개만큼 뛰면서 cands에 추가하고 이 중 최댓값을 return
-        return max([stones[1] - stones[0]] + [stones[i] - stones[i-2] for i in range(3, n, 2)] + [stones[i] - stones[i-2] for i in range(2, n, 2)])
+        return max([stones[1] - stones[0]] + [stones[i] - stones[i-2] for i in range(2, len(stones))])
