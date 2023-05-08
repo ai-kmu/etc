57bcc49759fd89aa2237188577d5188e880f691c
LeeMyungHak <mh9716@kookmin.ac.kr>
2023-05-09 00:18:55 +0900

    Update Myunghak.py

--- algorithm/2023/0510_2556_Disconnect_Path_in_a_Binary_Matrix_by_at_Most_One_Flip/Myunghak.py
+++ algorithm/2023/0510_2556_Disconnect_Path_in_a_Binary_Matrix_by_at_Most_One_Flip/Myunghak.py
@@ -1,34 +1,24 @@
-from collections import defaultdict
+# 그냥 첫번째 dfs 로 경로 탐색을 한번 한 후(이 때 지나간 경로는 표시함)
+# 해당 경로를 지나지 않고 가는 다른 경로가 있는지 찾기
 
 class Solution:
     def isPossibleToCutPath(self, grid):
         m, n = len(grid), len(grid[0])
-        paths_dict = defaultdict(int)
         visit = [[False] * n for _ in range(m)]
 
-        def dfs(x, y, visit, total_path, path_num):
+        def dfs(x, y, visit):
             if x == m - 1 and y == n - 1:
-                # print(total_path)
-                for d in total_path:
-                    paths_dict[d] += 1
-
-                return path_num + 1
+                visit[x][y] = False
+                return True
 
             for dx, dy in [(1, 0), (0, 1)]:
                 new_x, new_y = x + dx, y + dy
-                if new_x < m and new_y < n and not visit[new_x][new_y]  and grid[new_x][new_y] == 1:
+                if new_x < m and new_y < n and not visit[new_x][new_y]  and grid[new_x][new_y]:
                     visit[new_x][new_y] = True
-                    path_num = dfs(new_x, new_y, visit, total_path + [(new_x, new_y)], path_num )
-                    visit[new_x][new_y] = False
-            return path_num
+                    if dfs(new_x, new_y, visit):
+                        return True
 
-        visit[0][0] = True
-        path_num = dfs(0,0, visit, [(0,0)], 0)
-        # print(paths_dict)
-        paths_dict[(0,0)] = 0
-        paths_dict[(m-1,n-1)] = 0
+            return False
 
-        for k in paths_dict:
-            if paths_dict[k] == path_num:
-                return True
-        return False
+        path_num = dfs(0,0, visit)
+        return not dfs(0,0, visit)
