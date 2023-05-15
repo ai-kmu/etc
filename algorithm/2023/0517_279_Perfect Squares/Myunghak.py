837a835224eddd10a807920d22811327cc613819
LeeMyungHak <mh9716@kookmin.ac.kr>
2023-05-15 19:39:32 +0900

    Update Myunghak.py

--- algorithm/2023/0517_279_Perfect Squares/Myunghak.py
+++ algorithm/2023/0517_279_Perfect Squares/Myunghak.py
@@ -1,19 +1,29 @@
 class Solution:
     def numSquares(self, n: int) -> int:
-        squares = [i ** 2 for i in range(1, int(n ** 0.5) + 1)][::-1]
-        squares_set = set(squares)
+        dp = [0] * (n + 1)
 
-        def dp(n, ans, squares, min_ans):
-            if n in squares_set:
-                min_ans = min(min_ans, ans + 1)
-                return min_ans
+        for i in range(1, n + 1):
+            dp[i] = min([dp[i - j*j] + 1 for j in range(1, int(i ** 0.5) + 1)])
 
-            for i, s in enumerate(squares):
-                if ans+1 > min_ans:
-                    break
-                elif n - s >= 0:
-                    min_ans = dp(n - s, ans + 1, squares[i:], min_ans)
+        return dp[n]
 
-            return min_ans
+
+# class Solution:
+#     def numSquares(self, n: int) -> int:
+#         squares = [i ** 2 for i in range(1, int(n ** 0.5) + 1)][::-1]
+#         squares_set = set(squares)
+
+#         def dp(n, ans, squares, min_ans):
+#             if n in squares_set:
+#                 min_ans = min(min_ans, ans + 1)
+#                 return min_ans
+
+#             for i, s in enumerate(squares):
+#                 if ans+1 > min_ans:
+#                     break
+#                 elif n - s >= 0:
+#                     min_ans = dp(n - s, ans + 1, squares[i:], min_ans)
+
+#             return min_ans
         
-        return dp(n, 0, squares, n)
+#         return dp(n, 0, squares, n)
