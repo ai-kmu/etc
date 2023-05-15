6255334e1a632e0b98a46c6d6a58a95ff625590d
hacho08 <93698409+hacho08@users.noreply.github.com>
2023-05-16 00:24:46 +0900

    Update Hyunae.py

--- algorithm/2023/0517_279_Perfect Squares/Hyunae.py
+++ algorithm/2023/0517_279_Perfect Squares/Hyunae.py
@@ -3,23 +3,23 @@ import math
 class Solution:
     def numSquares(self, n: int) -> int:
         nums = []
-        dp = [0] * (n)
+        dp = [100000] * (n+1)
 
+        dp[1] = 1
+        
         for i in range(n):
             square = i*i
             if square > n:
                 break
             nums.append(square)
         
-        print(nums)
-        for i in range(1, n):
+        for i in range(1, n+1):
             j = 1
             while j*j <= i:
                 if i in nums:
                     dp[i] = 1
-                else:                
-                    dp[i] = min(dp[i-1]+1, dp[i-j*j] + 1)
+                else:           
+                    dp[i] = min(dp[i-1]+1, dp[i-j*j]+ dp[j*j], dp[i])
                 j += 1
 
-        print(dp)
-        return dp[n-1]
+        return dp[n]
