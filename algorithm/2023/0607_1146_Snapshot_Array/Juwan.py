1c3ff4f267ba0370d722750ac91aed866a47eeee
Juwan-s <65757425+Juwan-s@users.noreply.github.com>
2023-06-05 21:06:44 +0900

    Update Juwan.py

--- algorithm/2023/0607_1146_Snapshot_Array/Juwan.py
+++ algorithm/2023/0607_1146_Snapshot_Array/Juwan.py
@@ -1,24 +1,39 @@
-#  Memory Limit 나서 개선 중
-
 from collections import defaultdict
+from functools import lru_cache
 
 class SnapshotArray:
 
     def __init__(self, length: int):
         
-        self.arr = [0] * length
-        self.snap_dict = {0 : self.arr}
+        self.snap_dict = defaultdict(int)
         self.snap_id = 0
 
-    def set(self, index: int, val: int) -> None:
-        self.arr[index] = val
+        self.mapp = defaultdict(int)
+        self.map_id = -1
 
+        self.arr = {}
+
+    def set(self, index: int, val: int) -> None: 
+        self.snap_dict[index] = val
+
+    @lru_cache
     def snap(self) -> int:
-        self.snap_dict[self.snap_id] = self.arr
-        self.snap_id = self.snap_id + 1
+
+        self.snap_id += 1
+
+        if self.snap_id > 1 and self.arr[self.mapp[self.snap_id - 1]].items() == self.snap_dict.items():
+            self.mapp[self.snap_id] = self.map_id
+        else:
+            self.map_id += 1
+            self.mapp[self.snap_id] = self.map_id
+            self.arr[self.mapp[self.snap_id]] = self.snap_dict.copy()
+
+        return self.snap_id - 1
 
     def get(self, index: int, snap_id: int) -> int:
         
-        return self.snap_dict[snap_id][index]
-      
-      
+        # print(self.arr)
+
+        return self.arr[self.mapp[snap_id + 1]][index]
+
+
