7face08657c87c1954984e08111ffaae1b7ed31a
SeungHeon <84365977+song1248@users.noreply.github.com>
2023-06-26 15:26:45 +0900

    Update Seungheon.py

--- algorithm/2023/0628_Group_Anagrams/Seungheon.py
+++ algorithm/2023/0628_Group_Anagrams/Seungheon.py
@@ -3,14 +3,10 @@ from collections import defaultdict
 class Solution:
     def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
         
-        # 등장하는 str을 카운트해서 dictionary의 key값으로 사용
-        # value는 str
+        # str을 sort하여 key로 기존 str을 value로 사용
 
         str_dict = defaultdict(list)
         for s in strs:
-            key = [0]*26
-            for c in s:
-                key[ord(c)-97] += 1
-            str_dict[tuple(key)].append(s)
+            str_dict[tuple(sorted(list(s)))].append(s)
 
         return str_dict.values()
