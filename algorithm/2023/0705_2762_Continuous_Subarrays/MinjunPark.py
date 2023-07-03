229adc42f9677645e424aa4267107ef96d77a984
mjun4138 <39406901+mjun4138@users.noreply.github.com>
2023-07-03 22:08:47 +0900

    Update MinjunPark.py

--- algorithm/2023/0705_2762_Continuous_Subarrays/MinjunPark.py
+++ algorithm/2023/0705_2762_Continuous_Subarrays/MinjunPark.py
@@ -6,19 +6,19 @@ class Solution:
         ans = 0
 
         for i in range(len(nums)):
-            s, e = i, i
-            minNum, maxNum = nums[s], nums[s]
+            s, e = i, i # s == subarray의 시작, e == subarray의 끝
+            minNum, maxNum = nums[s], nums[s] # initialize
 
-            while e < len(nums):
-                if nums[e] > maxNum:
+            while e < len(nums): 
+                if nums[e] > maxNum: # max값 갱신
                     maxNum = nums[e]
                 
-                if nums[e] < minNum:
+                if nums[e] < minNum:# min값 갱신
                     minNum = nums[e]
             
-                if maxNum - minNum <= 2:
+                if maxNum - minNum <= 2: # 조건에 부합하면 ans += 1
                     ans += 1
-                else:
+                else: # 부합하지 않으면 i+1번쨰 원소를 시작으로하는 subarray를 계산
                     break
 
                 e += 1
