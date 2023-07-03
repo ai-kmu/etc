7769e733ebac40d66e27e28cea908b3bc478f80c
Kang4882 <85673206+Kang4882@users.noreply.github.com>
2023-07-04 03:52:00 +0900

    Update MinjunKang.py

--- algorithm/2023/0705_2762_Continuous_Subarrays/MinjunKang.py
+++ algorithm/2023/0705_2762_Continuous_Subarrays/MinjunKang.py
@@ -1,13 +1,27 @@
+# 구현 실패
 class Solution:
     def continuousSubarrays(self, nums: List[int]) -> int:
         a = len(nums)
 
-        cnt = len(nums)
+        cnt = 0
+        ans = 0
         while a > 0:
+
             tmp = []
             for i in range(a-1):
-                tmp.append(nums[i+1]-nums[i])
+                diff = nums[i+1]-nums[i]
+                tmp.append(diff)
+            nope = []
+            for j, v in enumerate(tmp):
+                if v > 2:
+                    nope.append(j)
 
-        b = []
+
+            if not nope:
+                t = len(tmp) - (len(nums)-cnt) + 1
+                ans += t
+            a -= 1
+            nums = tmp
+            cnt += 1
         
-        return cnt
+        return ans
