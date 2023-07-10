fbb1e0e2edaf3fd9302a38b71f3e9bc1d0408476
Kang4882 <85673206+Kang4882@users.noreply.github.com>
2023-07-11 01:19:13 +0900

    Update MinjunKang.py

--- algorithm/2023/0712_2300_Successful_Pairs_of_Spells_and_Potions/MinjunKang.py
+++ algorithm/2023/0712_2300_Successful_Pairs_of_Spells_and_Potions/MinjunKang.py
@@ -1,7 +1,12 @@
+import bisect
+
 class Solution:
     def successfulPairs(self, spells: List[int], potions: List[int], success: int) -> List[int]:
-            ans = []
-            for s in spells:
-                a = [i for i, v in enumerate(potions) if s * v >= success]
-                ans.append(len(a))
-            return ans
+        # spells 요소 * potions 요소 >= succes 찾는 문제
+        # 이진분류로 idx 찾아서 개수 구하는 방법으로 접근
+
+        # 전체 개수: success 보다 작은 마지막 idx 구해서 빼기 위함
+        n = len(potions)
+        # 이진분류를 위해 정렬
+        potions.sort()
+        return [n - bisect_right(potions, (success-1)//i) for i in spells]
