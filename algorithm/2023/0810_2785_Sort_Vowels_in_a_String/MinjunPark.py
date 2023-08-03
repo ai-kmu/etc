2688b26ca81d474f65b40c44f185b782c5906acd
mjun4138 <39406901+mjun4138@users.noreply.github.com>
2023-08-03 17:50:07 +0900

    Update MinjunPark.py

--- algorithm/2023/0810_2785_Sort_Vowels_in_a_String/MinjunPark.py
+++ algorithm/2023/0810_2785_Sort_Vowels_in_a_String/MinjunPark.py
@@ -1,18 +1,19 @@
 class Solution:
     def sortVowels(self, s: str) -> str:
-        vs = set(['A', 'E', 'I', 'O', 'U', 'a', 'e', 'i', 'o', 'u'])
-        vl = []
-        indices = []
+        vs = set(['A', 'E', 'I', 'O', 'U', 'a', 'e', 'i', 'o', 'u'])  # 모음 집합
+        vl = []  # 찾은 모음
+        indices = []  # 모음의 위치
 
-        for idx, letter in enumerate(s):
-            if letter in vs:
-                indices.append(idx)
-                vl.append(letter)
-
-        vl.sort(key=lambda x: -ord(x))
         s = list(s)
 
-        for idx in indices:
+        for idx, letter in enumerate(s):
+            if letter in vs:  # letter가 모음일 경우
+                indices.append(idx)  # 그 위치를 저장
+                vl.append(letter)  # letter를 저장
+
+        vl.sort(key=lambda x: -ord(x))  # 뒤쪽 index부터 뺄 것이기 때문에 내림차순 정렬
+        
+        for idx in indices:  # 미리 찾아놓은 모음 위치에서 ASCII값이 작은 순으로 교체
             s[idx] = vl.pop()
 
         return ''.join(s)
