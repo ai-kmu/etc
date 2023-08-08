5107ca9f908ef45f239805ae7c24454d235fda5f
Kang4882 <85673206+Kang4882@users.noreply.github.com>
2023-08-09 00:04:24 +0900

    Update MinjunKang.py

--- algorithm/2023/0810_2785_Sort_Vowels_in_a_String/MinjunKang.py
+++ algorithm/2023/0810_2785_Sort_Vowels_in_a_String/MinjunKang.py
@@ -1,16 +1,19 @@
 class Solution:
     def sortVowels(self, s: str) -> str:
-        indices = []
         vowels = ['a', 'e', 'i', 'o', 'u']
+        # 모음 인덱스, 값 추출
+        indices = []
+        v = []
         for i, _ in enumerate(s):
             if _.lower() in vowels:
                 indices.append(i)
-        v = []
-        for _ in indices:
-            v.append(s[_])
+                v.append(_)
+        # 정렬
         v.sort()
+        # 기존 s에 정렬된 모음 값으로 대치를 위해 charactor 단위로 분리
         ans = [*s]
+        # 순서에 따라 모음 대치
         if indices:
             for i, k in enumerate(v):
-                ans[indices[i]] = v
-        print(''.join(ans))
+                ans[indices[i]] = k
+        return ''.join(ans)
