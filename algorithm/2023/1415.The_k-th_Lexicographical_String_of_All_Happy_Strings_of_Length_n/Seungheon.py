2d2b08ebdcd2c541a3ad871aec9b0a968a55af4c
SeungHeon <84365977+song1248@users.noreply.github.com>
2023-08-22 00:00:51 +0900

    Update Seungheon.py

--- algorithm/2023/1415.The_k-th_Lexicographical_String_of_All_Happy_Strings_of_Length_n/Seungheon.py
+++ algorithm/2023/1415.The_k-th_Lexicographical_String_of_All_Happy_Strings_of_Length_n/Seungheon.py
@@ -3,11 +3,14 @@ class Solution(object):
 
         char_list = ['a', 'b', 'c']
 
+        # 재귀적으로 탐색
         def make_list(prv_chr='@', ch_list=[], tmp_list=[]):
+            # 길이가 n 이되면 그만 탑색하고 값을 tmp_list에 저장
             if len(ch_list) == n:
                 tmp_list.append(''.join(ch_list))
                 return tmp_list
-                
+            
+            # 같은 char이 아닐때만 탐색
             for i, ch in enumerate(char_list):
                 if prv_chr == ch:
                     continue
@@ -17,7 +20,8 @@ class Solution(object):
 
         tmp_list = make_list()
         tmp_list.sort()
-
+        
+        # 범위 밖이면 ""를 return
         try:
             return tmp_list[k-1]
         except:
