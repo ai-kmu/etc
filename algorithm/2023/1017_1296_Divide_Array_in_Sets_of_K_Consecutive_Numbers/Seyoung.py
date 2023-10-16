2724d8506614808a6acf5dd207f11b7849740d45
SeYoung Choi <123931913+sese502@users.noreply.github.com>
2023-10-16 16:04:30 +0900

    Update Seyoung.py

--- algorithm/2023/1017_1296_Divide_Array_in_Sets_of_K_Consecutive_Numbers/Seyoung.py
+++ algorithm/2023/1017_1296_Divide_Array_in_Sets_of_K_Consecutive_Numbers/Seyoung.py
@@ -1,17 +1,19 @@
- class Solution(object):
+class Solution(object):
     def isPossibleDivide(self, nums, k):
-        """
-        :type nums: List[int]
-        :type k: int
-        :rtype: bool
-        """
         from collections import Counter
         count_map = Counter(nums)
+        
+        # 숫자를 오름차순으로 정렬
         for num in sorted(count_map.keys()):
+            # 이미 사용된 숫자라면 건너뛴다 (빈도가 0 이하)
             if count_map[num] <= 0:
                 continue
+            
+            # 현재 숫자로부터 k만큼 큰 숫자의 빈도를 감소시키고, 음수가 되면 False를 반환
             for index in range(1, k):
-                count_map[num+index] -= count_map[num]
-                if count_map[num+index] < 0:
+                count_map[num + index] -= count_map[num]
+                if count_map[num + index] < 0:
                     return False
+        
+        # 모든 숫자를 반복하고도 조건을 만족하면 True를 반환
         return True
