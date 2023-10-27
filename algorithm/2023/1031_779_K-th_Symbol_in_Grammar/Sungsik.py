627b6d4601e9c1a74a02dd6c4f46cdfdd56e199b
Kim Sungsik <sskim0126@kookmin.ac.kr>
2023-10-27 15:10:13 +0900

    Create Sungsik.py

---
+++ algorithm/2023/1031_779_K-th_Symbol_in_Grammar/Sungsik.py
@@ -0,0 +1,18 @@
+class Solution:
+    def kthGrammar(self, n: int, k: int) -> int:
+        '''
+        0
+        0 1
+        0 1 1 0
+        규칙 : binary tree로 고려하고, 
+              k에 값에 의해 왼쪽으로 갈지, 오른쪽으로 갈지 결정
+              오른쪽으로 갈 경우 bit를 flop
+        '''
+        bit = 0
+        
+        for i in range(n-2, -1, -1):
+            if k > 2 ** i:
+                k -= 2 ** i
+                bit = 1 - bit
+        
+        return bit
