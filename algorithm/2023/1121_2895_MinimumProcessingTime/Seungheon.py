0b50c648755feaf83364182a3766fe8b2d257fae
SeungHeon <84365977+song1248@users.noreply.github.com>
2023-11-19 21:11:48 +0900

    Create Seungheon.py

---
+++ algorithm/2023/1121_2895_MinimumProcessingTime/Seungheon.py
@@ -0,0 +1,10 @@
+class Solution:
+    def minProcessingTime(self, processorTime: List[int], tasks: List[int]) -> int:
+
+        # 시간이 긴 task 부터 처리하기 위함
+        # 4개씩 슬라이싱 프로세스 할당
+
+        tasks.sort(reverse = True)
+        processorTime.sort()
+            
+        return max([pt + max(tasks[i * 4: (i + 1) * 4]) for i, pt in enumerate(processorTime)])
