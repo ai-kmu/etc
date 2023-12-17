5f69c787cad179db7ab92c33520b6eeae2075894
yoon1544 <45252342+dbsdmlgus50@users.noreply.github.com>
2023-12-18 00:08:04 +0900

    Update Euihyun.py

--- algorithm/2023/1219_79_Word Search/Euihyun.py
+++ algorithm/2023/1219_79_Word Search/Euihyun.py
@@ -1,4 +1,4 @@
-# 저번주 없었어서 까먹었어요! 푸는중이에요!
+# 실패하고 답 봤습니다. 리뷰 안해주셔도 돼요
 
 class Solution(object):
     def exist(self, board, word):
@@ -7,4 +7,41 @@ class Solution(object):
         :type word: str
         :rtype: bool
         """
-      
+        rows, cols = len(board), len(board[0])
+
+        # 재귀
+        def search(row, col, index):
+            # 단어의 모든 문자를 찾았을 때
+            if index == len(word):
+                return True
+
+            # 보드 범위를 벗어나거나 현재 셀의 문자가 일치하지 않는 경우
+            if (row < 0 or row >= rows or col < 0 or col >= cols or board[row][col] != word[index]):
+                return False
+
+            # 현재 셀을 일시적으로 표시하여 다시 방문하지않음
+            temp, board[row][col] = board[row][col], "*"
+
+            # 상하좌우 인접 셀로 이동하면서 다음 문자를 찾음
+            find = (
+                search(row + 1, col, index + 1)
+                or search(row - 1, col, index + 1)
+                or search(row, col + 1, index + 1)
+                or search(row, col - 1, index + 1)
+            )
+
+            # 탐색이 끝나면 현재 셀을 원래대로 복원
+            board[row][col] = temp
+
+            return find
+
+        # 보드의 모든 셀에서 시작하여 단어를 찾음
+        for row in range(rows):
+            for col in range(cols):
+                if board[row][col] == word[0]:
+                    if search(row, col, 0):
+                        return True
+
+        return False
+
+        
