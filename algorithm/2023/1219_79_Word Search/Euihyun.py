4845e48040d7adaccceda86a014819a3421deeb2
yoon1544 <45252342+dbsdmlgus50@users.noreply.github.com>
2023-12-17 23:57:23 +0900

    Create Euihyun.py

---
+++ algorithm/2023/1219_79_Word Search/Euihyun.py
@@ -0,0 +1,10 @@
+# 저번주 없었어서 까먹었어요! 푸는중이에요!
+
+class Solution(object):
+    def exist(self, board, word):
+        """
+        :type board: List[List[str]]
+        :type word: str
+        :rtype: bool
+        """
+      
