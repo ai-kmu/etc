ae69341bae8428fce51d872d3b111201e37e18b7
SeoJunghyeon <jh04270312@kookmin.ac.kr>
2024-02-06 16:08:17 +0900

    Create Junghyeon.py

---
+++ algorithm/2024/0208_122_Best_Time_to_Buy_and_Sell_Stock_II/Junghyeon.py
@@ -0,0 +1,14 @@
+# DP로 풀이
+class Solution:
+    def maxProfit(self, prices: List[int]) -> int:
+        if not prices:
+            return 0
+        
+        result = 0
+
+        for i in range(1, len(prices)):
+            # 이전 가격보다 클 경우 둘의 차이가 이익이 됨
+            if prices[i] > prices[i - 1]:
+                max_profit += prices[i] - prices[i - 1]
+        
+        return result
