b78bcb0a242bf243e08f4bec291a14e92f4839e4
SeungHeon <84365977+song1248@users.noreply.github.com>
2024-03-29 19:14:16 +0900

    Create Seungheon.py

---
+++ algorithm/2024/0408_539_Minimum_Time_Difference/Seungheon.py
@@ -0,0 +1,15 @@
+class Solution:
+    def findMinDifference(self, timePoints: List[str]) -> int:
+
+        answer = float('inf')
+
+        timePoints.sort()
+
+        time_list = [ int(tp[:2]) * 60 + int(tp[3:]) for tp in timePoints]
+
+        answer = min(abs(time_list[-1] - time_list[0]), abs(time_list[-1] - time_list[0] - 60 * 24))
+
+        for i, t in enumerate(time_list[:-1]):
+            answer = min(abs(time_list[i+1] - time_list[i]), answer)
+
+        return answer
