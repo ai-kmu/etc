c4ae7e65139d798698c34a3c1d3a09b349efd21a
SeungHeon <84365977+song1248@users.noreply.github.com>
2024-07-25 00:47:52 +0900

    Update Seungheon.py

--- algorithm/2024/0721_2415_Reverse_Odd_Levels_of_Binary_Tree/Seungheon.py
+++ algorithm/2024/0721_2415_Reverse_Odd_Levels_of_Binary_Tree/Seungheon.py
@@ -1,30 +1,24 @@
-# fail code
-
-from collections import deque
 
 class Solution:
     def reverseOddLevels(self, root: Optional[TreeNode]) -> Optional[TreeNode]:
         
-        layer = 0
-        layer_len = 2**layer
-        count = 0
+        # 각 even layer의 절반을 순서 교환
 
-        node_list = deque([root])
+        node_list = [root]
+        even = False
+        while node_list[0] is not None:
+            layer_len = len(node_list)
+            next_node_list = []
 
-        while node_list:
-            node = node_list.popleft()
-
-            # 바꾸기
-            if layer % 2 == 1 and count + 1 < layer_len:
-                node.val, node_list[layer_len - 2*(count+1)].val = node_list[layer_len - 2*(count+1)].val, node.val
-            count += 1
-            if count == layer_len:
-                layer += 1
-                layer_len = 2**layer
-                count = 0
-
-            if node.left is not None:
-                node_list.append(node.left)
-                node_list.append(node.right)
+            # layer 마다 순서 교환
+            for i in range(layer_len):
+                # 새로운 node list 만들기
+                next_node_list.append(node_list[i].left)
+                next_node_list.append(node_list[i].right)
+                if even and i < layer_len/2 :
+                    node_list[i].val, node_list[-i-1].val = node_list[-i-1].val, node_list[i].val
+            node_list = next_node_list
+            # Flag 변경
+            even = ~even
 
         return root   
