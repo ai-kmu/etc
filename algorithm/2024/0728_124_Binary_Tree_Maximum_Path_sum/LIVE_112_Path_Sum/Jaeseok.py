a2b7cc66023bf9c351ebe4c055acfe45347425ec
Jae seok Lee <dlwotjraks@gmail.com>
2024-08-09 12:13:28 +0900

    Create Jaeseok.py

---
+++ algorithm/2024/0728_124_Binary_Tree_Maximum_Path_sum/LIVE_112_Path_Sum/Jaeseok.py
@@ -0,0 +1,21 @@
+from collections import deque
+
+
+class Solution:
+    def hasPathSum(self, root: Optional[TreeNode], targetSum: int) -> bool:
+        if root is None:
+            return False
+        answer = False
+        q = deque()
+        q.append((root, root.val))
+        while q:
+            node, val = q.popleft()
+            if val == targetSum and node.left is None and node.right is None:
+                return True
+            if node.left is not None:
+                q.append((node.left, val + node.left.val))
+            if node.right is not None:
+                q.append((node.right, val + node.right.val))
+        
+        return answer
+      
