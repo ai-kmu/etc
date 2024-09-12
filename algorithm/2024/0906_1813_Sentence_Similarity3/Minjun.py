cbe9ca894c940fa19405221ed375e2fcb8b7204b
Kang4882 <85673206+Kang4882@users.noreply.github.com>
2024-09-12 17:50:15 +0900

    Update Minjun.py

--- algorithm/2024/0906_1813_Sentence_Similarity3/Minjun.py
+++ algorithm/2024/0906_1813_Sentence_Similarity3/Minjun.py
@@ -2,50 +2,21 @@ class Solution:
     def areSentencesSimilar(self, sentence1: str, sentence2: str) -> bool:
         a = sentence1.split()
         b = sentence2.split()
-        c = -2
+
         if len(a) > len(b):
-            big = a
-            small = b
+            big, small = a, b
         else:
-            big = b
-            small = a
-
-        if small[-1] != big[-1] and small[0] != big[0]:
-            return False
-        if len(small) == len(big):
-            for i in range(len(small)):
-                if small[i] != big[i]:
-                    return False
+            big, small = b, a
+        idx = 0
+        # 앞에서부터 탐색
+        while idx != len(small):
+            if small[idx] != big[idx]:
+                break
+            idx += 1
+        if idx == len(small):
             return True
-
-        if small[0] != big[0]:
-            idx = -1
-            for i in range(len(small)):
-                if small[idx] != big[idx]:
-                    return False
-                if -idx == len(small):
-                    break
-                idx -= 1
-                
-        if small[-1] != big[-1]:
-            for i in range(len(small)):
-                if small[i] != big[i]:
-                    return False
-        
-        if small[0] == big[0] and small[-1] == big[-1]:
-            prefix = 0
-            for i in range(len(small)):
-                if small[i] != big[i]:
-                    prefix = i
-                    break
-            small = small[prefix:]
-            big = big[prefix:]
-            idx = -1
-            for i in range(len(small)):
-                print(small, big)
-                if -idx == len(small):
-                    break
-                if small[idx] != big[idx]:
-                    return False
-                idx -= 1
-        return True
+        print(idx)
+        # 뒤에서부터 탐색
+        if small[idx-len(small):] == big[idx-len(small):]:
+            return True
+        return False
