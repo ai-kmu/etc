2abab295e0506f4ba71d0b4c1df55fb786f87783
HAI-syz <100561089+HAI-syz@users.noreply.github.com>
2024-09-27 13:21:33 +0900

    Update Wooseong.py

--- algorithm/2024/0920_0423_Reconstruct_Original_Digits_from_English/151_Reverse_Words_in_a_String/Wooseong.py
+++ algorithm/2024/0920_0423_Reconstruct_Original_Digits_from_English/151_Reverse_Words_in_a_String/Wooseong.py
@@ -1,3 +1,6 @@
 class Solution:
     def reverseWords(self, s: str) -> str:
+        # s.split() -> words를 word로 찢음
+        # [::-1] -> 순서 뒤집음
+        # ' '.join() -> word를 words로 합침
         return ' '.join(s.split()[::-1])
