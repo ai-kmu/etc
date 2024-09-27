d49f64b1e99f06a50a4c5a4fcd6f38fa61c4d2a2
justinday123 <45382680+justinday123@users.noreply.github.com>
2024-09-27 13:31:06 +0900

    Create Jaewoong.py

---
+++ algorithm/2024/0920_0423_Reconstruct_Original_Digits_from_English/151_Reverse_Words_in_a_String/Jaewoong.py
@@ -0,0 +1,39 @@
+class Solution:
+    def reverseWords(self, s: str) -> str:
+        word_list = []
+        for i in s:
+            word_list.append(i)
+        
+        word = ''
+        all_word = []
+
+        if word_list[-1] != ' ':
+            word_list.append(' ')
+        
+        for i in word_list:
+            if i != ' ':
+                word += i
+            elif i == ' ':
+                all_word.append(word)
+                word = ''
+        ans = ''
+
+        reverse_all_word = list(reversed(all_word))
+        print(reverse_all_word)
+        reverse_all_word_removed = []
+        for i in reverse_all_word:
+            if i != '':
+                reverse_all_word_removed.append(i)
+
+        for i in range(len(reverse_all_word_removed)):
+            if i != len(reverse_all_word_removed) - 1:
+                ans += reverse_all_word_removed[i]
+                ans += ' '
+            else:
+                ans += reverse_all_word_removed[i]
+        
+        return ans
+            
+        
+
+            
