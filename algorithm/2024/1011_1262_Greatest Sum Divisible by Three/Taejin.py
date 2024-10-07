474c6620239fe93d0d5cea8a2f0abb60d6c197df
Taej-P <78579124+Taej-P@users.noreply.github.com>
2024-10-07 14:04:00 +0900

    Update Taejin.py

--- algorithm/2024/1011_1262_Greatest Sum Divisible by Three/Taejin.py
+++ algorithm/2024/1011_1262_Greatest Sum Divisible by Three/Taejin.py
@@ -6,10 +6,7 @@ class Solution:
         # 합, 모듈로 3 계산
         sums = sum(nums)
         remainder = sums % 3
-        
-        # 모듈로 3 = 0이면 영향 없으므로 1, 2만 리스트로
-        modulos = [[i for i in nums if i % 3 == j + 1] for j in range(2)]
-        
+                
         # sums (mod 3) = 0이면 그대로 합계 반환
         if remainder == 0:
             return sums
@@ -19,13 +16,15 @@ class Solution:
             # remainder = 1인 경우 -> 요소에서 나머지가 1인 것 하나 또는 나머지가 2인 것 두개 제외
             # remainder = 1인 경우 -> 요소에서 나머지가 2인 것 하나 또는 나머지가 1인 것 두개 제외
         else:
+            # 모듈로 3 = 0이면 영향 없으므로 1, 2만 리스트로하며 최대 2개만 쓰므로 2개만 사용하도록
+            modulos = [[i for i in nums if i % 3 == j + 1][:2] for j in range(2)]
             # 최솟값 초기화
             mins = nums[-1]
             
             if modulos[remainder - 1]:
                 mins = modulos[remainder - 1][0]
             
-            if len(modulos[2 - remainder]) >= 2:
-                mins = min(mins, sum(modulos[2 - remainder][:2]))
+            if len(modulos[2 - remainder]) == 2:
+                mins = min(mins, sum(modulos[2 - remainder]))
 
             return sums - mins
