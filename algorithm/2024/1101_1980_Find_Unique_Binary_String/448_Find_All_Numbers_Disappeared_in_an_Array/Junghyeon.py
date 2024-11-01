7206f3e0d9e2f1d2b5b6fd137eb9324239f777e8
SeoJunghyeon <jh04270312@kookmin.ac.kr>
2024-11-01 14:03:18 +0900

    Create Junghyeon.py

---
+++ algorithm/2024/1101_1980_Find_Unique_Binary_String/448_Find_All_Numbers_Disappeared_in_an_Array/Junghyeon.py
@@ -0,0 +1,19 @@
+class Solution:
+    def findDisappearedNumbers(self, nums: List[int]) -> List[int]:
+        from collections import defaultdict
+
+        d = defaultdict(int)
+
+        result =[]
+
+        max_nums = max(nums)
+        max_nums = max(max_nums, len(nums))
+
+        for i in nums:
+            d[i] = 1
+
+        for i in range(1, max_nums+1):
+            if i not in d.keys():
+                result.append(i)
+        
+        return result
