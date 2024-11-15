65678f94cf5f6524cffa409238e00210d51f62e1
Taej-P <78579124+Taej-P@users.noreply.github.com>
2024-11-15 15:42:19 +0900

    Delete algorithm/2024/1115_1914_Cyclically_Rotating_a_Grid/832_Flipping_an_Image directory

--- algorithm/2024/1115_1914_Cyclically_Rotating_a_Grid/832_Flipping_an_Image/Sungsik.py
+++
@@ -1,3 +0,0 @@
-class Solution:
-    def flipAndInvertImage(self, image: List[List[int]]) -> List[List[int]]:
-        return [[1 - x for x in reversed(row)] for row in image]
--- algorithm/2024/1115_1914_Cyclically_Rotating_a_Grid/832_Flipping_an_Image/Taejin.py
+++
@@ -1,3 +0,0 @@
-class Solution:
-    def flipAndInvertImage(self, image: List[List[int]]) -> List[List[int]]:
-        return [[int(not j) for j in i][::-1] for i in image]
