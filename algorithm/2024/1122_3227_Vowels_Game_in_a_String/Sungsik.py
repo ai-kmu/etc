c4d54057955f2f94e10049765a488d2059838a59
Kim Sungsik <sskim0126@kookmin.ac.kr>
2024-11-18 13:25:08 -0800

    Update Sungsik.py

--- algorithm/2024/1122_3227_Vowels_Game_in_a_String/Sungsik.py
+++ algorithm/2024/1122_3227_Vowels_Game_in_a_String/Sungsik.py
@@ -1,6 +1,6 @@
 class Solution:
     def doesAliceWin(self, s: str) -> bool:
-        # Alice가 있는 필승법이 있을 것 같다.
+        # Alice가 이기는 필승법이 있을 것 같다.
         # 그래서 모음이 하나라도 없는 경우를 제외하고 True를 리턴했더니 풀렸다.
         vowels = set(['a','i','e','o','u'])
         for c in s:
