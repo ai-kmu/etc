ed7307e1bbf16059d9915b8fdbe9c5f2d16461fa
YSKang <ysck73@kookmin.ac.kr>
2024-11-20 22:23:44 +0900

    Create Yunseok.py

---
+++ algorithm/2024/1122_3227_Vowels_Game_in_a_String/Yunseok.py
@@ -0,0 +1,48 @@
+# 문제 풀이 실패
+# 문제를 잘못 이해하였음
+class Solution:
+
+    def reindex_vowels_list(self, vowels_list, target_idx):
+        if target_idx == -1:
+            for idx in range(len(vowels_list)):
+                vowels_list[idx] -= 1
+            return vowels_list
+
+        reoffset_value = vowels_list[target_idx] + 1
+        for idx in range(len(vowels_list)):
+            vowels_list[idx] -= reoffset_value
+    
+        return vowels_list
+
+
+    def doesAliceWin(self, s: str) -> bool:
+        # 초기화
+        is_alices_turn = True
+        next_str = s
+        vowels_idx_list = []
+        for idx, elem in enumerate(next_str):
+            if elem in ['a', 'e', 'i', 'o', 'u']:
+                vowels_idx_list.append(idx)
+
+        while True:
+            vowels_left_cnt = len(vowels_idx_list)
+            if vowels_left_cnt == 0:
+                return False
+            elif vowels_left_cnt == 1 and vowels_idx_list[0] == 0:
+                return True
+
+            is_even = True if vowels_left_cnt % 2 == 0 else False
+            if is_alices_turn: # 앨리스 턴은 홀수일 경우 카운트
+                target_idx = vowels_left_cnt if is_even is not True else vowels_left_cnt - 1
+                is_alices_turn = False
+            else:
+                target_idx = vowels_left_cnt if is_even is True else vowels_left_cnt - 1
+                is_alices_turn = True
+                if target_idx == 0:
+                    vowels_idx_list = self.reindex_vowels_list(vowels_idx_list, -1)
+                    next_str = next_str[1:]
+                    continue
+
+            target_idx_value = vowels_idx_list[target_idx]
+            vowels_idx_list = self.reindex_vowels_list(vowels_idx_list, target_idx - 1)[target_idx:]
+            next_str = next_str[target_idx_value - 1:]
