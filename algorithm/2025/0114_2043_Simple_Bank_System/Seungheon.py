fe454e414872535bbf38aa7cb92aa5a6f59a0fc9
SeungHeon <84365977+song1248@users.noreply.github.com>
2025-01-13 14:25:13 +0900

    Update Seungheon.py

--- algorithm/2025/0114_2043_Simple_Bank_System/Seungheon.py
+++ algorithm/2025/0114_2043_Simple_Bank_System/Seungheon.py
@@ -1,3 +1,15 @@
+
+# 아래 코드 간단하게 바꾼 버전. 데코레이터 사용(공부하려면 이거 리뷰하는게 좋을듯)
+
+# 데코레이터 생성, try except으로 false 처리
+def operation_func(func):
+    def wrapper(*args, **kwargs):
+        try:
+            return func(*args, **kwargs)
+        except :
+            return False
+    return wrapper
+
 class Bank(object):
 
     def __init__(self, balance):
@@ -6,6 +18,7 @@ class Bank(object):
         """
         self.money = balance
 
+    @operation_func
     def transfer(self, account1, account2, money):
         """
         :type account1: int
@@ -13,47 +26,38 @@ class Bank(object):
         :type money: int
         :rtype: bool
         """
-
-        try:
-            account1 -= 1
-            account2 -= 1
-            if self.money[account1] >=  money and self.money[account2] is not None: 
-                self.money[account1] -= money
-                self.money[account2] += money
-                return True
-            else:
-                return False
-        except:
+        account1 -= 1
+        account2 -= 1
+        if self.money[account1] >= money and self.money[account2] is not None:
+            self.money[account1] -= money
+            self.money[account2] += money
+            return True
+        else:
             return False
-        
 
+    @operation_func
     def deposit(self, account, money):
         """
         :type account: int
         :type money: int
         :rtype: bool
         """
-        try:
-            account -= 1
-            self.money[account] += money
-            return True
-        except:
-            return False
-        
+        account -= 1
+        self.money[account] += money
+        return True
+
+    @operation_func
     def withdraw(self, account, money):
         """
         :type account: int
         :type money: int
         :rtype: bool
         """
-        try:
-            account -= 1
-            if self.money[account] >=  money:
-                self.money[account] -= money
-                return True
-            else:
-                return False
-        except:
+        account -= 1
+        if self.money[account] >= money:
+            self.money[account] -= money
+            return True
+        else:
             return False
 
 
@@ -62,3 +66,72 @@ class Bank(object):
 # param_1 = obj.transfer(account1,account2,money)
 # param_2 = obj.deposit(account,money)
 # param_3 = obj.withdraw(account,money)
+
+
+# Your Bank object will be instantiated and called as such:
+# obj = Bank(balance)
+# param_1 = obj.transfer(account1,account2,money)
+# param_2 = obj.deposit(account,money)
+# param_3 = obj.withdraw(account,money)
+
+
+
+# class Bank(object):
+
+#     def __init__(self, balance):
+#         """
+#         :type balance: List[int]
+#         """
+#         self.money = balance
+
+#     def transfer(self, account1, account2, money):
+#         """
+#         :type account1: int
+#         :type account2: int
+#         :type money: int
+#         :rtype: bool
+#         """
+
+#         try:
+#             account1 -= 1
+#             account2 -= 1
+#             if self.money[account1] >=  money and self.money[account2] is not None: 
+#                 self.money[account1] -= money
+#                 self.money[account2] += money
+#                 return True
+#             else:
+#                 return False
+#         except:
+#             return False
+        
+
+#     def deposit(self, account, money):
+#         """
+#         :type account: int
+#         :type money: int
+#         :rtype: bool
+#         """
+#         try:
+#             account -= 1
+#             self.money[account] += money
+#             return True
+#         except:
+#             return False
+        
+#     def withdraw(self, account, money):
+#         """
+#         :type account: int
+#         :type money: int
+#         :rtype: bool
+#         """
+#         try:
+#             account -= 1
+#             if self.money[account] >=  money:
+#                 self.money[account] -= money
+#                 return True
+#             else:
+#                 return False
+#         except:
+#             return False
+
+
