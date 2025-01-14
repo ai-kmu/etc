31a8f7b9007df37f9223e628d8da799c1e93c848
SeungHeon <84365977+song1248@users.noreply.github.com>
2025-01-14 16:19:21 +0900

    Update Seungheon.py

--- algorithm/2025/0114_2043_Simple_Bank_System/2293_Min_Max_Game/Seungheon.py
+++ algorithm/2025/0114_2043_Simple_Bank_System/2293_Min_Max_Game/Seungheon.py
@@ -10,10 +10,9 @@ class Solution(object):
                 return nums[0]
             else:
                 new_nums = []
-                half = len(nums)/2/2
                 for j in range(len(nums)/2):
                     i = 2*j
-                    if i % 2 == 0:
+                    if j % 2 == 0:
                         new_nums.append(min(nums[i],nums[i+1]))
                     else:
                         new_nums.append(max(nums[i],nums[i+1]))
