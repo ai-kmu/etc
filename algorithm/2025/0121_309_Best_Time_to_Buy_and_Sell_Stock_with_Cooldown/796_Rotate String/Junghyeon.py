d6fa31a0a8f85342379d5547bfcca9ecde442375
SeoJunghyeon <jh04270312@kookmin.ac.kr>
2025-01-21 15:17:16 +0900

    Create Junghyeon.py

---
+++ algorithm/2025/0121_309_Best_Time_to_Buy_and_Sell_Stock_with_Cooldown/796_Rotate String/Junghyeon.py
@@ -0,0 +1,8 @@
+class Solution:
+    def rotateString(self, s: str, goal: str) -> bool:
+        for _ in range(len(s)):
+            s = s[1:] + s[0]
+            if s == goal:
+                return True
+            
+        return False
