2d0cb741419b9cd73982791c98b289d0a03763f2
Jae seok Lee <dlwotjraks@gmail.com>
2025-02-04 14:16:02 +0900

    Update Jaeseok.py

--- algorithm/2025/0204_73_Set_Matrix_Zeroes/1582_special_positions_in_a_binary_matrix/Jaeseok.py
+++ algorithm/2025/0204_73_Set_Matrix_Zeroes/1582_special_positions_in_a_binary_matrix/Jaeseok.py
@@ -18,4 +18,24 @@ class Solution:
         return answer
 
 
-class 
+# 더 나은 풀이
+from collections import Counter
+
+class Solution:
+    def numSpecial(self, mat: List[List[int]]) -> int:
+        answer = 0
+        m, n = len(mat), len(mat[0])
+        rowsum = [0] * m
+        colsum = [0] * n
+        for i in range(m):
+            rowsum[i] = mat[i].count(1)
+
+        for j in range(n):
+            colsum[j] = Counter(row[j] for row in mat)[1]
+            
+        for i in range(m):
+            for j in range(n):
+                if mat[i][j] == 1 and rowsum[i] == 1 and colsum[j] == 1:
+                    answer += 1
+                    
+        return answer
