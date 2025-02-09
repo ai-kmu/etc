0428a0a6fecc991c04c52f696320b715122aadfa
Kim Sungsik <sskim0126@kookmin.ac.kr>
2025-02-08 17:09:27 -0800

    Create Sungsik.py

---
+++ algorithm/2025/0211_1424_Diagonal_Traverse_II/Sungsik.py
@@ -0,0 +1,13 @@
+class Solution:
+    def findDiagonalOrder(self, nums: List[List[int]]) -> List[int]:
+        m, n = len(nums), max([len(row) for row in nums])
+        
+        diagonals = [[] for _ in range(m+n-1)]
+
+        for i, row in enumerate(nums):
+            for j, num in enumerate(row):
+                # 같은 대각선의 num은 i + j가 같음을 이용
+                diagonals[i+j].append(num)
+        
+        # 역순으로 집어넣었기 때문에 diagonal을 reverse
+        return [num for diagonal in diagonals for num in reversed(diagonal)]
