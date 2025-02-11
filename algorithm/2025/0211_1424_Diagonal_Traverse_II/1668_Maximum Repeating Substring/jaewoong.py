e1e659b538c47389133df342a40bc39c655a1285
justinday123 <45382680+justinday123@users.noreply.github.com>
2025-02-11 14:34:47 +0900

    Update jaewoong.py

--- algorithm/2025/0211_1424_Diagonal_Traverse_II/1668_Maximum Repeating Substring/jaewoong.py
+++ algorithm/2025/0211_1424_Diagonal_Traverse_II/1668_Maximum Repeating Substring/jaewoong.py
@@ -1,1 +1,8 @@
-
+class Solution:
+    def maxRepeating(self, sequence: str, word: str) -> int:
+        k = 0
+        repeated_word = word
+        while repeated_word in sequence:
+            k += 1
+            repeated_word = word * (k + 1)
+        return k        
