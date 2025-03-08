55c6030cd0e0b95f536988d312af61640e31e06b
YSKang <ysck73@kookmin.ac.kr>
2025-03-08 22:37:19 +0900

    Create Yunseok.py

---
+++ algorithm/2025/0225_54_Spiral_Matrix/Yunseok.py
@@ -0,0 +1,31 @@
+class Solution:
+    def spiralOrder(self, matrix: List[List[int]]) -> List[int]:
+        if not matrix or not matrix[0]:
+            return []
+        
+        result = []
+        top = 0
+        bottom = len(matrix) - 1
+        left = 0
+        right = len(matrix[0]) - 1
+
+        while left <= right and top <= bottom:
+            for col in range(left, right + 1):
+                result.append(matrix[top][col])
+            top += 1
+
+            for row in range(top, bottom + 1):
+                result.append(matrix[row][right])
+            right -= 1
+
+            if top <= bottom:
+                for col in range(right, left - 1, -1):
+                    result.append(matrix[bottom][col])
+                bottom -= 1
+
+            if left <= right:
+                for row in range(bottom, top - 1, -1):
+                    result.append(matrix[row][left])
+                left += 1
+
+        return result
