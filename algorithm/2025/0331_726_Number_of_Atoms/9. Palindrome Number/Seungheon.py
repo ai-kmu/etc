11aeab2d58faebad5e8ab2b10c998e9793903454
SeungHeon <84365977+song1248@users.noreply.github.com>
2025-03-31 13:02:44 +0900

    Create Seungheon.py

---
+++ algorithm/2025/0331_726_Number_of_Atoms/9. Palindrome Number/Seungheon.py
@@ -0,0 +1,12 @@
+class Solution(object):
+    def isPalindrome(self, x):
+        """
+        :type x: int
+        :rtype: bool
+        """
+        x = str(x)
+        for i in range(len(x)):
+            if x[i] != x[-1-i]:
+                return False
+
+        return True
