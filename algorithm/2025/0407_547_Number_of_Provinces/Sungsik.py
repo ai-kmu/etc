06c2cb7745f000ab7adcecab2fa391e9cfc653b5
Kim Sungsik <sskim0126@kookmin.ac.kr>
2025-04-03 19:16:06 +0900

    Create Sungsik.py

---
+++ algorithm/2025/0407_547_Number_of_Provinces/Sungsik.py
@@ -0,0 +1,22 @@
+from collections import deque
+
+class Solution:
+    def findCircleNum(self, isConnected: List[List[int]]) -> int:
+        n = len(isConnected)
+        answer = 0
+        visited = [False] * n
+
+        for i in range(n):
+            if visited[i]:
+                continue
+            answer += 1
+            visited[i] = True
+            # DFS로 연결된 node를 방문
+            queue = deque([i])
+            while queue:
+                node = queue.popleft()
+                for j in range(n):
+                    if node != j and isConnected[node][j] and not visited[j]:
+                        visited[j] = True
+                        queue.append(j)
+        return answer
