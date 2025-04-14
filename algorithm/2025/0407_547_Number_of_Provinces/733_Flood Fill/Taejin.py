146463941102c4364399012cb75271a5cd0adbde
Taej-P <78579124+Taej-P@users.noreply.github.com>
2025-04-14 12:54:47 +0900

    Update Taejin.py

--- algorithm/2025/0407_547_Number_of_Provinces/733_Flood Fill/Taejin.py
+++ algorithm/2025/0407_547_Number_of_Provinces/733_Flood Fill/Taejin.py
@@ -4,7 +4,7 @@ class Solution:
         visited = [[False] * len(image[0]) for _ in range(len(image))]
         target = image[sr][sc]
 
-        def bfs(x, y):
+        def change(x, y):
             nonlocal image, visited, target
             if not visited[x][y] and (image[x][y] == target):
                 visited[x][y] = True
@@ -13,9 +13,9 @@ class Solution:
                 for dx, dy in directions:
                     new_x, new_y = x + dx, y + dy
                     if (0 <= new_x < len(image)) and (0 <= new_y < len(image[0])):
-                        bfs(new_x, new_y)
+                        change(new_x, new_y)
 
-        bfs(sr, sc)
+        change(sr, sc)
         return image
 
 
