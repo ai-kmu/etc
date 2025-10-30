633ead528a3667a5fdab7d20525e78ba6826d09d
Kim Jiwon <71117552+KJW988@users.noreply.github.com>
2025-10-30 01:01:31 -0700

    Create Jiwon.py

---
+++ algorithm/2025/1029_1996_The_Number_of_Weak_Characters_in_the_Game/Jiwon.py
@@ -0,0 +1,15 @@
+class Solution:
+    def numberOfWeakCharacters(self, properties: List[List[int]]) -> int:
+        # attack 오름차순, defense 내림차순
+        properties.sort(key=lambda x: (x[0], -x[1]))
+        cnt = 0
+        max_defense = -9999  # 임시값
+
+        # 공격력 큰 순으로 탐색
+        for a, d in reversed(properties):
+            if d < max_defense:
+                cnt += 1
+            else:
+                max_defense = d
+
+        return cnt
