a37933216d818cf529560c307806fe736359e7f6
SeoJunghyeon <jh04270312@kookmin.ac.kr>
2025-12-03 10:56:49 -0800

    Implement numWaterBottles function in Junghyeon.py

---
+++ algorithm/2025/1129_1518_Water_Bottles/Junghyeon.py
@@ -0,0 +1,9 @@
+class Solution(object):
+    def numWaterBottles(self, numBottles, numExchange):
+        """
+        :type numBottles: int
+        :type numExchange: int
+        :rtype: int
+        """
+        return (numBottles * numExchange - 1)//(numExchange - 1)
+        
