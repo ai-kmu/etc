d8c1f993f662ee1cc30d61c4a6604f8f37ed0836
Taej-P <78579124+Taej-P@users.noreply.github.com>
2026-01-05 11:02:14 +0900

    Delete algorithm/2026/0108_179_Largest_Number directory

--- algorithm/2026/0108_179_Largest_Number/Taejin.py
+++
@@ -1,3 +0,0 @@
-class Solution:
-    def largestNumber(self, nums: List[int]) -> str:
-        return str(int("".join(sorted([str(i) for i in nums], key=lambda x: [x + x * (10 - len(x))][:10], reverse=True))))
