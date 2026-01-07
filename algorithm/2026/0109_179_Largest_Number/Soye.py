369adc75b597199058a9b85a033ed18eee96c90e
SoyE <55877726+soyekwon@users.noreply.github.com>
2026-01-07 17:24:12 +0900

    Create Soye.py

---
+++ algorithm/2026/0109_179_Largest_Number/Soye.py
@@ -0,0 +1,11 @@
+# 솔루션 참고 했습니다. 
+class Solution:
+    def largestNumber(self, nums: List[int]) -> str:
+        for i in range(len(nums)):
+            for j in range(0,len(nums)-1-i):
+                if str(nums[j]) + str(nums[j+1]) < str(nums[j+1]) + str(nums[j]):
+                    nums[j], nums[j+1] = nums[j+1], nums[j]
+                  
+        result = list(map(str, nums))
+      
+        return ''.join(result) if nums[0] != 0 else "0"
