eb91084c3001d6df5e3558abd61fcb8aa8001247
Kang4882 <85673206+Kang4882@users.noreply.github.com>
2026-02-11 23:55:53 +0900

    Create Minjun.py

---
+++ algorithm/2026/0206_189_Rotate_Array/Minjun.py
@@ -0,0 +1,11 @@
+class Solution:
+    def rotate(self, nums: List[int], k: int) -> None:
+        n = len(nums)
+        k = k % n
+        rotated = [0] * n
+
+        for i in range(n):
+            rotated[(i + k) % n] = nums[i]
+        
+        for i in range(n):
+            nums[i] = rotated[i]
