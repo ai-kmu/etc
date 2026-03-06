51b17b88703ae365483025e73135382bab2fa670
justinday123 <45382680+justinday123@users.noreply.github.com>
2026-03-06 12:50:19 +0900

    Create Jaewoong.py

---
+++ algorithm/2026/0227_593_Valid_Square/643. Maximum Average Subarray/Jaewoong.py
@@ -0,0 +1,12 @@
+from typing import List
+
+class Solution:
+    def findMaxAverage(self, nums: List[int], k: int) -> float:
+        curr_sum = sum(nums[:k])
+        max_sum = curr_sum
+
+        for i in range(k, len(nums)):
+            curr_sum = curr_sum - nums[i - k] + nums[i]
+            max_sum = max(max_sum, curr_sum)
+
+        return max_sum / k
