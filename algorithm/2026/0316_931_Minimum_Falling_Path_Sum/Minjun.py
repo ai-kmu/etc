bf50989ba2700a74aabe2bd56c54ec3b75f8c65d
Kang4882 <85673206+Kang4882@users.noreply.github.com>
2026-03-18 18:43:55 +0900

    Update Minjun.py

--- algorithm/2026/0316_931_Minimum_Falling_Path_Sum/Minjun.py
+++ algorithm/2026/0316_931_Minimum_Falling_Path_Sum/Minjun.py
@@ -1,40 +1,22 @@
-# 솔루션봤음. 어렵다.
+# 나와 인접한 행 중 나 포함 좌우 3개 열만 탐색해서 계산하고, 이를 전부 계산하면 됨. 
+# 완전 탐색의 경우 3^n-1승임
+# 점화식 세워서 dp로 가즈아
+# 내 위에 행 3개 열에 대해서 최소값 더해가기
 class Solution:
-    def minFallingPathSum(self, A: List[List[int]]) -> int:
-        m, n = len(A), len(A[0])
-
-        if m == 1 or n == 1:
-            return A[0][0]
-
-        dp = [[float('inf')] * n for _ in range(m)]
-        ans = float('inf')
-
-        for i in range(len(A)):
-            ans = min(ans, self.minFallingPathSumHelper(A, 0, i, dp))
-
-        return ans
-
-    def minFallingPathSumHelper(self, A, row, col, dp):
-        m, n = len(A), len(A[0])
-
-        if dp[row][col] != float('inf'):
-            return dp[row][col]
-
-        if row == m - 1:
-            return A[row][col]
-
-        left = right = float('inf')
-
-        if col > 0:
-            left = self.minFallingPathSumHelper(A, row + 1, col - 1, dp)
-
-        straight = self.minFallingPathSumHelper(A, row + 1, col, dp)
-
-        if col < n - 1:
-            right = self.minFallingPathSumHelper(A, row + 1, col + 1, dp)
-
-        dp[row][col] = min(left, min(straight, right)) + A[row][col]
-
-        return dp[row][col]
-
-
+    def minFallingPathSum(self, matrix: List[List[int]]) -> int:
+        dp = [[0] * len(matrix) for i in range(len(matrix))]
+        # 첫행 나로 초기화
+        for i in range(len(matrix)):
+            dp[0][i] = matrix[0][i]
+        # 두번째 행부터 바로 윗 행에서 나포함 좌우 탐색해서 최소값 더함
+        for row in range(1, len(matrix)):
+            for col in range(len(matrix)):
+                tmp = dp[row-1][col]
+                if col > 0:
+                    if tmp > dp[row-1][col-1]:
+                        tmp = min(dp[row-1][col-1], tmp)
+                if col < len(matrix)-1:
+                    if tmp > dp[row-1][col+1]:
+                        tmp = min(dp[row-1][col+1], tmp)
+                dp[row][col] = tmp + matrix[row][col]
+        return min(dp[-1])
