c7107d1c6f9519c2cde369b6e8ae0bb05fd63506
Kim Jiwon <71117552+KJW988@users.noreply.github.com>
2026-03-18 23:40:55 +0900

    Create Jiwon.py

---
+++ algorithm/2026/0316_931_Minimum_Falling_Path_Sum/Jiwon.py
@@ -0,0 +1,27 @@
+# DP (Solution 참고)
+class Solution:
+    def minFallingPathSum(self, matrix: List[List[int]]) -> int:
+        row = len(matrix)
+        col = len(matrix[0])
+        dp = []
+
+        for i in range(row):
+            curr = []
+            for j in range(col):
+                curr.append(0)
+            dp.append(curr)
+
+        for i in range(col):
+            dp[0][i] = matrix[0][i]
+
+        for i in range(1,row):
+            for j in range(col):
+                if j == 0:
+                    dp[i][j] = matrix[i][j]+min(dp[i-1][j],dp[i-1][j+1])
+                elif j == col -1:
+                    dp[i][j] = matrix[i][j]+min(dp[i-1][j],dp[i-1][j-1])
+                else:
+                    dp[i][j] = matrix[i][j]+ min(dp[i-1][j],dp[i-1][j-1],dp[i-1][j+1])
+
+        return min(dp[row-1])
+        
