d151a98580d20ba504dc312dc65eedf704420dba
Kim Jiwon <71117552+KJW988@users.noreply.github.com>
2026-03-26 00:01:21 +0900

    Create Jiwon.py

---
+++ algorithm/2026/0323_Unique_Paths/Jiwon.py
@@ -0,0 +1,14 @@
+# 솔루션 참고
+
+class Solution:
+    def uniquePaths(self, m: int, n: int) -> int:
+        
+        aboveRow = [1] * n
+
+        for _ in range(m - 1):
+            currentRow = [1] * n
+            for i in range(1, n):
+                currentRow[i] = currentRow[i-1] + aboveRow[i]
+            aboveRow = currentRow
+        
+        return aboveRow[-1]
