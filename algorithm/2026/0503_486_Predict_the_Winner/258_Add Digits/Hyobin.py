aa5de61e5007a1f4149d70ed5f7fa2764fff4f15
Hyobin01 <67259135+Hyobin01@users.noreply.github.com>
2026-05-08 13:43:52 +0900

    Update Hyobin.py

--- algorithm/2026/0503_486_Predict_the_Winner/258_Add Digits/Hyobin.py
+++ algorithm/2026/0503_486_Predict_the_Winner/258_Add Digits/Hyobin.py
@@ -4,3 +4,12 @@ class Solution:
             num = (num // 10) + (num % 10)
 
         return num
+
+# ====================================
+
+class Solution:
+    def addDigits(self, num: int) -> int:
+        if num % 9 == 0 and num != 0:
+            return 9
+        else:
+            return num % 9
