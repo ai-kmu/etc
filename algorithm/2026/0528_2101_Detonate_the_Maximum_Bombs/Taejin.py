b46207d34d2f5a32db0952e3f4a0fce01343b2ac
Taej-P <78579124+Taej-P@users.noreply.github.com>
2026-06-04 12:20:41 +0900

    Update Taejin.py

--- algorithm/2026/0528_2101_Detonate_the_Maximum_Bombs/Taejin.py
+++ algorithm/2026/0528_2101_Detonate_the_Maximum_Bombs/Taejin.py
@@ -1,51 +1,29 @@
-# 140 테케에서 실패..
-
 class Solution:
     def maximumDetonation(self, bombs: List[List[int]]) -> int:
-        # 유효거리 정의
+        # 유효거리 정의 방정식
         is_in = lambda a, b: (a[0] - b[0])**2 + (a[1] - b[1])**2 <= a[2]**2
 
         # 유효거리 이내 폭탄 리스트
         booms = [[j for j in range(len(bombs)) if (j != i and is_in(bombs[i], bombs[j]))] for i in range(len(bombs))]
 
-        print(booms)
-
         ret = 0
 
         # i번째 index에서 폭탄 계산
-        def bfs(v, i):
-            queue = booms[i]
-            v[i] = 1
+        for i in range(len(bombs)):
+            visited = [0 for i in range(len(bombs))]
+            queue = booms[i][::]
+            visited[i] = 1
 
+            #bfs
             while queue:
-                print(queue)
                 j = queue.pop()
-                if v[j]:
+
+                if visited[j]:
                     continue
 
+                visited[j] = 1
                 queue += booms[j]
-                v[j] = 1
-
-            print(f"i total : {v}")
-
-        # 계산
-        for i in range(len(bombs)):
-            print(i, "="*10)
-            visited = [0 for i in range(len(bombs))]
-            bfs(visited, i)
-
-            print(sum(visited))
 
             ret = max(ret, sum(visited))
 
         return ret
-
-        
-
-
-
-
-    
-
-        
-
