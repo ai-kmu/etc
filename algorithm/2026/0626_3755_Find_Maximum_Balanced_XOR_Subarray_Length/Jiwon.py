c64bac33dbc31fb325a8fce5c03190f0736808a7
Kim Jiwon <71117552+KJW988@users.noreply.github.com>
2026-06-30 15:14:44 +0900

    Create Jiwon.py

---
+++ algorithm/2026/0626_3755_Find_Maximum_Balanced_XOR_Subarray_Length/Jiwon.py
@@ -0,0 +1,23 @@
+# 솔루션 참고: 미안합니다 ^^;;~
+
+class Solution:
+    def maxBalancedSubarray(self, nums: List[int]) -> int:
+
+        states = defaultdict(int)
+        cur_state = (0, 0)
+        ans = 0
+
+        states[cur_state] = -1
+
+        for idx, num in enumerate(nums):
+            xor_, evn_odd = cur_state
+            cur_state = (xor_^ num, evn_odd + 2 * (num%2) - 1)
+            
+            if cur_state not in states:
+                states[cur_state] = idx
+            else:
+                span = idx - states[cur_state]
+                if span > ans: ans = span
+                
+        return ans
+    
