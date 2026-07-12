9f514bf922171f636eeab232e50c732a9ea06a2b
Taej-P <78579124+Taej-P@users.noreply.github.com>
2026-07-12 19:09:45 +0900

    Update Taejin.py

--- algorithm/2026/0712_781_Rabbits_in_Forest/Taejin.py
+++ algorithm/2026/0712_781_Rabbits_in_Forest/Taejin.py
@@ -3,7 +3,7 @@ import math
 
 class Solution:
     def numRabbits(self, answers: List[int]) -> int:
-        # answer[i]라고 말한 토끼 : i + 1 배수만큼 있어야함
+        # answer[i]라고 말한 토끼 : answer[i] + 1 배수만큼 있어야함
         # 1이라고 답변 -> 최소 2마리, 1이 3개 있으면, 4마리 있어야함 -> 답변 + 1의 배수만큼
         cnt = Counter(answers)
         return sum([(k + 1) * math.ceil(v / (k + 1)) for k, v in cnt.items()])
