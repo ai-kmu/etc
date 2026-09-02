5c29ec85875d3d6f3e39d93a7a8fb2f64001755c
Taej-P <78579124+Taej-P@users.noreply.github.com>
2026-09-02 11:36:06 +0900

    Create Taejin.py

---
+++ algorithm/2026/0826_3867_Sum_of_GCD_of_Formed_Pairs/242_Valid_Anagram/Taejin.py
@@ -0,0 +1,6 @@
+class Solution:
+    def isAnagram(self, s: str, t: str) -> bool:
+        for c in set(s+t):
+            if s.count(c) != t.count(c):
+                return False
+        return True
