9772b7eed965a4c75ec0f2e49d57b689e0dfe871
Geonung Choi <56158371+ddugel3@users.noreply.github.com>
2026-09-02 11:51:01 +0900

    Remove debug print statements from isAnagram method

--- algorithm/2026/0826_3867_Sum_of_GCD_of_Formed_Pairs/242_Valid_Anagram/Geonung.py
+++ algorithm/2026/0826_3867_Sum_of_GCD_of_Formed_Pairs/242_Valid_Anagram/Geonung.py
@@ -1,7 +1,3 @@
 class Solution:
     def isAnagram(self, s: str, t: str) -> bool:
-        print(type(s))
-        print("".join(sorted(s)))
-        print("".join(sorted(t)))
-        print("".join(sorted(s)) == "".join(sorted(t)))
         return "".join(sorted(s)) == "".join(sorted(t))
